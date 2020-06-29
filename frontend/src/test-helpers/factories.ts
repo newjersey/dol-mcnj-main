efa0765c9df6bbcd4ae268056647fc33cd77c157
Anne LoVerso <anne.loverso@gmail.com>
2020-06-29 15:00:26 -0400

    use 120 chars for prettier line length

--- frontend/src/test-helpers/factories.ts
+++ frontend/src/test-helpers/factories.ts
@@ -1,7 +1,6 @@
 import { Program } from "../domain/Program";
 
-const randomInt = (): number =>
-  Math.floor(Math.random() * Math.floor(10000000));
+const randomInt = (): number => Math.floor(Math.random() * Math.floor(10000000));
 
 export const buildProgram = (overrides: Partial<Program>): Program => {
   return {
