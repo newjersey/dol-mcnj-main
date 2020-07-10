ea9f4732b0f0aeeb0f939649f45c580ce3b07f82
Anne LoVerso <anne.loverso@gmail.com>
2020-07-10 18:00:17 -0400

    Filter out suspended/pending results when search query is empty
    [Fixes #173755125]

--- backend/src/domain/searchTrainings.ts
+++ backend/src/domain/searchTrainings.ts
@@ -3,23 +3,17 @@ import { DataClient } from "./DataClient";
 import { SearchTrainings } from "./types";
 
 export const searchTrainingsFactory = (dataClient: DataClient): SearchTrainings => {
-  return async (searchQuery?: string): Promise<TrainingResult[]> => {
-    if (!searchQuery) {
-      return await dataClient.findAllTrainings();
-    }
-
-    const matchingIds = await dataClient.search(searchQuery);
-
-    return dataClient
-      .findTrainingsByIds(matchingIds)
-      .then((trainings: TrainingResult[]): TrainingResult[] => {
-        return trainings.filter(
-          (training) =>
-            training.status !== Status.SUSPENDED &&
-            training.status !== Status.PENDING &&
-            training.provider.status !== Status.SUSPENDED &&
-            training.provider.status !== Status.PENDING
-        );
-      });
-  };
+  return async (searchQuery?: string): Promise<TrainingResult[]> =>
+    (searchQuery
+      ? dataClient.search(searchQuery).then(dataClient.findTrainingsByIds)
+      : dataClient.findAllTrainings()
+    ).then((trainings: TrainingResult[]): TrainingResult[] => {
+      return trainings.filter(
+        (training) =>
+          training.status !== Status.SUSPENDED &&
+          training.status !== Status.PENDING &&
+          training.provider.status !== Status.SUSPENDED &&
+          training.provider.status !== Status.PENDING
+      );
+    });
 };
