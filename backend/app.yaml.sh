902945e0c2347e355e10917d8657fee5ebb67d63
Anne LoVerso <ann.loverso@gmail.com>
2020-07-14 14:24:41 -0400

    Deploy to production environment
    [Finishes #173756001]

--- backend/app.yaml.sh
+++ backend/app.yaml.sh
@@ -4,7 +4,7 @@ runtime: nodejs10
 env_variables:
   DB_USER: postgres
   DB_PASS: \"$DB_PASS\"
-  DB_NAME: d4addev
-  DB_ENV: gcpdev
-  CLOUD_SQL_CONNECTION_NAME: d4ad-dev:us-east4:d4ad-dev-gcp-pg
+  DB_NAME: \"$DB_NAME\"
+  DB_ENV: \"$DB_ENV\"
+  CLOUD_SQL_CONNECTION_NAME: \"$CLOUD_SQL_CONNECTION_NAME\"
 """
