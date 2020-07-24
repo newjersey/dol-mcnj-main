0563b62f553cd87ba58d15208a74a56d3fff355e
Anne LoVerso <ann.loverso@gmail.com>
2020-07-24 15:35:35 -0400

    Add cypress-axe for automated accessibility testing
    [Finishes #173990560]

--- frontend/cypress/integration/training-page.js
+++ frontend/cypress/integration/training-page.js
@@ -1,6 +1,8 @@
 describe('Training Page', () => {
   it('displays training details', () => {
     cy.visit('/training/61');
+    cy.injectAxe();
+
     cy.contains('Welding Technology/ Welder').should('exist');
     cy.contains('www.mcts.edu').should('exist');
     cy.contains('13 months to 2 years to complete').should('exist');
@@ -15,5 +17,7 @@ describe('Training Page', () => {
       'Career Track: Welders, Cutters, Solderers, and Brazers, ' +
       'Welding, Soldering, and Brazing Machine Setters, Operators, and Tenders'
     ).should('exist');
+
+    cy.checkA11y();
   });
 });
