75c0a8f0f8391f8abe0203dbdf8e24392443035f
Anne LoVerso <ann.loverso@gmail.com>
2020-08-05 16:04:20 -0400

    Fix highlight bug when training has no description
    [Finishes #173687188]

--- backend/migrations/sqls/20200616144558-seed-programs-up-TEST.sql
+++ backend/migrations/sqls/20200616144558-seed-programs-up-TEST.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dad3b6e25f1800710f245982ee46a222057aba06d3b19e59a94dc066b7b74107
-size 7228
+oid sha256:59505eab7c170ef11d353c1a126c46571ab29c84a7be6abcdd02d7c9ceda520b
+size 8536
