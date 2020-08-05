2826ee9e6d75f786eb53657b08ecc06ecaee9a5e
Anne LoVerso <ann.loverso@gmail.com>
2020-08-05 11:32:34 -0400

    Make beta banner text shorter all the time
    - addresses large font size issues
    
    [#173968909]

--- frontend/src/components/BetaBanner.tsx
+++ frontend/src/components/BetaBanner.tsx
@@ -1,48 +1,11 @@
 import React, { ReactElement } from "react";
-import { useMediaQuery } from "@material-ui/core";
 
 interface Props {
   noHeader?: boolean;
 }
 
 export const BetaBanner = (props: Props): ReactElement => {
-  const isTabletAndUp = useMediaQuery("(min-width:768px)");
   const feedbackLink = "https://forms.gle/XSmLCPHBctFVSGsA6";
-
-  const getMessage = (): ReactElement => {
-    if (isTabletAndUp) {
-      return (
-        <>
-          Welcome to the beta version of Training Explorer. Please give us feedback on your
-          experience&nbsp;
-          <a
-            className="link-format-blue"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={feedbackLink}
-          >
-            here
-          </a>
-          .
-        </>
-      );
-    } else {
-      return (
-        <>
-          This site is in beta. Feedback welcome&nbsp;
-          <a
-            className="link-format-blue"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={feedbackLink}
-          >
-            here
-          </a>
-          .
-        </>
-      );
-    }
-  };
 
   const getHeaderMarginClass = (): string => {
     if (props.noHeader) {
@@ -52,5 +15,13 @@ export const BetaBanner = (props: Props): ReactElement => {
     }
   };
 
-  return <aside className={`beta-banner ${getHeaderMarginClass()}`}>{getMessage()}</aside>;
+  return (
+    <aside className={`beta-banner ${getHeaderMarginClass()}`}>
+      This site is in beta. Feedback welcome&nbsp;
+      <a className="link-format-blue" target="_blank" rel="noopener noreferrer" href={feedbackLink}>
+        here
+      </a>
+      .
+    </aside>
+  );
 };
