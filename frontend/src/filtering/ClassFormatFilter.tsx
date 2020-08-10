f3c49039d8cfb96c88da7bc5fe2a4a823a84e696
Anne LoVerso <ann.loverso@gmail.com>
2020-08-10 15:01:21 -0400

    Add filter for online/in-person class format
    [Finishes #174011524]

---
+++ frontend/src/filtering/ClassFormatFilter.tsx
@@ -0,0 +1,82 @@
+import React, { ChangeEvent, ReactElement, useContext, useEffect, useState } from "react";
+import { FilterActionType, FilterContext } from "../App";
+import { FilterableElement } from "../domain/Filter";
+import { TrainingResult } from "../domain/Training";
+import { FormControlLabel, FormGroup } from "@material-ui/core";
+import { SpacedCheckbox } from "../components/SpacedCheckbox";
+
+interface ClassFormat {
+  online: boolean;
+  inPerson: boolean;
+}
+
+export const ClassFormatFilter = (): ReactElement => {
+  const [classFormat, setClassFormat] = useState<ClassFormat>({
+    online: false,
+    inPerson: false,
+  });
+
+  const { state, dispatch } = useContext(FilterContext);
+
+  useEffect(() => {
+    const classFormatFilter = state.filters.find(
+      (filter) => filter.element === FilterableElement.CLASS_FORMAT
+    );
+    if (classFormatFilter) {
+      setClassFormat(classFormatFilter.value);
+    }
+  }, [state.filters]);
+
+  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>, checked: boolean): void => {
+    const newClassFormat = {
+      ...classFormat,
+      [event.target.name]: checked,
+    };
+
+    setClassFormat(newClassFormat);
+
+    const nothingIsChecked = Object.values(newClassFormat).every((value) => !value);
+
+    dispatch({
+      type: nothingIsChecked ? FilterActionType.REMOVE : FilterActionType.ADD,
+      filter: {
+        element: FilterableElement.CLASS_FORMAT,
+        value: newClassFormat,
+        func: (trainingResults): TrainingResult[] =>
+          trainingResults.filter((it) => {
+            return (newClassFormat.inPerson && !it.online) || (newClassFormat.online && it.online);
+          }),
+      },
+    });
+  };
+
+  return (
+    <label className="bold" htmlFor="classFormat">
+      Class Format
+      <FormGroup id="classFormat">
+        <FormControlLabel
+          control={
+            <SpacedCheckbox
+              checked={classFormat.inPerson}
+              onChange={handleCheckboxChange}
+              name="inPerson"
+              color="primary"
+            />
+          }
+          label="In-Person"
+        />
+        <FormControlLabel
+          control={
+            <SpacedCheckbox
+              checked={classFormat.online}
+              onChange={handleCheckboxChange}
+              name="online"
+              color="primary"
+            />
+          }
+          label="Online"
+        />
+      </FormGroup>
+    </label>
+  );
+};
