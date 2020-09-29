b42a12410465057bdedc71c383d433e4a075f7bb
Anne LoVerso <anne.loverso@gmail.com>
2020-09-29 09:59:13 -0400

    Parallelize calls for occupation detail page
    [Finishes #174867952]

--- backend/src/domain/occupations/getOccupationDetail.ts
+++ backend/src/domain/occupations/getOccupationDetail.ts
@@ -16,52 +16,71 @@ export const getOccupationDetailFactory = (
   dataClient: DataClient
 ): GetOccupationDetail => {
   return async (soc: string): Promise<OccupationDetail> => {
+
     const isInDemand = async (soc: string): Promise<boolean> => {
       const inDemandOccupations = await dataClient.getInDemandOccupationTitles();
       return inDemandOccupations.map((it) => it.soc).includes(soc);
     };
 
-    const education = await getEducationText(soc);
-    const medianSalary = await getSalaryEstimate(soc);
-
     return getOccupationDetailFromOnet(soc)
-      .then(async (onetOccupationDetail: OccupationDetailPartial) => {
-        return {
-          ...onetOccupationDetail,
-          education: education,
-          inDemand: await isInDemand(soc),
-          medianSalary: medianSalary,
-          openJobsCount: await getOpenJobsCount(soc),
-        };
+      .then((onetOccupationDetail: OccupationDetailPartial) => {
+
+        return Promise.all([
+          isInDemand(soc),
+          getOpenJobsCount(soc),
+          getEducationText(soc),
+          getSalaryEstimate(soc)
+        ]).then(([inDemand, openJobsCount, education, medianSalary]) => {
+          return {
+            ...onetOccupationDetail,
+            education: education,
+            inDemand: inDemand,
+            medianSalary: medianSalary,
+            openJobsCount: openJobsCount,
+          };
+        })
       })
       .catch(async () => {
         const occupationTitles2010 = await dataClient.find2010OccupationTitlesBySoc2018(soc);
 
         if (occupationTitles2010.length === 1) {
           const soc2010 = occupationTitles2010[0].soc;
-          const onetOccupationDetail = await getOccupationDetailFromOnet(soc2010);
 
-          return {
-            ...onetOccupationDetail,
-            soc: soc,
-            education: education,
-            inDemand: await isInDemand(soc2010),
-            medianSalary: medianSalary,
-            openJobsCount: await getOpenJobsCount(soc2010),
-          };
+          return Promise.all([
+            getOccupationDetailFromOnet(soc2010),
+            isInDemand(soc2010),
+            getOpenJobsCount(soc2010),
+            getEducationText(soc),
+            getSalaryEstimate(soc)
+          ]).then(([onetOccupationDetail, inDemand, openJobsCount, education, medianSalary]) => {
+            return {
+              ...onetOccupationDetail,
+              soc: soc,
+              education: education,
+              inDemand: inDemand,
+              medianSalary: medianSalary,
+              openJobsCount: openJobsCount,
+            };
+          })
         } else {
-          const socDefinition = await dataClient.findSocDefinitionBySoc(soc);
-
-          return {
-            soc: socDefinition.soc,
-            title: socDefinition.soctitle,
-            description: socDefinition.socdefinition,
-            tasks: [],
-            education: education,
-            inDemand: await isInDemand(socDefinition.soc),
-            medianSalary: medianSalary,
-            openJobsCount: await getOpenJobsCount(socDefinition.soc),
-          };
+          return Promise.all([
+            dataClient.findSocDefinitionBySoc(soc),
+            isInDemand(soc),
+            getOpenJobsCount(soc),
+            getEducationText(soc),
+            getSalaryEstimate(soc)
+          ]).then(([socDefinition, inDemand, openJobsCount, education, medianSalary]) => {
+            return {
+              soc: socDefinition.soc,
+              title: socDefinition.soctitle,
+              description: socDefinition.socdefinition,
+              tasks: [],
+              education: education,
+              inDemand: inDemand,
+              medianSalary: medianSalary,
+              openJobsCount: openJobsCount,
+            };
+          })
         }
       });
   };
