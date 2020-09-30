7ad17d1d58e89f7ebe262eeb4fef5001cfa2c606
Anne LoVerso <anne.loverso@gmail.com>
2020-09-30 00:33:38 -0400

    Chunk migration for easier processing

---
+++ backend/migrations/20200916215915-update-etpl-table_3.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+var fs = require('fs');
+var path = require('path');
+var Promise;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+  Promise = options.Promise;
+};
+
+exports.up = function(db) {
+  const fileName = process.env.NODE_ENV === 'test' ? '20200916215915-update-etpl-table-up-TEST.sql' : '20200916215915-update-etpl-table-up_3.sql';
+  var filePath = path.join(__dirname, 'sqls', fileName);
+  return new Promise( function( resolve, reject ) {
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+      if (err) return reject(err);
+      console.log('received data: ' + data);
+
+      resolve(data);
+    });
+  })
+  .then(function(data) {
+    return db.runSql(data);
+  });
+};
+
+exports.down = function(db) {
+  const fileName = process.env.NODE_ENV === 'test' ? '20200916215915-update-etpl-table-down-TEST.sql' : '20200916215915-update-etpl-table-down.sql';
+  var filePath = path.join(__dirname, 'sqls', fileName);
+  return new Promise( function( resolve, reject ) {
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+      if (err) return reject(err);
+      console.log('received data: ' + data);
+
+      resolve(data);
+    });
+  })
+  .then(function(data) {
+    return db.runSql(data);
+  });
+};
+
+exports._meta = {
+  "version": 1
+};
