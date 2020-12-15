a987634e13be81e21a86588f5cc16e73cc015f28
Aisha Green <aishagreen2008@gmail.com>
2020-12-15 16:20:23 -0500

    add footer to site
    - update footer content
    
    [#176124563]

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,19 +1,27 @@
 import React, { ReactElement } from "react";
-import { Link } from "@reach/router";
+import { Icon } from "@material-ui/core";
+// import { Link } from "@reach/router";
 
 export const Footer = (): ReactElement => {
   return (
-    <footer className="footer bg-light-purple pvd width-100">
+    <footer className="footer bg-light-purple pvd width-100 flex fac">
       <div className="container">
-        <Link className="no-link-format prd" to="/about">
-          About
-        </Link>
-        <Link className="no-link-format phd" to="/privacy">
-          Privacy Policy
-        </Link>
-        <Link className="no-link-format phd" to="/tos">
-          Terms of Service
-        </Link>
+        <p className="flex fac maz">
+          Made with&nbsp;
+          <Icon className="red">favorite</Icon>
+          &nbsp;by the Office of Innovation
+        </p>
+        {/*
+          <Link className="no-link-format prd" to="/about">
+            About
+          </Link>
+          <Link className="no-link-format phd" to="/privacy">
+            Privacy Policy
+          </Link>
+          <Link className="no-link-format phd" to="/tos">
+            Terms of Service
+          </Link>
+        */}
       </div>
     </footer>
   );
