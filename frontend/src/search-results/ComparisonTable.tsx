8f5483ededac4154a258830d59737a9725f81390
Aisha Green <aishagreen2008@gmail.com>
2021-04-29 14:37:11 -0400

    comparison tool style fixes
    [#177349051]

--- frontend/src/search-results/ComparisonTable.tsx
+++ frontend/src/search-results/ComparisonTable.tsx
@@ -20,10 +20,11 @@ export const ComparisonTable = (props: Props): ReactElement => {
   const showInDemand = item1?.inDemand || item2?.inDemand || item3?.inDemand;
 
   const tblClasses = (): string => {
-    const view = isTablet ? "comparison-table " : "comparison-table-mobile ";
-    const wide = props.data.length > 2 ? "wide " : "";
-    const scroll = !props.scrollEnd ? "scroll-end " : "";
-    return view + wide + scroll + "mbs";
+    const view = isTablet ? "comparison-table" : "comparison-table-mobile";
+    const wide = props.data.length > 2 ? "wide" : "";
+    const scroll = !props.scrollEnd ? "scroll-end" : "";
+    const margin = !isTablet ? "mbs" : "";
+    return `${view} ${wide} ${scroll} ${margin}`;
   };
 
   const mobileTable = (): ReactElement => {
@@ -32,115 +33,139 @@ export const ComparisonTable = (props: Props): ReactElement => {
         <tbody>
           <tr>
             {item1 && (
-              <td className="weight-500 align-center" key={`${item1.id}-nam`}>
+              <td
+                className="weight-500 align-top align-center pas phs bhdcg"
+                key={`${item1.id}-nam`}
+              >
                 {item1.name}
               </td>
             )}
             {item2 && (
-              <td className="weight-500 align-center" key={`${item2.id}-nam`}>
+              <td
+                className="weight-500 align-top align-center pas phs bhdcg"
+                key={`${item2.id}-nam`}
+              >
                 {item2.name}
               </td>
             )}
             {item3 && (
-              <td className="weight-500 align-center" key={`${item3.id}-nam`}>
+              <td
+                className="weight-500 align-top align-center pas phs bhdcg"
+                key={`${item3.id}-nam`}
+              >
                 {item3.name}
               </td>
             )}
           </tr>
           <tr>
             {item1 && (
-              <td className="align-center" key={`${item1.id}-pro`}>
+              <td
+                className={`align-top align-center bhdcg ${showInDemand ? "pas" : "pts pbd phs"}`}
+                key={`${item1.id}-pro`}
+              >
                 {item1.providerName}
               </td>
             )}
             {item2 && (
-              <td className="align-center" key={`${item2.id}-pro`}>
+              <td
+                className={`align-top align-center bhdcg ${showInDemand ? "pas" : "pts pbd phs"}`}
+                key={`${item2.id}-pro`}
+              >
                 {item2.providerName}
               </td>
             )}
             {item3 && (
-              <td className="align-center" key={`${item3.id}-pro`}>
+              <td
+                className={`align-top align-center bhdcg ${showInDemand ? "pas" : "pts pbd phs"}`}
+                key={`${item3.id}-pro`}
+              >
                 {item3.providerName}
               </td>
             )}
           </tr>
           {showInDemand && (
             <tr>
-              {item1.inDemand && (
-                <td className="align-center spacing" key={`${item1.id}-dem`}>
-                  {<InDemandTag />}
+              <td className={`align-center pas brdcg`} key={`${item1.id}-dem`}>
+                {item1?.inDemand && <InDemandTag />}
+              </td>
+              {(item2 || item3) && (
+                <td className="align-center pas" key={`${item2.id}-dem`}>
+                  {item2?.inDemand && <InDemandTag />}
                 </td>
               )}
-              {item2?.inDemand && (
-                <td className="align-center spacing" key={`${item2.id}-dem`}>
-                  <InDemandTag />
-                </td>
-              )}
-              {item3?.inDemand && (
-                <td className="align-center spacing" key={`${item3.id}-dem`}>
-                  <InDemandTag />
+              {item3 && (
+                <td className={`align-center pas bldcg`} key={`${item3.id}-dem`}>
+                  {item3?.inDemand && <InDemandTag />}
                 </td>
               )}
             </tr>
           )}
           <tr>
-            <td className="weight-500">
+            <td className="weight-500 pas bhdcg bg-light-purple-30">
               <span>Cost</span>
               {/*!item2 && <Icon fontSize="inherit">info</Icon>*/}
             </td>
             {item2 && (
-              <td>
+              <td className="weight-500 pas bhdcg bg-light-purple-30">
                 {/*props.scrollEnd ? <Icon fontSize="inherit">info</Icon> : 'Cost'*/}
                 {!props.scrollEnd && <span className="weight-500">Cost</span>}
               </td>
             )}
-            {item3 && <td>{/*<Icon fontSize="inherit">info</Icon>*/}</td>}
+            {item3 && (
+              <td className="weight-500 pas bhdcg bg-light-purple-30">
+                {/*<Icon fontSize="inherit">info</Icon>*/}
+              </td>
+            )}
           </tr>
           <tr>
             {item1 && (
-              <td className="align-center spacing" key={`${item1.id}-cos`}>
+              <td className="align-center pas bhdcg" key={`${item1.id}-cos`}>
                 {formatMoney(item1.totalCost)}
               </td>
             )}
             {item2 && (
-              <td className="align-center spacing" key={`${item2.id}-cos`}>
+              <td className="align-center pas bhdcg" key={`${item2.id}-cos`}>
                 {formatMoney(item2.totalCost)}
               </td>
             )}
             {item3 && (
-              <td className="align-center spacing" key={`${item3.id}-cos`}>
+              <td className="align-center pas bhdcg" key={`${item3.id}-cos`}>
                 {formatMoney(item3.totalCost)}
               </td>
             )}
           </tr>
           <tr>
-            <td className="weight-500">
+            <td className="weight-500 pas bhdcg bg-light-purple-50">
               <span>Employment Rate</span>
               {/*!item2 && <Icon fontSize="inherit">info</Icon>*/}
             </td>
             {item2 && (
-              <td>
+              <td className="weight-500 pas bhdcg bg-light-purple-50">
                 {/*props.scrollEnd ? <Icon fontSize="inherit">info</Icon> : 'Cost'*/}
                 {!props.scrollEnd && <span className="weight-500">Employment Rate</span>}
               </td>
             )}
-            {item3 && <td>{/*<Icon fontSize="inherit">info</Icon>*/}</td>}
+            {item3 && (
+              <td className="weight-500 pas bhdcg bg-light-purple-50">
+                {/*<Icon fontSize="inherit">info</Icon>*/}
+              </td>
+            )}
           </tr>
           <tr>
-            <td className="align-center spacing" key={`${item1.id}-emp`}>
+            <td className="align-center pas bhdcg" key={`${item1.id}-emp`}>
               {item1.percentEmployed
                 ? formatPercentEmployed(item1.percentEmployed) + " Employed"
                 : "--"}
             </td>
             {item2 && (
-              <td className="align-center spacing" key={`${item2.id}-emp`}>
+              <td className="align-center pas bhdcg" key={`${item2.id}-emp`}>
                 {item2.percentEmployed
                   ? formatPercentEmployed(item2.percentEmployed) + " Employed"
                   : "--"}
               </td>
             )}
             {item3 && (
-              <td className="align-center spacing" key={`${item3.id}-emp`}>
+              <td className="align-center pas bhdcg" key={`${item3.id}-emp`}>
                 {item3.percentEmployed
                   ? formatPercentEmployed(item3.percentEmployed) + " Employed"
                   : "--"}
@@ -148,52 +173,56 @@ export const ComparisonTable = (props: Props): ReactElement => {
             )}
           </tr>
           <tr>
-            <td className="weight-500">
+            <td className="weight-500 pas bhdcg bg-light-purple">
               <span>Time to Complete</span>
               {/*!item2 && <Icon fontSize="inherit">info</Icon>*/}
             </td>
             {item2 && (
-              <td>
+              <td className="weight-500 pas bhdcg bg-light-purple">
                 {/*props.scrollEnd ? <Icon fontSize="inherit">info</Icon> : 'Cost'*/}
                 {!props.scrollEnd && <span className="weight-500">Time to Complete</span>}
               </td>
             )}
-            {item3 && <td>{/*<Icon fontSize="inherit">info</Icon>*/}</td>}
+            {item3 && (
+              <td className="weight-500 pas bhdcg bg-light-purple">
+                {/*<Icon fontSize="inherit">info</Icon>*/}
+              </td>
+            )}
           </tr>
           <tr>
             {item1 && (
-              <td className="duration align-center spacing" key={`${item1.id}-cal`}>
+              <td className="align-center ptd pbxl bhdcg" key={`${item1.id}-cal`}>
                 {CalendarLengthLookup[item1.calendarLength]}
               </td>
             )}
             {item2 && (
-              <td className="duration align-center spacing" key={`${item2.id}-cal`}>
+              <td className="align-center ptd pbxl bhdcg" key={`${item2.id}-cal`}>
                 {CalendarLengthLookup[item2.calendarLength]}
               </td>
             )}
             {item3 && (
-              <td className="duration align-center spacing" key={`${item3.id}-cal`}>
+              <td className="align-center ptd pbxl bhdcg" key={`${item3.id}-cal`}>
                 {CalendarLengthLookup[item3.calendarLength]}
               </td>
             )}
           </tr>
           <tr>
             {item1 && (
-              <td className="align-center" key={`${item1.id}-det`}>
+              <td className="align-center ptd pbs bhdcg" key={`${item1.id}-det`}>
                 <LinkButton className=" btn-details" to={`/training/${item1.id}`}>
                   See Details
                 </LinkButton>
               </td>
             )}
             {item2 && (
-              <td className="align-center" key={`${item2.id}-det`}>
+              <td className="align-center ptd pbs bhdcg" key={`${item2.id}-det`}>
                 <LinkButton className=" btn-details" to={`/training/${item2.id}`}>
                   See Details
                 </LinkButton>
               </td>
             )}
             {item3 && (
-              <td className="align-center" key={`${item3.id}-det`}>
+              <td className="align-center ptd pbs bhdcg" key={`${item3.id}-det`}>
                 <LinkButton className=" btn-details" to={`/training/${item3.id}`}>
                   See Details
                 </LinkButton>
@@ -210,63 +239,79 @@ export const ComparisonTable = (props: Props): ReactElement => {
       <table className={tblClasses()}>
         <tbody>
           <tr>
-            <td className="tbl-header"></td>
+            <td className="tbl-header pas" />
             {item1 && (
-              <td className="weight-500 align-center" key={`${item1.id}-nam`}>
+              <td className="weight-500 align-top align-center pvs" key={`${item1.id}-nam`}>
                 {item1.name}
               </td>
             )}
             {item2 && (
-              <td className="weight-500 align-center bldcg" key={`${item2.id}-nam`}>
+              <td className="weight-500 align-top align-center pvs bldcg" key={`${item2.id}-nam`}>
                 {item2.name}
               </td>
             )}
             {item3 && (
-              <td className="weight-500 align-center bldcg" key={`${item3.id}-nam`}>
+              <td className="weight-500 align-top align-center pvs bldcg" key={`${item3.id}-nam`}>
                 {item3.name}
               </td>
             )}
           </tr>
           <tr>
-            <td className="tbl-header"></td>
+            <td className="tbl-header" />
             {item1 && (
-              <td className="align-center" key={`${item1.id}-pro`}>
+              <td
+                className={`align-top align-center ${!showInDemand ? "pbd" : ""}`}
+                key={`${item1.id}-pro`}
+              >
                 {item1.providerName}
               </td>
             )}
             {item2 && (
-              <td className="align-center bldcg" key={`${item2.id}-pro`}>
+              <td
+                className={`align-top align-center bldcg ${!showInDemand ? "pbd" : ""}`}
+                key={`${item2.id}-pro`}
+              >
                 {item2.providerName}
               </td>
             )}
             {item3 && (
-              <td className="align-center bldcg" key={`${item3.id}-pro`}>
+              <td
+                className={`align-top align-center bldcg ${!showInDemand ? "pbd" : ""}`}
+                key={`${item3.id}-pro`}
+              >
                 {item3.providerName}
               </td>
             )}
           </tr>
           {showInDemand && (
             <tr>
-              <td className="tbl-header"></td>
-              {item1.inDemand && (
-                <td className="align-center spacing" key={`${item1.id}-dem`}>
-                  {<InDemandTag />}
-                  {/*<Icon fontSize="inherit">info</Icon>*/}
+              <td className="tbl-header" />
+              <td className={`align-center pvd brdcg`} key={`${item1.id}-dem`}>
+                {item1?.inDemand && (
+                  <>
+                    <InDemandTag />
+                    {/*<Icon fontSize="inherit">info</Icon>*/}
+                  </>
+                )}
+              </td>
+              {(item2 || item3) && (
+                <td className="align-center pvd" key={`${item2.id}-dem`}>
+                  {item2?.inDemand && (
+                    <>
+                      <InDemandTag />
+                      {/*<Icon fontSize="inherit">info</Icon>*/}
+                    </>
+                  )}
                 </td>
               )}
-              {item2?.inDemand && (
-                <td
-                  className={`align-center spacing bldcg ${item3 && "brdcg"}`}
-                  key={`${item2.id}-dem`}
-                >
-                  <InDemandTag />
-                  {/*<Icon fontSize="inherit">info</Icon>*/}
-                </td>
-              )}
-              {item3?.inDemand && (
-                <td className="align-center spacing bldcg" key={`${item3.id}-dem`}>
-                  <InDemandTag />
-                  {/*<Icon fontSize="inherit">info</Icon>*/}
+              {item3 && (
+                <td className={`align-center pvd bldcg`} key={`${item3.id}-dem`}>
+                  {item3?.inDemand && (
+                    <>
+                      <InDemandTag />
+                      {/*<Icon fontSize="inherit">info</Icon>*/}
+                    </>
+                  )}
                 </td>
               )}
             </tr>
@@ -277,17 +322,17 @@ export const ComparisonTable = (props: Props): ReactElement => {
               {/*<Icon fontSize="inherit">info</Icon>*/}
             </td>
             {item1 && (
-              <td className="align-center spacing btdcg" key={`${item1.id}-cos`}>
+              <td className="align-center pvd btdcg" key={`${item1.id}-cos`}>
                 {formatMoney(item1.totalCost)}
               </td>
             )}
             {item2 && (
-              <td className="align-center spacing btdcg bldcg" key={`${item2.id}-cos`}>
+              <td className="align-center pvd btdcg bldcg" key={`${item2.id}-cos`}>
                 {formatMoney(item2.totalCost)}
               </td>
             )}
             {item3 && (
-              <td className="align-center spacing btdcg bldcg" key={`${item3.id}-cos`}>
+              <td className="align-center pvd btdcg bldcg" key={`${item3.id}-cos`}>
                 {formatMoney(item3.totalCost)}
               </td>
             )}
@@ -297,20 +342,20 @@ export const ComparisonTable = (props: Props): ReactElement => {
               <span>Employment Rate %</span>
               {/*<Icon fontSize="inherit">info</Icon>*/}
             </td>
-            <td className="align-center spacing btdcg" key={`${item1.id}-emp`}>
+            <td className="align-center pvd btdcg" key={`${item1.id}-emp`}>
               {item1.percentEmployed
                 ? formatPercentEmployed(item1.percentEmployed) + " Employed"
                 : "--"}
             </td>
             {item2 && (
-              <td className="align-center spacing btdcg bldcg" key={`${item2.id}-emp`}>
+              <td className="align-center pvd btdcg bldcg" key={`${item2.id}-emp`}>
                 {item2.percentEmployed
                   ? formatPercentEmployed(item2.percentEmployed) + " Employed"
                   : "--"}
               </td>
             )}
             {item3 && (
-              <td className="align-center spacing btdcg bldcg" key={`${item3.id}-emp`}>
+              <td className="align-center pvd btdcg bldcg" key={`${item3.id}-emp`}>
                 {item3.percentEmployed
                   ? formatPercentEmployed(item3.percentEmployed) + " Employed"
                   : "--"}
@@ -323,17 +368,17 @@ export const ComparisonTable = (props: Props): ReactElement => {
               {/*<Icon fontSize="inherit">info</Icon>*/}
             </td>
             {item1 && (
-              <td className="duration align-center spacing btdcg" key={`${item1.id}-cal`}>
+              <td className="align-center ptd pbl btdcg" key={`${item1.id}-cal`}>
                 {CalendarLengthLookup[item1.calendarLength]}
               </td>
             )}
             {item2 && (
-              <td className="duration align-center spacing btdcg bldcg" key={`${item2.id}-cal`}>
+              <td className="align-center ptd pbl btdcg bldcg" key={`${item2.id}-cal`}>
                 {CalendarLengthLookup[item2.calendarLength]}
               </td>
             )}
             {item3 && (
-              <td className="duration align-center spacing btdcg bldcg" key={`${item3.id}-cal`}>
+              <td className="align-center ptd pbl btdcg bldcg" key={`${item3.id}-cal`}>
                 {CalendarLengthLookup[item3.calendarLength]}
               </td>
             )}
@@ -341,21 +386,21 @@ export const ComparisonTable = (props: Props): ReactElement => {
           <tr>
             <td className="tbl-header"></td>
             {item1 && (
-              <td className="align-center details bldcg" key={`${item1.id}-det`}>
+              <td className="align-center align-bottom ptd pbz bldcg" key={`${item1.id}-det`}>
                 <LinkButton className=" btn-details" to={`/training/${item1.id}`}>
                   See Details
                 </LinkButton>
               </td>
             )}
             {item2 && (
-              <td className="align-center details bldcg" key={`${item2.id}-det`}>
+              <td className="align-center align-bottom ptd pbz bldcg" key={`${item2.id}-det`}>
                 <LinkButton className=" btn-details" to={`/training/${item2.id}`}>
                   See Details
                 </LinkButton>
               </td>
             )}
             {item3 && (
-              <td className="align-center details bldcg" key={`${item3.id}-det`}>
+              <td className="align-center align-bottom ptd pbz bldcg" key={`${item3.id}-det`}>
                 <LinkButton className=" btn-details" to={`/training/${item3.id}`}>
                   See Details
                 </LinkButton>
