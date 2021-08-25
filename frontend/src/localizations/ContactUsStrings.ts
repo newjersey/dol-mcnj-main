a84ca66dee99b0c988a2c1a244b8fd7826d10f6a
Crystal <penalosa619@gmail.com>
2021-08-25 14:35:45 -0400

    Update ContactUsStrings.ts
    changes contact email to TEU email

--- frontend/src/localizations/ContactUsStrings.ts
+++ frontend/src/localizations/ContactUsStrings.ts
@@ -2,5 +2,5 @@ export const ContactUsStrings: Record<string, string> = {
   header: "Have More Questions?",
   getInTouchText: "Get in Touch",
   contactUsButtonText: "Contact Us",
-  contactUsButtonLink: "mailto:test@email.com",
+  contactUsButtonLink: "mailto:TrainingEvaluationUnit@dol.nj.gov",
 };
