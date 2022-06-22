3feebac263b2270ac0c1a2ccf34d113dd63b042d
Naman Agrawal <naman@innovation.nj.gov>
2022-06-22 15:20:46 -0400

    chore: delete oldest 5 gcloud app engine versions on each deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,13 @@
 cd backend
 ./app.yaml.sh > app.yaml
 gcloud app deploy
+
+versions=$(gcloud app versions list \
+  --service default \
+  --sort-by '~VERSION.ID' \
+  --format 'value(VERSION.ID)' | sed 1,5d)
+for version in $versions; do
+  gcloud app versions delete "$version" \
+    --service default \
+    --quiet
+done
