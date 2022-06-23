2e5a4652b19a7f41ddfd865f508e4f756b75d89d
Naman Agrawal <naman@innovation.nj.gov>
2022-06-23 11:07:48 -0400

    fix: [#248] remove console log from google tracking

--- frontend/src/analytics.ts
+++ frontend/src/analytics.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const window: any;
 export function logEvent(category: string, action: string, label: string): void {
-  console.log(category, action, label);
   if (typeof window.gtag === "function") {
     window.gtag("event", action, {
       event_category: category,
