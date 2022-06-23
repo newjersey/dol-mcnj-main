fb859f25ec2497251056f2ea777f9420c954c8ef
Naman Agrawal <naman@innovation.nj.gov>
2022-06-23 16:59:29 -0400

    feat: [#248] adding google analytics event tracking for key interactions

--- frontend/src/analytics.ts
+++ frontend/src/analytics.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const window: any;
-export function logEvent(category: string, action: string, label: string): void {
+export function logEvent(category: string, action: string, label: string | undefined): void {
   if (typeof window.gtag === "function") {
     window.gtag("event", action, {
       event_category: category,
