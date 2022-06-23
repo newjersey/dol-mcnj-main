54e5f931d03861e8b35168eb176b11c515e88076
Naman Agrawal <naman@innovation.nj.gov>
2022-06-23 11:07:09 -0400

    feat: [#248] add google event tracking for filters and language switch

--- frontend/src/components/LanguageSwitchButton.tsx
+++ frontend/src/components/LanguageSwitchButton.tsx
@@ -1,16 +1,19 @@
 import React, { ReactElement } from "react";
 import { Button } from "./Button";
 import { useTranslation } from "react-i18next";
+import { logEvent } from "../analytics";
 
 export const LanguageSwitchButton = (): ReactElement => {
   const { i18n } = useTranslation();
 
+  function onClick() {
+    const newLang = i18n.language === "en" ? "es" : "en";
+    i18n.changeLanguage(newLang);
+    logEvent("General", "Switched language", newLang);
+  }
+
   return (
-    <Button
-      variant="secondary"
-      className="language-switch-buttton width-auto"
-      onClick={() => i18n.changeLanguage(i18n.language === "en" ? "es" : "en")}
-    >
+    <Button variant="secondary" className="language-switch-buttton width-auto" onClick={onClick}>
       {i18n.language === "en" ? "Español" : "English"}
     </Button>
   );
--- frontend/src/filtering/FilterContext.ts
+++ frontend/src/filtering/FilterContext.ts
@@ -1,7 +1,8 @@
 /* eslint-disable  @typescript-eslint/no-redeclare */
 
 import React, { Dispatch, Reducer } from "react";
-import { Filter } from "../domain/Filter";
+import { logEvent } from "../analytics";
+import { Filter, FilterableElement } from "../domain/Filter";
 
 export const initialFilterState = {
   filters: [],
@@ -34,25 +35,63 @@ export enum FilterActionType {
   REMOVE_ALL,
 }
 
+const ACTION_TO_EVENT_ACTION: { [key in FilterActionType]: string } = {
+  [FilterActionType.ADD]: "Added filters",
+  [FilterActionType.REMOVE]: "Removed filters",
+  [FilterActionType.REMOVE_ALL]: "Cleared filters",
+};
+
 const removeFilter = (filter: Filter | undefined, list: Filter[]): Filter[] => {
   if (filter == null) return list;
   return list.filter((it: Filter) => it.element !== filter.element);
 };
 
+const logFilterEvent = (filters: Filter[], action: FilterActionType): void => {
+  const formattedFilters = filters.map((f) => {
+    let loggedFilterValue = f.value;
+    if (f.element === FilterableElement.LOCATION) {
+      loggedFilterValue = [f.value.center, f.value.radius];
+    }
+    if (
+      typeof loggedFilterValue === "object" &&
+      loggedFilterValue != null &&
+      !Array.isArray(loggedFilterValue)
+    ) {
+      loggedFilterValue = Object.entries(loggedFilterValue)
+        .filter(([key, val]) => val === true)
+        .map(([key, val]) => key);
+    }
+    return {
+      type: FilterableElement[f.element],
+      value: Array.isArray(loggedFilterValue)
+        ? loggedFilterValue.join(",")
+        : String(loggedFilterValue),
+    };
+  });
+
+  logEvent("Search", ACTION_TO_EVENT_ACTION[action], JSON.stringify(formattedFilters));
+};
+
 export const filterReducer = (state: FilterState, action: FilterAction): FilterState => {
   switch (action.type) {
-    case FilterActionType.ADD:
+    case FilterActionType.ADD: {
       if (action.filter == null) return state;
+      const newFilters = [...removeFilter(action.filter, state.filters), action.filter];
+      logFilterEvent(newFilters, action.type);
       return {
         ...state,
-        filters: [...removeFilter(action.filter, state.filters), action.filter],
+        filters: newFilters,
       };
-    case FilterActionType.REMOVE:
+    }
+    case FilterActionType.REMOVE: {
       if (action.filter == null) return state;
+      const newFilters = removeFilter(action.filter, state.filters);
+      logFilterEvent(newFilters, action.type);
       return {
         ...state,
-        filters: removeFilter(action.filter, state.filters),
+        filters: newFilters,
       };
+    }
     case FilterActionType.REMOVE_ALL:
       return initialFilterState;
     default:
