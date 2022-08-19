05df981603f4896d0878775ae4ddb7fbc856426a
Brandon Bodnar <121035+bodnarbm@users.noreply.github.com>
2022-08-19 11:04:58 -0400

    feat: output esl and preapprenticeship as certificateofcompletion
    Until credential engine supports these additional credential types
    we are going to treat them as certificates of completion when
    labeling the credential engine type.

--- backend/data/program-credentials/merge-credentials.py
+++ backend/data/program-credentials/merge-credentials.py
@@ -114,10 +114,12 @@ class CredentialType(Enum):
     # Credential assuring that an organization, program, or awarded credential meets prescribed requirements and may include development and administration of qualifying examinations.
     QualityAssuranceCredential = 'Quality Assurance Credential'
 
-    # TODO: How to handle ESL when it is not a type in CredentialEngine
-    ESL = 'ESL'
-    # TODO: How to handle PreApprenticeship when it is not a type in CredentialEngine
-    PreApprenticeship = 'Pre-Apprenticeship'
+    # We want to seperately detect ESL, but credential enginer does not currently support this type,
+    # until it does, we will treat it the same as CertificateOfCompletion in the output.
+    ESL = CertificateOfCompletion
+    # We want to seperately detect Pre-Apprenticeships, but credential enginer does not currently support this type,
+    # until it does, we will treat it the same as CertificateOfCompletion in the output.
+    PreApprenticeship = CertificateOfCompletion
 
     def __str__(self) -> str:
         return str(self.value)
