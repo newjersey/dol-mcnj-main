ef51fa28844c3cdd3dd0b79e92a462746b7da409
Brandon Bodnar <121035+bodnarbm@users.noreply.github.com>
2022-08-24 21:11:46 -0400

    fix: degree order precedence
    Our labeling for degrees by degree awarded information was running after other labels,
    such as industry certifications, when it should have been running closer to when we were
    labeling degrees based on official name content.
    
    This moves the degree awarded labeling code to be higher in precedence and captures some
    more missing degrees that were being labeled as industry certifications before.

---
+++ backend/migrations/sqls/20220825005529-update-etpl-down.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:da713a51b6abd6980dce2706349e6ff67457320b443d176c0b005a95b88f0825
+size 22839780
---
+++ backend/migrations/sqls/20220825005529-update-etpl-up.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e8d196f944d21535664870fa316310690f7ccfce89cec0f3549d7341b0c35bb
+size 22839798
