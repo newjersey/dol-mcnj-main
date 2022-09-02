20121e910d7aee56f961695f9140ec390a5bb1e1
Brandon Bodnar <bodnarbm@gmail.com>
2022-09-02 09:40:21 -0400

    chore(deps-dev): bump cypress from 9.7.0 to 10.7.0 in /frontend

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on("task", {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+    },
+    baseUrl: "http://localhost:3000",
+  },
+});
