7118380143982185c63b34b1ab2a31582d9ba7ec
Chelsea Kelly-Reif <chelsea.kelly-reif@coforma.io>
2022-10-19 12:21:11 -0700

    Implemented basic working njwds navbar minus logo alignment issue

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
-import React, { ReactElement } from "react";
-import { Icon, } from "@material-ui/core";
+import React, {ReactElement} from "react";
 import njLogo from "../njlogo.svg";
+import navCloseButton from "@newjersey/njwds/dist/img/usa-icons/close.svg"
 import { useTranslation } from "react-i18next";
 import "@newjersey/njwds/dist/css/styles.css";
 import "@newjersey/njwds/dist/js/uswds";
@@ -9,6 +9,7 @@ const COUNSELING_URL = "https://nj.gov/labor/career-services/";
 
 export const Header = (): ReactElement => {
   const { t } = useTranslation();
+
 
   const nav = (): ReactElement => {
     return (
@@ -19,11 +20,12 @@ export const Header = (): ReactElement => {
               <a href="/" className="flex-align-center link-format-black fin fac width-content">
                 <img className="mrd" src={njLogo} alt={t("IconAlt.njLogo")} />
                 <h1 className="text-m bold">{t("Header.title")}</h1>
-              </a>            </div>
+              </a>
+            </div>
             <button className="usa-menu-btn">Menu</button>
           </nav>
           <nav aria-label="Primary navigation" className="usa-nav">
-            <button className="usa-nav__close"><img src="../../dist/img/close.svg" alt="close"/></button>
+            <button className="usa-nav__close"><img src={navCloseButton} alt="close"/></button>
             <ul className="usa-nav__primary usa-accordion">
               <li className="usa-nav__primary-item">
               <a className="usa-nav__link" href="/search"><span>{t("Header.linkToSearch")}</span></a>
@@ -35,7 +37,7 @@ export const Header = (): ReactElement => {
                 <a className="usa-nav__link" href="/funding"><span>{t("Header.linkToFunding")}</span></a>
               </li>
               <li className="usa-nav__primary-item">
-                <a className="usa-nav__link" href={COUNSELING_URL} target={"_blank"} rel={"noreferrer"}><span>{t("Header.linkToCounselingText")}<Icon>launch</Icon></span></a>
+                <a className="usa-nav__link" href={COUNSELING_URL} target={"_blank"} rel={"noreferrer"}><span>{t("Header.linkToCounselingText")}{/*<Icon>launch</Icon>*/}</span></a>
               </li>
             </ul>
           </nav>
