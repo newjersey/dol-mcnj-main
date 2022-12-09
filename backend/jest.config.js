168c30e818348d86c1b57c53abc6f93427c2dad1
Chelsea Kelly-Reif (she/her) <chelsea.kelly-reif@coforma.io>
2022-12-08 17:19:20 -0800

    tell babel-jest to transform axios

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(axios)/)'],
   globalSetup: "<rootDir>/src/globalSetup.js",
   globalTeardown: "<rootDir>/src/globalTeardown.js",
 };
