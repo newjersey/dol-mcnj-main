cf8717ccbcc8676fce1f710cf520f7ea72687b3d
Derek J. Kinsman <derek@unttld.com>
2022-12-16 17:22:23 -0500

    Add front end API hooks.

--- frontend/src/domain/Client.ts
+++ frontend/src/domain/Client.ts
@@ -1,12 +1,14 @@
 import { Training, TrainingResult } from "./Training";
 import { Error } from "./Error";
 import { InDemandOccupation, OccupationDetail } from "./Occupation";
+import { AxiosResponse } from "axios";
 
 export interface Client {
   getTrainingsByQuery: (query: string, observer: Observer<TrainingResult[]>) => void;
   getTrainingById: (id: string, observer: Observer<Training>) => void;
   getInDemandOccupations: (observer: Observer<InDemandOccupation[]>) => void;
   getOccupationDetailBySoc: (soc: string, observer: Observer<OccupationDetail>) => void;
+  getAllCertificates: (query: object, skip: number, take: number, sort: string, cancel: boolean, observer: Observer<AxiosResponse>) => void;
 }
 
 export interface Observer<T> {
