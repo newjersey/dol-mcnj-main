958f22810053c9d2f58e235052913ee8f7214a3f
Derek J. Kinsman <derek@unttld.com>
2022-12-22 17:21:05 -0500

    Add new frontend Interface.

---
+++ frontend/src/domain/CredentialEngine.ts
@@ -0,0 +1,26 @@
+export interface Keypair {
+  key: string,
+  value: string | Keypair | Keypair[],
+}
+
+export interface Certificate {
+  [propName: string]: string | string[] | Keypair | Keypair[],
+}
+
+export interface Extra {
+  Results?: number,
+	RelatedItems?: number,
+	RelatedItemsMap?: number,
+	ResultsMetadata?: number,
+	TotalResults: number,
+	ElapsedMilliseconds: number,
+	Errors: [],
+	Debug?: number,
+}
+
+export interface Certificates {
+  data: Certificate[],
+	valid: boolean,
+	status: string,
+  extra?: Extra,
+}
