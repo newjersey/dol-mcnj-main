42c4c6ac33f1205bf04c47d016b2a56e367749b4
Derek J. Kinsman <derek@unttld.com>
2022-12-22 17:25:18 -0500

    New backend interfaces.

---
+++ backend/src/domain/credentialengine/CredentialEngineInterface.ts
@@ -0,0 +1,37 @@
+export interface CredentialEngineQuery {
+  skip: number,
+  take: number,
+  sort: string,
+  cancel: boolean,
+}
+
+export interface CredentialEngineObject {
+  credentialEngineParams: CredentialEngineQuery,
+}
+
+export interface Keypair {
+  key: string,
+  value: string | Keypair | Keypair[],
+}
+
+export interface Certificate {
+  [propName: string]: string | string[] | Keypair | Keypair[],
+}
+
+export interface Extra {
+  Results?: number,
+	RelatedItems?: number,
+	RelatedItemsMap?: number,
+	ResultsMetadata?: number,
+	TotalResults: number,
+	ElapsedMilliseconds: number,
+	Errors: [],
+	Debug?: number,
+}
+
+export interface Certificates {
+  data: Certificate[],
+	valid: boolean,
+	status: string,
+  extra?: Extra,
+}
