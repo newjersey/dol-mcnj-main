7410845f681e694342ac237e5c71834acec01519
Scott Wambach <scott@scottwamba.ch>
2023-04-05 15:41:11 -0500

    resource items

--- frontend/src/financial-page/FinancialPage.tsx
+++ frontend/src/financial-page/FinancialPage.tsx
@@ -5,8 +5,10 @@ import { Client } from "../domain/Client";
 import { FinancialResourcePageData, FinancialResourcePageProps } from "../types/contentful";
 import { PageBanner } from "../components/PageBanner";
 import { OverlayTool } from "../components/OverlayTool";
-import image from "../overlayImages/Financial Resources.png";
+import image from "../overlayImages/Financial Resources - Mobile.png";
 import { FinancialResourceFilter } from "../components/FinancialResourceFilter";
+import { FinancialResource } from "../components/FinancialResource";
+import { ContentfulRichText } from "../components/ContentfulRichText";
 
 interface Props extends RouteComponentProps {
   client: Client;
@@ -37,8 +39,20 @@ export const FinancialPage = (props: Props): ReactElement => {
   ];
 
   return (
-    <Layout client={props.client}>
-      <OverlayTool img={image} />
+    <Layout
+      client={props.client}
+      footerComponent={
+        data?.page.footerBannerTitle && data?.page.footerBannerCopy ? (
+          <div className="footer-banner">
+            <div className="container">
+              <h3>{data?.page.footerBannerTitle}</h3>
+              <ContentfulRichText document={data?.page.footerBannerCopy?.json} />
+            </div>
+          </div>
+        ) : undefined
+      }
+    >
+      {/* <OverlayTool img={image} /> */}
 
       <PageBanner
         breadCrumbs={breadCrumbs}
@@ -49,7 +63,14 @@ export const FinancialPage = (props: Props): ReactElement => {
       <section className="resource-filter">
         <div className="container">
           <FinancialResourceFilter education={data?.education} funding={data?.funding} />
-          <div className="content">test</div>
+          <div className="content">
+            <div className="heading">
+              <h3>showing 11 out of 11</h3>
+            </div>
+            {data?.resources.items.map((resource) => (
+              <FinancialResource key={resource.sys?.id} {...resource} />
+            ))}
+          </div>
         </div>
       </section>
     </Layout>
