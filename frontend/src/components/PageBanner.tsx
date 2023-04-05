988f2abfde7cbf917c9b86904d0b91a8e523d83d
Scott Wambach <scott@scottwamba.ch>
2023-04-05 09:15:05 -0500

    refactor to use nj web design styles

--- frontend/src/components/PageBanner.tsx
+++ frontend/src/components/PageBanner.tsx
@@ -3,6 +3,7 @@ import { RightArrow } from "../svg/RightArrow";
 import { ContentfulRichText as RichTextProps } from "../types/contentful";
 import { ContentfulRichText } from "./ContentfulRichText";
 import { Icon } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 
 interface PageBannerProps {
   heading: string;
@@ -23,31 +24,43 @@ export const PageBanner = ({
   breadCrumbs,
   subheading,
 }: PageBannerProps) => {
+  const isLoading = heading === "undefined" ? true : false;
+
   return (
     <section className="page-banner">
       <div>
         <div className="copy">
           {breadCrumbs && (
-            <>
-              <ul className="breadcrumbs unstyled">
-                {breadCrumbs.map((crumb, index: number) => (
-                  <Fragment key={crumb.text + index}>
-                    {index > 0 && <RightArrow />}
-                    <li>{crumb.href ? <a href={crumb.href}>{crumb.text}</a> : crumb.text}</li>
-                  </Fragment>
-                ))}
-              </ul>
-              <a className="back" href={breadCrumbs[0].href}>
-                <Icon>arrow_back</Icon>
-                {breadCrumbs[breadCrumbs.length - 1].text}
-              </a>
-            </>
+            <nav className="usa-breadcrumb" aria-label="Breadcrumbs">
+              <ol className="usa-breadcrumb__list">
+                {breadCrumbs.map((crumb, index: number) => {
+                  const isCurrent = breadCrumbs.length - 1 === index;
+                  return (
+                    <li
+                      className={`usa-breadcrumb__list-item${isCurrent ? " usa-current" : ""}`}
+                      aria-current={isCurrent ? "page" : undefined}
+                      key={crumb.text + index}
+                    >
+                      {crumb.href ? (
+                        <a className="usa-breadcrumb__link" href={crumb.href}>
+                          {crumb.text}
+                        </a>
+                      ) : (
+                        <span>{crumb.text}</span>
+                      )}
+                    </li>
+                  );
+                })}
+              </ol>
+            </nav>
           )}
-          <h1>
-            <span>{heading}</span>
-            {svg && !image && svg}
-            {image && !svg && <img src={image} alt={`Icon for ${heading}`} />}
-          </h1>
+          {!isLoading && (
+            <h1>
+              <span>{heading}</span>
+              {svg && !image && svg}
+              {image && !svg && <img src={image} alt={`Icon for ${heading}`} />}
+            </h1>
+          )}
           {subheading && <p>{subheading}</p>}
           {message && (
             <>
@@ -56,8 +69,8 @@ export const PageBanner = ({
           )}
         </div>
         <div className="icon">
-          {svg && !image && svg}
-          {image && !svg && <img src={image} alt={`Icon for ${heading}`} />}
+          {svg && !image && !isLoading && svg}
+          {image && !svg && !isLoading && <img src={image} alt={`Icon for ${heading}`} />}
         </div>
       </div>
     </section>
