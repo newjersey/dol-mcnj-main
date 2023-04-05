3e4b719a89de8a95d27ec54a7a40db2ed95a597f
Scott Wambach <scott@scottwamba.ch>
2023-04-05 16:38:43 -0500

    filter function

--- frontend/src/components/FinancialResourceFilter.tsx
+++ frontend/src/components/FinancialResourceFilter.tsx
@@ -6,9 +6,13 @@ export const FinancialResourceFilter = ({
   education,
   funding,
   className,
+  setActiveTags,
+  activeTags = [],
 }: {
   education?: FinResourceTypeProps;
   funding?: FinResourceTypeProps;
+  setActiveTags: (tags: string[]) => void;
+  activeTags: string[];
   className?: string;
 }) => {
   const [open, setOpen] = useState<boolean>(false);
@@ -41,22 +45,57 @@ export const FinancialResourceFilter = ({
             <legend className="usa-legend">Funding Type</legend>
             {funding?.items.map(({ title, sys }) => (
               <div className="usa-checkbox" key={sys?.id}>
-                <input className="usa-checkbox__input" id={sys?.id} type="checkbox" />
+                <input
+                  className="usa-checkbox__input"
+                  id={sys?.id}
+                  type="checkbox"
+                  onChange={() => {
+                    if (activeTags.includes(`${sys?.id}`)) {
+                      setActiveTags(activeTags.filter((tag) => tag !== sys?.id));
+                    } else {
+                      setActiveTags([...activeTags, `${sys?.id}`]);
+                    }
+                  }}
+                />
                 <label className="usa-checkbox__label" htmlFor={sys?.id}>
                   {title}
                 </label>
               </div>
             ))}
+
             <legend className="usa-legend">Education Type</legend>
             {education?.items.map(({ title, sys }) => (
               <div className="usa-checkbox" key={sys?.id}>
-                <input className="usa-checkbox__input" id={sys?.id} type="checkbox" />
+                <input
+                  className="usa-checkbox__input"
+                  id={sys?.id}
+                  type="checkbox"
+                  onChange={() => {
+                    if (activeTags.includes(`${sys?.id}`)) {
+                      setActiveTags(activeTags.filter((tag) => tag !== sys?.id));
+                    } else {
+                      setActiveTags([...activeTags, `${sys?.id}`]);
+                    }
+                  }}
+                />
                 <label className="usa-checkbox__label" htmlFor={sys?.id}>
                   {title}
                 </label>
               </div>
             ))}
-            <button className="reset">Clear Selection</button>
+            <button
+              className="reset"
+              onClick={(e) => {
+                e.preventDefault();
+                const checkboxes = document.querySelectorAll("input[type=checkbox]");
+                checkboxes.forEach((checkbox) => {
+                  (checkbox as HTMLInputElement).checked = false;
+                });
+                setActiveTags([]);
+              }}
+            >
+              Clear Selection
+            </button>
           </fieldset>
         </form>
       </div>
