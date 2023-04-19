b4d0aafd4ab2a565226d18f341ceb1698c9bdd2a
Scott Wambach <scott@scottwamba.ch>
2023-04-19 12:07:26 -0500

    Revert "Merge pull request #959 from newjersey/ckr/njwe-581/etpl-data-2023-04-18"
    This reverts commit 21c29d2903257d0e99c1a6994a920246eeed30ac, reversing
    changes made to 02339db2f48fcf92ebd33f60a321d346398dd6cd.

--- backend/migrations/sqls/20230418155940-update-etpl-down.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bc4bfa28958a49757222587358bb040118372569bb67e0d8adb86791112a647a
-size 23157018
--- backend/migrations/sqls/20230418155940-update-etpl-up.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:931e8953f694052fd19e9c70ae9f05e12f97e9117ff911fa238eeb7c2c7cb30f
-size 23146607
