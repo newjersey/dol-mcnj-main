a9376c42db9f096a1abb62aa6a9f5e20e4e3f35b
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-05-10 12:39:13 -0700

    refresh in-demand CIPs and SOCs, by request

---
+++ backend/migrations/sqls/20230510184430-update-indemand-cips-socs-up-TEST.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d26a04b593f5998ecb3b1f759e964b2c3598355bb34ed94302b22c2e7038c8bd
+size 161
---
+++ backend/migrations/sqls/20230510184430-update-indemand-cips-socs-up.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d95e4248c81f2a374af49f2bab5fd7305a47b71d0eb404e9318bd8b6d962b60
+size 118396
