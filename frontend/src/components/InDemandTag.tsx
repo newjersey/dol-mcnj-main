4a9cd8ef4800d2ee73a94270b693302c2b38bf64
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-05-17 16:52:34 -0700

    locally in-demand county tag on training pages

--- frontend/src/components/InDemandTag.tsx
+++ frontend/src/components/InDemandTag.tsx
@@ -4,15 +4,32 @@ import { InlineIcon } from "./InlineIcon";
 
 interface Props {
   className?: string;
+  counties?: string[];
 }
 
 export const InDemandTag = (props: Props): ReactElement => {
   const { t } = useTranslation();
 
+  // Helper function to format array into human-readable string
+  const formatCounties = (counties: string[] = []) => {
+    if(counties.length === 0) return '';
+    if(counties.length === 1) return counties[0];
+
+    const lastCounty = counties.pop();
+    return `${counties.join(', ')}, and ${lastCounty}`;
+  };
+
+  // Formatted counties string
+  const countiesString = formatCounties(props.counties);
+
   return (
-    <span className={`fin fas bg-orange-tag tag pvxxs phd mrs ${props.className}`}>
+      <span className={`fin fas bg-orange-tag tag pvxxs phd mrs ${props.className}`}>
       <InlineIcon className="mrxs">local_fire_department</InlineIcon>
-      {t("SearchResultsPage.inDemandTag")}
+        {
+          countiesString
+              ? t("SearchResultsPage.inDemandCountiesTag", {counties: countiesString})
+              : t("SearchResultsPage.inDemandTag")
+        }
     </span>
   );
 };
