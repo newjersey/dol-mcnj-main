1dad426331f2fa28e07aabafeb33339cb748cc3a
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-05-18 18:04:21 -0700

    added error handling and logging

--- backend/src/domain/search/searchTrainings.ts
+++ backend/src/domain/search/searchTrainings.ts
@@ -6,57 +6,63 @@ import { SearchClient } from "./SearchClient";
 import { Selector } from "../training/Selector";
 
 export const searchTrainingsFactory = (
-  findTrainingsBy: FindTrainingsBy,
-  searchClient: SearchClient
+    findTrainingsBy: FindTrainingsBy,
+    searchClient: SearchClient
 ): SearchTrainings => {
   return async (searchQuery: string): Promise<TrainingResult[]> => {
-    const searchResults = await searchClient.search(searchQuery);
-    const trainings = await findTrainingsBy(
-      Selector.ID,
-      searchResults.map((it) => it.id)
-    );
+    try {
+      console.log(`Starting search for: ${searchQuery}`);
+      const searchResults = await searchClient.search(searchQuery);
+      const trainings = await findTrainingsBy(
+          Selector.ID,
+          searchResults.map((it) => it.id)
+      );
 
-    return Promise.all(
-      trainings.map(async (training: Training) => {
-        let highlight = "";
-        let rank = 0;
+      return await Promise.all(
+          trainings.map(async (training: Training) => {
+            let highlight = "";
+            let rank = 0;
 
-        if (searchQuery) {
-          highlight = await searchClient.getHighlight(training.id, searchQuery);
-        }
+            if (searchQuery) {
+              highlight = await searchClient.getHighlight(training.id, searchQuery);
+            }
 
-        if (searchResults) {
-          const foundRank = searchResults.find((it) => it.id === training.id)?.rank;
-          if (foundRank) {
-            rank = foundRank;
-          }
-        }
+            if (searchResults) {
+              const foundRank = searchResults.find((it) => it.id === training.id)?.rank;
+              if (foundRank) {
+                rank = foundRank;
+              }
+            }
 
-        return {
-          id: training.id,
-          name: training.name,
-          cipCode: training.cipCode,
-          totalCost: training.totalCost,
-          percentEmployed: training.percentEmployed,
-          calendarLength: training.calendarLength,
-          localExceptionCounty: training.localExceptionCounty,
-          online: training.online,
-          providerId: training.provider.id,
-          providerName: training.provider.name,
-          city: training.provider.address.city,
-          zipCode: training.provider.address.zipCode,
-          county: training.provider.county,
-          inDemand: training.inDemand,
-          highlight: stripUnicode(highlight),
-          rank: rank,
-          socCodes: training.occupations.map((o) => o.soc),
-          hasEveningCourses: training.hasEveningCourses,
-          languages: training.languages,
-          isWheelchairAccessible: training.isWheelchairAccessible,
-          hasJobPlacementAssistance: training.hasJobPlacementAssistance,
-          hasChildcareAssistance: training.hasChildcareAssistance,
-        };
-      })
-    );
+            return {
+              id: training.id,
+              name: training.name,
+              cipCode: training.cipCode,
+              totalCost: training.totalCost,
+              percentEmployed: training.percentEmployed,
+              calendarLength: training.calendarLength,
+              localExceptionCounty: training.localExceptionCounty,
+              online: training.online,
+              providerId: training.provider.id,
+              providerName: training.provider.name,
+              city: training.provider.address.city,
+              zipCode: training.provider.address.zipCode,
+              county: training.provider.county,
+              inDemand: training.inDemand,
+              highlight: stripUnicode(highlight),
+              rank: rank,
+              socCodes: training.occupations.map((o) => o.soc),
+              hasEveningCourses: training.hasEveningCourses,
+              languages: training.languages,
+              isWheelchairAccessible: training.isWheelchairAccessible,
+              hasJobPlacementAssistance: training.hasJobPlacementAssistance,
+              hasChildcareAssistance: training.hasChildcareAssistance,
+            };
+          })
+      );
+    } catch (error) {
+      console.error(`Failed to search for trainings with the query: ${searchQuery}`, error);
+      throw error;
+    }
   };
 };
