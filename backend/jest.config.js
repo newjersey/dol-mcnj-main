92b10848061b4ad5937435bb8c8c1bf9a9ecc647
Scott Wambach <scott@scottwamba.ch>
2023-05-22 12:21:06 -0500

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,4 +3,10 @@ module.exports = {
   testEnvironment: "node",
   globalSetup: "<rootDir>/src/globalSetup.js",
   globalTeardown: "<rootDir>/src/globalTeardown.js",
+  moduleNameMapper: {
+    "next/router": "<rootDir>/__mocks__/next/router.js",
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$": "<rootDir>/__mocks__/file-mock.js",
+    "react-markdown": "<rootDir>/node_modules/react-markdown/react-markdown.min.js",
+  },
 };
