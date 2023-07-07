90c502b355e65aa17e99e136ce2e6143448221ad
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-07-06 19:00:20 -0700

    debug a11y vio

--- frontend/cypress/e2e/career-support-page.cy.js
+++ frontend/cypress/e2e/career-support-page.cy.js
@@ -2,12 +2,10 @@ describe("Career Support Page", () => {
   it("is accessible", () => {
     cy.visit("/support-resources/career-support");
     cy.injectAxe();
-
     cy.contains("Career Guidance, Job Search Help, and Re-entering the Workforce").should("exist");
-    cy.checkA11y();
     cy.checkA11y(null, null, (violations) => {
-      console.log('Accessibility violations found:');
-      console.table(violations)
+      cy.task('log', 'Accessibility violations found:');
+      cy.task('table', violations);
     });
   });
 });
