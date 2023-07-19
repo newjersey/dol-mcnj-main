106819758998d3701639a636f0eecee37be4879b
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-07-18 18:22:31 -0700

    fixed some tests

--- frontend/src/components/BetaBanner.test.tsx
+++ frontend/src/components/BetaBanner.test.tsx
@@ -5,13 +5,13 @@ import { BetaBanner } from "./BetaBanner";
 describe("<BetaBanner />", () => {
   it("accepts a prop for removing the header margin", () => {
     const subject = render(<BetaBanner noHeader={true} />);
-    const banner = subject.getByText("beta", { exact: false });
+    const banner = subject.getByText("Share your feedback!", { exact: false });
     expect(banner).toHaveClass("no-header-margin");
   });
 
   it("uses default margin if no header prop passed", () => {
     const subject = render(<BetaBanner />);
-    const banner = subject.getByText("beta", { exact: false });
+    const banner = subject.getByText("Share your feedback!", { exact: false });
     expect(banner).not.toHaveClass("no-header-margin");
   });
 });
