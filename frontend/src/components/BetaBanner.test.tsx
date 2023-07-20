9ad33a1f249faa0016f291db6f89bccd2483f509
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-07-20 08:47:37 -0700

    get banner by parentElement

--- frontend/src/components/BetaBanner.test.tsx
+++ frontend/src/components/BetaBanner.test.tsx
@@ -5,7 +5,7 @@ import { BetaBanner } from "./BetaBanner";
 describe("<BetaBanner />", () => {
   it("accepts a prop for removing the header margin", () => {
     const subject = render(<BetaBanner noHeader={true} />);
-    const banner = subject.getByText("feedback", { exact: false });
+    const banner = subject.getByText("feedback", { exact: false }).parentElement;
     expect(banner).toHaveClass("no-header-margin");
   });
 });
