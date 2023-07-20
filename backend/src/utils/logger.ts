cac21c3f507ab6605c4b31ebf0619103d48b18db
Lesly Revenge <lrevenge@fm-n20mc4kpx0.lan>
2023-07-20 15:01:14 -0400

    remove timestamp for local logs

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv'
 import { createLogger, transports, format } from "winston";
-import Sentry from 'winston-sentry-log';
 
 dotenv.config()
 
@@ -8,32 +7,22 @@ const { NODE_ENV, SENTRY_DSN } = process.env;
 const payload = [];
 
 if (NODE_ENV == "production" && SENTRY_DSN) {
-  const options = {
-    config: {
-      dsn: SENTRY_DSN
-    },
-    level: "info"
-  };
-
-  payload.push(
-    new Sentry(options)
-  );
-} else {
-  // add to sentry if in production
   payload.push(
     new transports.Console({
       level: "debug",
       format: format.combine(format.timestamp(), format.json()),
     })
   );
+} else {
+  // add to sentry if in production
+  payload.push(
+    new transports.Console({
+      level: "info",
+      format: format.json()
+    })
+  );
 }
 
-payload.push(
-  new transports.Console({
-    level: "debug",
-    format: format.combine(format.timestamp(), format.json()),
-  })
-);
 
 const logger = createLogger({
   transports: payload,
