0cf294f97720b2eae88a476b0f80159c7f19d566
Lesly Revenge <lrevenge@fm-n20mc4kpx0.lan>
2023-07-20 13:56:23 -0400

    fix variable prefix error

--- backend/src/utils/global.ts
+++ backend/src/utils/global.ts
@@ -5,7 +5,7 @@ import logger from './logger'
 (() => {
   console.log = function (message, status = "info") {
     // the available status
-    let status_dict = ["error", "warn", "info", "http", "verbose", "debug"];
+    const status_dict = ["error", "warn", "info", "http", "verbose", "debug"];
     // if status is not part of the availability, change it to info
     if (!status_dict.includes(status)) {
       status = "info";
