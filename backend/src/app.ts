b8da1f36bf741b049ae543bab0413a5a30fd640e
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-07-31 14:30:16 -0700

    split dev and test into reader/writer

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,9 @@
 import * as dotenv from "dotenv";
 import * as Sentry from "@sentry/node";
 import express, { Request, Response } from "express";
-import path from "path";
 import cors from "cors";
 import { routerFactory } from "./routes/router";
 import { PostgresDataClient } from "./database/data/PostgresDataClient";
-import { PostgresSearchClient } from "./database/search/PostgresSearchClient";
 import { findTrainingsByFactory } from "./domain/training/findTrainingsBy";
 import { searchTrainingsFactory } from "./domain/search/searchTrainings";
 import { getInDemandOccupationsFactory } from "./domain/occupations/getInDemandOccupations";
@@ -14,27 +12,21 @@ import { OnetClient } from "./oNET/OnetClient";
 import { getEducationTextFactory } from "./domain/occupations/getEducationText";
 import { getSalaryEstimateFactory } from "./domain/occupations/getSalaryEstimate";
 import { CareerOneStopClient } from "./careeronestop/CareerOneStopClient";
-import * as databaseConfig from '../database.json';
+import fs from "fs";
+import path from "path";
+import {PostgresSearchClient} from "./database/search/PostgresSearchClient";
 
 dotenv.config();
-// console.log(process.env);
 
 const app = express();
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   integrations: [
-    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
     new Sentry.Integrations.Express({ app }),
-    // Automatically instrument Node.js libraries and frameworks
     ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
   ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
 
@@ -46,37 +38,34 @@ process.on('unhandledRejection', (reason) => {
   Sentry.captureException(reason);
 });
 
-// RequestHandler creates a separate execution context, so that all
-// transactions/spans/breadcrumbs are isolated across requests
 app.use(Sentry.Handlers.requestHandler());
-// TracingHandler creates a trace for every incoming request
 app.use(Sentry.Handlers.tracingHandler());
 
-const env = process.env.NODE_ENV || 'dev';
-const dbConfig = databaseConfig[env];
+// Load DB configurations from JSON file
+const dbConfigPath = path.join(__dirname, "..", "database.json");
+const dbConfigs = JSON.parse(fs.readFileSync(dbConfigPath, "utf-8"));
+const dbConfig = dbConfigs[process.env.NODE_ENV || "dev"];
 
-const dbWriterConfig = dbConfig.writer;
-const dbReaderConfig = dbConfig.reader;
-
-const connectionWriter = {
-  user: process.env[dbWriterConfig.user] || dbWriterConfig.user,
-  host: process.env[dbWriterConfig.host.ENV] || dbWriterConfig.host,
-  database: dbWriterConfig.database,
-  password: process.env[dbWriterConfig.password.ENV] || dbWriterConfig.password,
+// Define connection settings for reader and writer
+const readerConfig = {
+  driver: dbConfig.reader.driver,
+  user: dbConfig.reader.user,
+  host: process.env[dbConfig.reader.host.ENV],
+  database: dbConfig.reader.database,
+  password: process.env[dbConfig.reader.password.ENV],
   port: 5432,
 };
 
-const connectionReader = {
-  user: process.env[dbReaderConfig.user] || dbReaderConfig.user,
-  host: process.env[dbReaderConfig.host.ENV] || dbReaderConfig.host,
-  database: dbReaderConfig.database,
-  password: process.env[dbReaderConfig.password.ENV] || dbReaderConfig.password,
+/*const writerConfig = {
+  user: dbConfig.writer.user,
+  host: process.env[dbConfig.writer.host.ENV],
+  database: dbConfig.writer.database,
+  password: process.env[dbConfig.writer.password.ENV],
   port: 5432,
-};
+};*/
 
 const isCI = process.env.IS_CI;
 
-// default external api values
 const apiValues = {
   onetBaseUrl: "http://localhost:8090",
   onetAuth: {
@@ -88,8 +77,6 @@ const apiValues = {
   careerOneStopAuthToken: "CAREER_ONESTOP_AUTH_TOKEN",
 };
 
-// try to update to use env vars in all cases EXCEPT running feature tests in CI
-// because in CI,f we want to use wiremock jsons, not the real APIs
 if (!isCI) {
   apiValues.onetBaseUrl = process.env.ONET_BASEURL || "http://localhost:8090";
   apiValues.onetAuth = {
@@ -100,32 +87,33 @@ if (!isCI) {
   apiValues.careerOneStopBaseUrl = process.env.CAREER_ONESTOP_BASEURL || "http://localhost:8090";
   apiValues.careerOneStopUserId = process.env.CAREER_ONESTOP_USERID || "CAREER_ONESTOP_USERID";
   apiValues.careerOneStopAuthToken =
-    process.env.CAREER_ONESTOP_AUTH_TOKEN || "CAREER_ONESTOP_AUTH_TOKEN";
+      process.env.CAREER_ONESTOP_AUTH_TOKEN || "CAREER_ONESTOP_AUTH_TOKEN";
 }
 
-const postgresDataClient = new PostgresDataClient(connectionWriter);
-const postgresSearchClient = new PostgresSearchClient(connectionReader);
-const findTrainingsBy = findTrainingsByFactory(postgresDataClient);
+const postgresDataClientReader = new PostgresDataClient(readerConfig);
+console.log(JSON.stringify(readerConfig));
+const postgresSearchClientReader = new PostgresSearchClient(readerConfig);
+const findTrainingsBy = findTrainingsByFactory(postgresDataClientReader);
 
 const router = routerFactory({
-  searchTrainings: searchTrainingsFactory(findTrainingsBy, postgresSearchClient),
+  searchTrainings: searchTrainingsFactory(findTrainingsBy, postgresSearchClientReader),
   findTrainingsBy: findTrainingsBy,
-  getInDemandOccupations: getInDemandOccupationsFactory(postgresDataClient),
+  getInDemandOccupations: getInDemandOccupationsFactory(postgresDataClientReader),
   getOccupationDetail: getOccupationDetailFactory(
-    OnetClient(
-      apiValues.onetBaseUrl,
-      apiValues.onetAuth,
-      postgresDataClient.find2018OccupationsBySoc2010
-    ),
-    getEducationTextFactory(postgresDataClient),
-    getSalaryEstimateFactory(postgresDataClient),
-    CareerOneStopClient(
-      apiValues.careerOneStopBaseUrl,
-      apiValues.careerOneStopUserId,
-      apiValues.careerOneStopAuthToken
-    ),
-    findTrainingsBy,
-    postgresDataClient
+      OnetClient(
+          apiValues.onetBaseUrl,
+          apiValues.onetAuth,
+          postgresDataClientReader.find2018OccupationsBySoc2010
+      ),
+      getEducationTextFactory(postgresDataClientReader),
+      getSalaryEstimateFactory(postgresDataClientReader),
+      CareerOneStopClient(
+          apiValues.careerOneStopBaseUrl,
+          apiValues.careerOneStopUserId,
+          apiValues.careerOneStopAuthToken
+      ),
+      findTrainingsBy,
+      postgresDataClientReader
   ),
 });
 
@@ -142,7 +130,6 @@ app.get("*", (req: Request, res: Response) => {
 
 app.use(cors());
 
-// The error handler must be before any other error middleware and after all controllers
 app.use(Sentry.Handlers.errorHandler());
 
 export default app;
