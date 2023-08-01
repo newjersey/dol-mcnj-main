2b43796f48bc589d82e081d8bd582cf3550ce6b2
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-07-31 21:14:33 -0700

    fixing up various things

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import * as dotenv from "dotenv";
 import * as Sentry from "@sentry/node";
 import express, { Request, Response } from "express";
+import path from "path";
 import cors from "cors";
 import { routerFactory } from "./routes/router";
 import { PostgresDataClient } from "./database/data/PostgresDataClient";
+import { PostgresSearchClient } from "./database/search/PostgresSearchClient";
 import { findTrainingsByFactory } from "./domain/training/findTrainingsBy";
 import { searchTrainingsFactory } from "./domain/search/searchTrainings";
 import { getInDemandOccupationsFactory } from "./domain/occupations/getInDemandOccupations";
@@ -12,24 +14,27 @@ import { OnetClient } from "./oNET/OnetClient";
 import { getEducationTextFactory } from "./domain/occupations/getEducationText";
 import { getSalaryEstimateFactory } from "./domain/occupations/getSalaryEstimate";
 import { CareerOneStopClient } from "./careeronestop/CareerOneStopClient";
-import fs from "fs";
-import path from "path";
-import {PostgresSearchClient} from "./database/search/PostgresSearchClient";
+import { contentfulFactory } from "./domain/contentful/getContentful";
 
 dotenv.config();
+// console.log(process.env);
 
 const app = express();
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   integrations: [
+    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
     new Sentry.Integrations.Express({ app }),
+    // Automatically instrument Node.js libraries and frameworks
     ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
   ],
   tracesSampleRate: 1.0,
 });
 
+// Error handling for uncaught exceptions and unhandled rejections...
 process.on('uncaughtException', function (exception) {
   Sentry.captureException(exception);
 });
@@ -38,34 +43,69 @@ process.on('unhandledRejection', (reason) => {
   Sentry.captureException(reason);
 });
 
+// RequestHandler and TracingHandler configuration...
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 
-// Load DB configurations from JSON file
-const dbConfigPath = path.join(__dirname, "..", "database.json");
-const dbConfigs = JSON.parse(fs.readFileSync(dbConfigPath, "utf-8"));
-const dbConfig = dbConfigs[process.env.NODE_ENV || "dev"];
+// Determine if the NODE_ENV begins with "aws"
 
-// Define connection settings for reader and writer
-const readerConfig = {
-  driver: dbConfig.reader.driver,
-  user: dbConfig.reader.user,
-  host: process.env[dbConfig.reader.host.ENV],
-  database: dbConfig.reader.database,
-  password: process.env[dbConfig.reader.password.ENV],
-  port: 5432,
-};
 
-/*const writerConfig = {
-  user: dbConfig.writer.user,
-  host: process.env[dbConfig.writer.host.ENV],
-  database: dbConfig.writer.database,
-  password: process.env[dbConfig.writer.password.ENV],
-  port: 5432,
-};*/
+let connection: any = null;
+
+switch (process.env.NODE_ENV) {
+  case "dev":
+    connection = {
+      user: "postgres",
+      host: process.env.DB_HOST_DEV,
+      database: "d4adlocal",
+      password: process.env.DB_PASS_DEV,
+      port: 5432,
+    };
+    break;
+  case "test":
+    connection = {
+      user: "postgres",
+      host: process.env.DB_HOST_TEST,
+      database: "d4adtest",
+      password: process.env.DB_PASS_TEST,
+      port: 5432,
+    };
+    break;
+  case "awsdev":
+    connection = {
+      user: "postgres",
+      host: process.env.DB_HOST_WRITER_DEV,
+      database: "d4addev",
+      password: process.env.DB_PASS_DEV,
+      port: 5432,
+    };
+    break;
+  case "awstest":
+    connection = {
+      user: "postgres",
+      host: process.env.DB_HOST_WRITER_TEST,
+      database: "d4adtest",
+      password: process.env.DB_PASS_TEST,
+      port: 5432,
+    };
+    break;
+  case "awsprod":
+    connection = {
+      user: "postgres",
+      host: process.env.DB_HOST_WRITER_AWSPROD,
+      database: "d4adprod",
+      password: process.env.DB_PASS_AWSPROD,
+      port: 5432,
+    };
+    break;
+  default:
+    console.error("Invalid NODE_ENV. Please set NODE_ENV to one of: dev, test, awsdev, awstest, awsprod.");
+    process.exit(1);
+}
 
 const isCI = process.env.IS_CI;
 
+// Default external API values
 const apiValues = {
   onetBaseUrl: "http://localhost:8090",
   onetAuth: {
@@ -77,52 +117,62 @@ const apiValues = {
   careerOneStopAuthToken: "CAREER_ONESTOP_AUTH_TOKEN",
 };
 
+// Update external API values if not running in CI environment
 if (!isCI) {
   apiValues.onetBaseUrl = process.env.ONET_BASEURL || "http://localhost:8090";
   apiValues.onetAuth = {
     username: process.env.ONET_USERNAME || "ONET_USERNAME",
     password: process.env.ONET_PASSWORD || "ONET_PASSWORD",
   };
-
   apiValues.careerOneStopBaseUrl = process.env.CAREER_ONESTOP_BASEURL || "http://localhost:8090";
   apiValues.careerOneStopUserId = process.env.CAREER_ONESTOP_USERID || "CAREER_ONESTOP_USERID";
   apiValues.careerOneStopAuthToken =
       process.env.CAREER_ONESTOP_AUTH_TOKEN || "CAREER_ONESTOP_AUTH_TOKEN";
 }
 
-const postgresDataClientReader = new PostgresDataClient(readerConfig);
-console.log(JSON.stringify(readerConfig));
-const postgresSearchClientReader = new PostgresSearchClient(readerConfig);
-const findTrainingsBy = findTrainingsByFactory(postgresDataClientReader);
+const postgresDataClient = new PostgresDataClient(connection);
+const postgresSearchClient = new PostgresSearchClient(connection);
+const findTrainingsBy = findTrainingsByFactory(postgresDataClient);
 
 const router = routerFactory({
-  searchTrainings: searchTrainingsFactory(findTrainingsBy, postgresSearchClientReader),
+  getContentfulCPW: contentfulFactory("cpw"),
+  getContentfulFRP: contentfulFactory("frp"),
+  getContentfulFootNav2: contentfulFactory("footNav2"),
+  getContentfulFootNav: contentfulFactory("footNav"),
+  getContentfulMNav: contentfulFactory("mnav"),
+  getContentfulGNav: contentfulFactory("gnav"),
+  getContentfulTPR: contentfulFactory("tpr"),
+  getContentfulFAQ: contentfulFactory("faq"),
+  searchTrainings: searchTrainingsFactory(findTrainingsBy, postgresSearchClient),
   findTrainingsBy: findTrainingsBy,
-  getInDemandOccupations: getInDemandOccupationsFactory(postgresDataClientReader),
+  getInDemandOccupations: getInDemandOccupationsFactory(postgresDataClient),
   getOccupationDetail: getOccupationDetailFactory(
       OnetClient(
           apiValues.onetBaseUrl,
           apiValues.onetAuth,
-          postgresDataClientReader.find2018OccupationsBySoc2010
+          postgresDataClient.find2018OccupationsBySoc2010
       ),
-      getEducationTextFactory(postgresDataClientReader),
-      getSalaryEstimateFactory(postgresDataClientReader),
+      getEducationTextFactory(postgresDataClient),
+      getSalaryEstimateFactory(postgresDataClient),
       CareerOneStopClient(
           apiValues.careerOneStopBaseUrl,
           apiValues.careerOneStopUserId,
           apiValues.careerOneStopAuthToken
       ),
       findTrainingsBy,
-      postgresDataClientReader
+      postgresDataClient
   ),
 });
 
 app.use(express.static(path.join(__dirname, "build"), { etag: false, lastModified: false }));
 app.use("/api", router);
+
+// Routes for handling root and unknown routes...
 app.get("/", (req: Request, res: Response) => {
   res.setHeader("Cache-Control", "no-cache");
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 app.get("*", (req: Request, res: Response) => {
   res.setHeader("Cache-Control", "no-cache");
   res.sendFile(path.join(__dirname, "build", "index.html"));
@@ -130,6 +180,7 @@ app.get("*", (req: Request, res: Response) => {
 
 app.use(cors());
 
+// Error handler for Sentry...
 app.use(Sentry.Handlers.errorHandler());
 
 export default app;
--- scripts/db-migrate.sh
+++ scripts/db-migrate.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
 cd $(git rev-parse --show-toplevel)
+ENV=${NODE_ENV}
+HOST_ENV_VAR=$(jq -r ".${ENV}.writer.host.ENV" backend/database.json)
+PASSWORD_ENV_VAR=$(jq -r ".${ENV}.writer.password.ENV" backend/database.json)
+DB_NAME=$(jq -r ".${ENV}.writer.database" backend/database.json)
+
+# Debug: Print the environment variables' names
+echo "Host env var: $HOST_ENV_VAR"
+echo "Password env var: $PASSWORD_ENV_VAR"
+echo "Database name: $DB_NAME"
+
+# Fetch the actual values from the environment variables
+DB_HOST=${!HOST_ENV_VAR}
+DB_PASSWORD=${!PASSWORD_ENV_VAR}
+
+# Debug: Print the actual values fetched from the environment
+echo "Database host: $DB_HOST"
+echo "Database password: $DB_PASSWORD"
+
+# Construct the PostgreSQL URL
+DATABASE_URL="postgresql://postgres:$DB_PASSWORD@$DB_HOST:5432/$DB_NAME"
+
+echo "Database URL: $DATABASE_URL"
+
+# Export the DATABASE_URL environment variable
+export DATABASE_URL
+
 npm --prefix=backend run db-migrate up
