d6bb380c0be6272bf472fee710aeee31b5d454ff
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-07-31 18:06:31 -0700

    swap exec() with spawnPromise()

--- backend/src/globalSetup.js
+++ backend/src/globalSetup.js
@@ -1,8 +1,29 @@
-const util = require("util");
-const {exec} = require("child_process");
-const cmd = util.promisify(exec);
+const { spawn } = require('child_process');
+
+// This function wraps the spawn method in a Promise, allowing it to be used with async/await.
+function spawnPromise(command, args) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args);
+
+    child.stdout.on('data', (chunk) => {
+      console.log(chunk.toString());
+    });
+
+    child.stderr.on('data', (chunk) => {
+      console.error(chunk.toString());
+    });
+
+    child.on('exit', (code) => {
+      if(code !== 0) {
+        reject(new Error(`${command} exited with code ${code}`));
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
 module.exports = async () => {
-  await cmd("psql -c 'create database d4adtest;' -U postgres -h localhost -p 5432");
-  await cmd("npm run db-migrate up -- -e test");
+  await spawnPromise('psql', ['-c', 'create database d4adtest;', '-U', 'postgres', '-h', 'localhost', '-p', '5432']);
+  await spawnPromise('npm', ['run', 'db-migrate', 'up', '--', '-e', 'test']);
 };
