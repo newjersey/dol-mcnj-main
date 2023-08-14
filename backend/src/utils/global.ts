1ad31c43558857bd85c7e5dd79a348d2a99c33a4
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-08-14 15:49:12 -0700

    creating separate sentryLogger and cloudwatchLogger, tying them together in global.ts

--- backend/src/utils/global.ts
+++ backend/src/utils/global.ts
@@ -1,16 +1,57 @@
-import logger from './logger'
+import cloudwatchLogger from './cloudwatchLogger';
+import sentryLogger from './sentryLogger';
 
-// change global console.log to winston logger type
+(function overrideConsoleMethods() {
+  const statusDict: Array<string> = ["error", "warn", "info", "http", "verbose", "debug"];
 
-(() => {
-  console.log = function (message, status = "info") {
-    // the available status
-    const status_dict = ["error", "warn", "info", "http", "verbose", "debug"];
-    // if status is not part of the availability, change it to info
-    if (!status_dict.includes(status)) {
+  /* We're preserving the original console.log functionality just in case there are log messages that don't fit into
+  the predefined statuses. This helps in making sure we don't unintentionally suppress important log messages. */
+  const originalConsoleLog = console.log;
+
+  console.log = function (message: string, status = "info") {
+    if (statusDict.indexOf(status) !== -1) {
       status = "info";
     }
-    logger.log(status, message);
+
+    switch (status) {
+      case "error":
+        cloudwatchLogger.error(message);
+        sentryLogger.error(message);
+        break;
+      case "warn":
+        cloudwatchLogger.warn(message);
+        break;
+      case "info":
+        cloudwatchLogger.info(message);
+        break;
+      case "http":
+        cloudwatchLogger.log('http', message);
+        break;
+      case "debug":
+      case "verbose":
+        cloudwatchLogger.debug(message);
+        break;
+    }
+  };
+
+  console.info = function (...args) {
+    const message = args.join(' ');
+    cloudwatchLogger.info(message);
+  };
+
+  console.warn = function (...args) {
+    const message = args.join(' ');
+    cloudwatchLogger.warn(message);
+  };
+
+  console.error = function (...args) {
+    const message = args.join(' ');
+    cloudwatchLogger.error(message);
+    sentryLogger.error(message);
+  };
+
+  console.debug = function (...args) {
+    const message = args.join(' ');
+    cloudwatchLogger.debug(message);
   };
 })();
-
