a51a7d6dbece0487dfafcd99a41d551102ba83b1
Chelsea Kelly-Reif <chelsea.kelly-reif@coforma.io>
2023-08-15 18:01:14 -0700

    Update cloudwatchLogger.ts

--- backend/src/utils/cloudwatchLogger.ts
+++ backend/src/utils/cloudwatchLogger.ts
@@ -1,15 +1,35 @@
-import dotenv from 'dotenv'
-import { createLogger, transports, format } from "winston";
+import dotenv from 'dotenv';
+import { createLogger, transports, format } from 'winston';
 import WinstonCloudWatch from 'winston-cloudwatch';
 
-dotenv.config()
+dotenv.config();
+
+// Determine log group and stream based on NODE_ENV
+let logGroupName: string;
+let logStreamName: string;
+
+switch (process.env.NODE_ENV) {
+    case 'production':
+        logGroupName = process.env.PROD_LOG_GROUP_NAME || '';
+        logStreamName = process.env.PROD_LOG_STREAM_NAME || '';
+        break;
+    case 'test':
+        logGroupName = process.env.TEST_LOG_GROUP_NAME || '';
+        logStreamName = process.env.TEST_LOG_STREAM_NAME || '';
+        break;
+    case 'development':
+    default:
+        logGroupName = process.env.DEV_LOG_GROUP_NAME || '';
+        logStreamName = process.env.DEV_LOG_STREAM_NAME || '';
+        break;
+}
 
 const cloudwatchLogger = createLogger({
     format: format.json(),
     transports: [
         new WinstonCloudWatch({
-            logGroupName: process.env.LOG_GROUP_NAME,
-            logStreamName: process.env.LOG_STREAM_NAME,
+            logGroupName: logGroupName,
+            logStreamName: logStreamName,
             awsRegion: process.env.AWS_REGION,
             createLogGroup: true,
             createLogStream: true,
@@ -17,4 +37,4 @@ const cloudwatchLogger = createLogger({
     ],
 });
 
-export default cloudwatchLogger
+export default cloudwatchLogger;
