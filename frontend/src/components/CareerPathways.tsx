bdcc859ffabf87e3f3f2c85712d7dad826971689
Scott Wambach <scott@scottwamba.ch>
2023-08-18 16:00:28 -0500

    pathways styles and organization

--- frontend/src/components/CareerPathways.tsx
+++ frontend/src/components/CareerPathways.tsx
@@ -36,6 +36,10 @@ export const CareerPathways = ({
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
+    setFieldChanged(true);
+  }, [paths]);
+
+  useEffect(() => {
     const stored = localStorage.getItem("occupation");
     const pathItems = localStorage.getItem("pathItems");
     if (stored) {
@@ -44,6 +48,9 @@ export const CareerPathways = ({
 
     if (pathItems) {
       setPaths(JSON.parse(pathItems));
+      setTimeout(() => {
+        setFieldChanged(false);
+      }, 100);
     }
   }, []);
 
@@ -55,10 +62,6 @@ export const CareerPathways = ({
     pathway: details.title || "",
   };
 
-  useEffect(() => {
-    setFieldChanged(true);
-  }, [paths]);
-
   return (
     <div className="career-pathways">
       <div className="container plus">
@@ -68,101 +71,103 @@ export const CareerPathways = ({
             heading={`Select a ${industry} Field`}
             description="Select a field and explore different career pathways or click the tool tip to learn more about it."
           />
-          <div className="groups">
-            <p>
-              <strong>Select a {industry} Field</strong>
-            </p>
-            <div className="selections">
-              {careerMaps.map((map, index) => (
-                <PathwayGroup
-                  key={map.sys.id}
-                  {...map}
-                  icon={icon}
-                  selected={selected.id ? selected : localData || {}}
-                  setSelected={setSelected}
-                  active={details?.groupId === map.sys.id || index === 0}
-                  activeGroup={details?.groupId === map.sys.id}
-                  setPaths={setPaths}
-                  setOpen={setOpen}
-                />
-              ))}
-            </div>
+          <p>
+            <strong>Select a {industry} Field</strong>
+          </p>
+          <div className="selections">
+            {careerMaps.map((map, index) => (
+              <PathwayGroup
+                key={map.sys.id}
+                {...map}
+                icon={icon}
+                selected={selected.id ? selected : localData || {}}
+                setSelected={setSelected}
+                active={details?.groupId === map.sys.id}
+                activeGroup={details?.groupId === map.sys.id}
+                setPaths={setPaths}
+                setOpen={setOpen}
+              />
+            ))}
           </div>
         </div>
 
-        <div className="select">
-          <p>
-            <strong>Select an Occupation in {paths?.listTitle}</strong>
-          </p>
+        <div className="groups">
+          <SectionHeading
+            headingLevel={3}
+            heading={`Explore  ${paths?.listTitle} Occupations and Pathways`}
+            description="Explore related occupations and learn important details."
+          />
 
-          <button
-            type="button"
-            aria-label="occupation-selector"
-            className="select-button"
-            onClick={() => {
-              setOpen(!open);
-            }}
-          >
-            {!fieldChanged ? details?.shortTitle || details?.title || "---" : "---"}
-          </button>
+          <div className="select">
+            <button
+              type="button"
+              aria-label="occupation-selector"
+              className="select-button"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              {!fieldChanged ? details?.shortTitle || details?.title || "---" : "---"}
+            </button>
 
-          {open && (
-            <div className="dropdown-select">
-              {paths?.items.map((path) => (
-                <div key={path.sys.id}>
-                  <p className="path-title">
-                    <Path size={32} />
-                    {path.title}
-                  </p>
-                  {path.occupationsCollection?.items.map((occupation) => (
-                    <button
-                      aria-label="occupation-item"
-                      type="button"
-                      key={occupation.sys.id}
-                      className="occupation"
-                      onClick={() => {
-                        setFieldChanged(false);
-                        setSelected({
-                          pathway: path.occupationsCollection?.items,
-                          id: occupation.sys.id,
-                          title: occupation.title,
-                          shortTitle: occupation.shortTitle,
-                          groupId: paths.mapId,
-                        });
-                        localStorage.setItem(
-                          "occupation",
-                          JSON.stringify({
+            {open && (
+              <div className="dropdown-select">
+                {paths?.items.map((path) => (
+                  <div key={path.sys.id}>
+                    <p className="path-title">
+                      <Path size={32} />
+                      {path.title}
+                    </p>
+                    {path.occupationsCollection?.items.map((occupation) => (
+                      <button
+                        aria-label="occupation-item"
+                        type="button"
+                        key={occupation.sys.id}
+                        className="occupation"
+                        onClick={() => {
+                          setFieldChanged(false);
+                          setSelected({
                             pathway: path.occupationsCollection?.items,
                             id: occupation.sys.id,
-                            shortTitle: occupation.shortTitle,
                             title: occupation.title,
+                            shortTitle: occupation.shortTitle,
                             groupId: paths.mapId,
-                          }),
-                        );
-                        setOpen(false);
-                      }}
-                    >
-                      {occupation.shortTitle || occupation.title}
-                    </button>
-                  ))}
-                </div>
-              ))}
-            </div>
-          )}
+                          });
+                          localStorage.setItem(
+                            "occupation",
+                            JSON.stringify({
+                              pathway: path.occupationsCollection?.items,
+                              id: occupation.sys.id,
+                              shortTitle: occupation.shortTitle,
+                              title: occupation.title,
+                              groupId: paths.mapId,
+                            }),
+                          );
+                          setOpen(false);
+                        }}
+                      >
+                        {occupation.shortTitle || occupation.title}
+                      </button>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-
-        {details.id && (
-          <CareerDetail
-            detailsId={details.id}
-            breadcrumbs={breadcrumbs}
-            client={client}
-            pathway={selected.id ? selected.pathway : localData?.pathway || []}
-            selected={selected.id ? selected : localData || {}}
-            setSelected={setSelected}
-            groupTitle={careerMaps.filter((m) => m.sys.id === selected.groupId)[0]?.title || ""}
-          />
-        )}
       </div>
+
+      {details.id && (
+        <CareerDetail
+          detailsId={details.id}
+          breadcrumbs={breadcrumbs}
+          client={client}
+          pathway={selected.id ? selected.pathway : localData?.pathway || []}
+          selected={selected.id ? selected : localData || {}}
+          setSelected={setSelected}
+          groupTitle={careerMaps.filter((m) => m.sys.id === selected.groupId)[0]?.title || ""}
+        />
+      )}
     </div>
   );
 };
