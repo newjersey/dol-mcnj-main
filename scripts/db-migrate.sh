04bd2887565e55af1e39963d8c4a4e369e09bd85
Chelsea Kelly-Reif <chelsea.kelly-reif@coforma.io>
2023-09-05 16:48:06 -0700

    rm debug statements

--- scripts/db-migrate.sh
+++ scripts/db-migrate.sh
@@ -17,19 +17,10 @@ fi
 
 DB_NAME=$(jq -r ".${ENV}.writer.database" backend/database.json)
 
-echo "NODE_ENV value: $ENV"
-echo "Host env var: $HOST_ENV_VAR"
-echo "Password env var: $PASSWORD_ENV_VAR"
-echo "Database name: $DB_NAME"
 DB_HOST=${!HOST_ENV_VAR}
 DB_PASSWORD=${!PASSWORD_ENV_VAR}
-
-echo "Database host: $DB_HOST"
-echo "Database password length: ${#DB_PASSWORD}"
 
 DATABASE_URL="postgresql://postgres:${DB_PASSWORD}@$DB_HOST:5432/$DB_NAME"
 
 export DATABASE_URL
-echo "Database URL: $DATABASE_URL"
-echo "DB PW: $ENCODED_DB_PASS"
 npm --prefix=backend run db-migrate up
