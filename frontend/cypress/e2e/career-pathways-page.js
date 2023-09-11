277703eb2579b7fb38536ec4a9e17241e95598c6
Lesly Revenge <leslyrevenge@gmail.com>
2023-09-11 09:13:03 -0400

    NJWE-885 write en2 test

---
+++ frontend/cypress/e2e/career-pathways-page.js
@@ -0,0 +1,10 @@
+describe("Funding Page", () => {
+  it("Toggle open close industry detail tray - ", () => {
+    let path = "/career-pathways";
+    cy.visit(path);
+    cy.get("span").contains("Healthcare").click();
+    cy.get(".explore-button").contains("Healthcare").click();
+    cy.get(".panel .open");
+    cy.get("button.close").first().click();
+  });
+});
