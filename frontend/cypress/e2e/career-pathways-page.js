13bd1ff1a5b3fe8bd157f1f6ec2042597d717156
Chelsea Kelly-Reif <chelsea.kelly-reif@coforma.io>
2023-09-12 17:18:17 -0700

    restore

--- frontend/cypress/e2e/career-pathways-page.js
+++ frontend/cypress/e2e/career-pathways-page.js
@@ -7,5 +7,34 @@ describe("Career Pathway Page", () => {
     cy.get(".panel .open");
     cy.get("button.close").first().click();
   });
+  it("It visits each Navbar Tabs ", () => {
+   
+  });
+  it("Images load and have Alt texts ", () => {
+   
+  });
+  it("Map expands ", () => {
+   
+  });
+  it("Link to login & sign up from CN LP", () => {
+   
+  });
+  it("Tuition Assistance link goes to the right page ", () => {
+   
+  });
+  it("Link to more related trainings on TE takes you to the right search ", () => {
+   
+  });
+  it("Local in demand training county tags ", () => {
+   
+  });
+
+  it("Contact Us opens Google Form" , () => {
+   
+  });
+  it("Links out to Tuition Assistance, and one stop job board from in demand careers details with in Healthcare and TDL ", () => {
+   
+  });
+
 });
 
