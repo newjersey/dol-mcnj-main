fc55b63569a04fd0077e832d9eb466c0cb5bbc3c
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-09-18 15:38:27 -0700

    ran prettier

--- frontend/src/components/InDemandBlock.tsx
+++ frontend/src/components/InDemandBlock.tsx
@@ -4,36 +4,35 @@ import { useTranslation } from "react-i18next";
 import { formatCountiesArrayToString } from "../utils/formatCountiesArrayToString";
 
 interface Props {
-    counties: string[];
+  counties: string[];
 }
 
 export const InDemandBlock = (props: Props): ReactElement => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const isTabletAndUp = useMediaQuery("(min-width:768px)");
+  const isTabletAndUp = useMediaQuery("(min-width:768px)");
 
-    const countiesStr = props.counties ? formatCountiesArrayToString(props.counties) : null;
+  const countiesStr = props.counties ? formatCountiesArrayToString(props.counties) : null;
 
-    return (
-        <div className='bg-light-yellow countyBlock'>
-            <div>
-                {!countiesStr
-                    ? t("InDemandBlock.inDemandTitle")
-                    : t("InDemandBlock.localInDemandTitle", { countiesList: countiesStr })
-                }
-            </div>
-            {props.counties && isTabletAndUp && (
-                <div className="ptm pbs">
-                    <a
-                        className="link-format-blue"
-                        href="https://www.nj.gov/labor/career-services/tools-support/demand-occupations/waivers.shtml"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {t("InDemandBlock.localAndRegionalWaiversText")}
-                    </a>
-                </div>
-            )}
+  return (
+    <div className="bg-light-yellow countyBlock">
+      <div>
+        {!countiesStr
+          ? t("InDemandBlock.inDemandTitle")
+          : t("InDemandBlock.localInDemandTitle", { countiesList: countiesStr })}
+      </div>
+      {props.counties && isTabletAndUp && (
+        <div className="ptm pbs">
+          <a
+            className="link-format-blue"
+            href="https://www.nj.gov/labor/career-services/tools-support/demand-occupations/waivers.shtml"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {t("InDemandBlock.localAndRegionalWaiversText")}
+          </a>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
