3a10943d01811028c6fd96a63504b4155f28ef25
Lesly Revenge <leslyrevenge@gmail.com>
2023-09-26 09:01:13 -0400

    add image-svg file

---
+++ frontend/cypress/e2e/image-svg.cy.js
@@ -0,0 +1,51 @@
+let path = "https://d4ad-research2.uk.r.appspot.com/career-pathways";
+const navigation_paths = [
+  { path: path + "/", label: "Home Page" },
+  { path: path + "/search", label: "Search Page" },
+  {
+    path: path + "/in-demand-occupations",
+    label: "Indemand Occupations Page",
+  },
+  { path: path + "/support-resources", label: "Support Resource Page" },
+  {
+    path: path + "/support-resources/career-support",
+    label: "Career Support Page",
+  },
+  {
+    path: path + "/support-resources/tuition-assistance",
+    label: "Tuition Assistance Page",
+  },
+  { path: path + "/support-resources/other", label: "Other Assistant Page" },
+  { path: path + "/faq", label: "FAQ Page" },
+  {
+    path: path + "/training-provider-resources",
+    label: "Training Provider Resources Page",
+  },
+];
+
+describe("Images load and have Alt texts", () => {
+  navigation_paths.forEach((item) => {
+    it(item.label, () => {
+      cy.get("svg").each(($svg) => {
+        // check aria-label
+        cy.wrap($svg).should("have.attr", "aria-label").and("not.be.empty");
+      });
+    });
+    it(item.label, () => {
+      cy.visit(item.path);
+      cy.get("img")
+        .should("not.have.css", "display", "none")
+        .each(($img) => {
+          // check alt fvalue
+          cy.wrap($img).should("have.attr", "alt");
+
+          // check if image is visible
+          // cy.wrap($img).scrollIntoView().should("be.visible");
+
+          //Ensure the natural width and height is greater than 0.
+          expect($img[0].naturalWidth).to.be.greaterThan(0);
+          expect($img[0].naturalHeight).to.be.greaterThan(0);
+        });
+    });
+  });
+});
