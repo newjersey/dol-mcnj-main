3aa4f94df5e3ae516906b38f02e48cee3e8a3854
Chelsea Kelly-Reif <chelsea.kelly-reif@coforma.io>
2023-09-27 11:18:12 -0700

    Enhance error handling in Cypress test for image alt tags and dimensions

--- frontend/cypress/e2e/image-svg.cy.js
+++ frontend/cypress/e2e/image-svg.cy.js
@@ -7,13 +7,20 @@ describe("Images load and have Alt texts", () => {
       cy.get("img")
         .should("not.have.css", "display", "none")
         .each(($img) => {
-          // check alt fvalue
-          cy.wrap($img).should("have.attr", "alt").and("not.be.empty");
+            // Check alt value
+            cy.wrap($img)
+                .should("have.attr", "alt")
+                .and("not.be.empty")
+                .then(alt => {
+                    if (!alt) {
+                        throw new Error(`Image with src "${$img[0].src}" does not have an alt attribute or it's empty.`);
+                    }
+                });
 
-          //Ensure the natural width and height is greater than 0.
-          expect($img[0].naturalWidth).to.be.greaterThan(0);
-          expect($img[0].naturalHeight).to.be.greaterThan(0);
+            // Ensure the natural width and height is greater than 0.
+            expect($img[0].naturalWidth, `Image with src "${$img[0].src}" does not have a valid width.`).to.be.greaterThan(0);
+            expect($img[0].naturalHeight, `Image with src "${$img[0].src}" does not have a valid height.`).to.be.greaterThan(0);
+            });
         });
     });
-  });
 });
