6192e0208381862e0653e4bdbb4fa6596e2485c5
Lesly Revenge <lrevenge>
2023-10-05 15:00:03 -0400

    create fake credential tests

---
+++ frontend/cypress/e2e/career-pathways-page.cy.js
@@ -0,0 +1,27 @@
+
+describe("Link to login & sign up from CN LP", () => {
+  const navigateToOathPage = () => {
+    let path = "https://mycareer.nj.gov/#/";
+    cy.visit(path).get("button").contains("Sign in or Sign Up").click();
+    cy.get("button").contains("Continue with Career Central Account").click();
+    return cy;
+  };
+
+  it("It should navigate to nj auth0", () => {
+    cy = navigateToOathPage();
+    cy.origin("https://prod-nj.us.auth0.com", () => {
+      cy.get("input#username");
+    });
+  });
+  it("It should failed on false credentials", () => {
+    cy = navigateToOathPage();
+    cy.origin("https://prod-nj.us.auth0.com", () => {
+      cy.get("input#username").type("fakemeail@fakemail.com").wait(1000);
+      cy.get("input#password")
+        .type("fakepasswordfakepasswordfakepasswordfakepassword")
+        .wait(1000);
+      cy.get(`[name="action"]`).contains("Continue").click({ force: true });
+      cy.get("span").contains("Wrong email or password").wait(5000);
+    });
+  });
+});
