f567acf00f5bdc8b1e1d767fbe674be8d1e5928c
Lesly Revenge <lrevenge>
2023-10-19 16:28:21 -0400

    remove out of scope elements and wait functions

--- frontend/cypress/e2e/career-pathways-page.cy.js
+++ frontend/cypress/e2e/career-pathways-page.cy.js
@@ -39,35 +39,15 @@ describe("It visits each Navbar Tabs", () => {
 describe("career Pathways Page", () => {
   let path = "https://mycareer.nj.gov/#/";
 
-  it("It should navigate to nj auth0", () => {
+  it("It should log user in", () => {
     cy.visit(path).get("button").contains("Sign in or Sign Up").click();
     cy.get("button").contains("Continue with Career Central Account").click();
-    cy.origin("https://prod-nj.us.auth0.com", () => {
-      cy.get("input#username");
-    });
-  });
-  it("It should failed on false credentials", () => {
-    cy.visit(path).get("button").contains("Sign in or Sign Up").click();
-    cy.get("button").contains("Continue with Career Central Account").click();
-    cy.origin("https://prod-nj.us.auth0.com", () => {
-      cy.get("input#username").type("fakemeail@fakemail.com");
-      cy.get("input#password").type("fakepasswordfakepasswordfakepasswordfakepassword");
-      cy.get(`[name="action"]`).contains("Continue").click({ force: true });
-      cy.get("span").contains("Wrong email or password");
-    });
-  });
-
-  it("It should log user in and log user out", () => {
-    cy.visit(path).get("button").contains("Sign in or Sign Up").click();
-    cy.get("button").contains("Continue with Career Central Account").click();
-    cy.origin("https://prod-nj.us.auth0.com", () => {
+    cy.origin("https://prod-nj.us.auth0.com", async () => {
       const CYPRESS_GOOGLE_AUTH_EMAIL = Cypress.env("CYPRESS_GOOGLE_AUTH_EMAIL");
       const CYPRESS_GOOGLE_AUTH_PASSWORD = Cypress.env("CYPRESS_GOOGLE_AUTH_PASSWORD");
-      console.log(CYPRESS_GOOGLE_AUTH_EMAIL, CYPRESS_GOOGLE_AUTH_PASSWORD, "TESTING ENV");
       cy.get("input#username").type(CYPRESS_GOOGLE_AUTH_EMAIL);
       cy.get("input#password").type(CYPRESS_GOOGLE_AUTH_PASSWORD);
       cy.get(`[name="action"]`).contains("Continue").click({ force: true });
     });
-    cy.get("span").contains("Sign Out").click({ force: true });
   });
 });
