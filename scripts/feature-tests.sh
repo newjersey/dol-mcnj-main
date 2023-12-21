34cb47ca5bd7e36e561067155a7cbf54b975cb47
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-12-20 16:14:14 -0800

    latest

--- scripts/feature-tests.sh
+++ scripts/feature-tests.sh
@@ -21,17 +21,20 @@ done
 echo "App started"
 
 echo "Running Cypress tests..."
-npm --prefix=frontend run cypress:run -- --config baseUrl=http://localhost:${APP_PORT} | tee feature-test-output.txt
+npm --prefix=frontend run cypress:run -- --config baseUrl=http://localhost:${APP_PORT}
 
 echo "Cypress tests completed"
+set +e
 
 # Clean up
 kill $(lsof -i:${APP_PORT} -t)
 
 echo "App stopped"
 
-# Your ASCII art here
 echo "   __            _                                             _"
 echo "  / _| ___  __ _| |_ _   _ _ __ ___  ___   _ __   __ _ ___ ___| |"
-echo " | |_ / _ \/ _` | __| | | | '__/ _ \/ __| | '_ \ / _` / __/ __| |"
-echo " |  _|  __/ (_| | |_| |_| | | |  __/\__ \ | |_) | (_| \__ \
+echo " | |_ / _ \/ _\` | __| | | | '__/ _ \/ __| | '_ \ / _\` / __/ __| |"
+echo " |  _|  __/ (_| | |_| |_| | | |  __/\__ \ | |_) | (_| \__ \__ \_|"
+echo " |_|  \___|\__,_|\__|\__,_|_|  \___||___/ | .__/ \__,_|___/___(_)"
+echo "                                          |_|                    "
+echo ""
