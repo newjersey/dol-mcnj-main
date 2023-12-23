5ddee20c622a8cbafb56fd8367958a4d9122daf1
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-12-22 18:09:11 -0800

    fix

--- scripts/feature-tests.sh
+++ scripts/feature-tests.sh
@@ -1,48 +1,25 @@
 #!/usr/bin/env bash
 
 cd $(git rev-parse --show-toplevel)
-echo "DB_ENV is set to: $DB_ENV"
+
 APP_PORT=8080
 
-# Kill any process using the app port
+kill $(lsof -i:${APP_PORT} -t)
 
 set -e
 
-echo "Starting app..."
+echo "starting app"
 ./scripts/build.sh
-./scripts/prod-start-local.sh & # Start the app in the background
+./scripts/prod-start-local.sh > /dev/null &
+while ! echo exit | nc localhost ${APP_PORT}; do sleep 1; done
 
-echo "Waiting for app to start..."
-while ! nc -z localhost ${APP_PORT}; do
-  sleep 1 # wait for 1 second before check again
-done
+echo "app started"
 
-echo "App started"
-
-# Print that migration is starting
-echo "Starting DB migration..."
-
-# Run the migration and capture output to a temporary file
-npm run db-migrate up -- -e $DB_ENV &> temp_migration_output.txt
-
-# Extract the second to last line and get the migration name
-MIGRATION_NAME=$(tail -2 temp_migration_output.txt | head -1 | grep -oP '(?<=Processed migration ).*')
-
-# Print the name of the migration
-echo "Last migration: $MIGRATION_NAME"
-
-echo "DB migration completed"
-
-echo "Running Cypress tests..."
 npm --prefix=frontend run cypress:run -- --config baseUrl=http://localhost:${APP_PORT}
 
-echo "Cypress tests completed"
 set +e
 
-# Clean up
 kill $(lsof -i:${APP_PORT} -t)
-
-echo "App stopped"
 
 echo "   __            _                                             _"
 echo "  / _| ___  __ _| |_ _   _ _ __ ___  ___   _ __   __ _ ___ ___| |"
