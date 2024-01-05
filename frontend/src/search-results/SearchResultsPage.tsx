1b99905a09491bebe515a49f67780344e67ff399
Scott Wambach <scott@scottwamba.ch>
2024-01-05 16:22:51 -0600

    search results breadcrumbs

--- frontend/src/search-results/SearchResultsPage.tsx
+++ frontend/src/search-results/SearchResultsPage.tsx
@@ -15,6 +15,7 @@ import { ComparisonContext } from "../comparison/ComparisonContext";
 import { useTranslation } from "react-i18next";
 import { logEvent } from "../analytics";
 import { Layout } from "../components/Layout";
+import { ArrowLeft } from "@phosphor-icons/react";
 
 interface Props extends RouteComponentProps {
   client: Client;
@@ -23,6 +24,7 @@ interface Props extends RouteComponentProps {
 
 export const SearchResultsPage = (props: Props): ReactElement<Props> => {
   const isTabletAndUp = useMediaQuery("(min-width:768px)");
+  const isTabletAndBelow = useMediaQuery("(max-width:767px)");
   const { t } = useTranslation();
 
   const [trainings, setTrainings] = useState<TrainingResult[]>([]);
@@ -193,13 +195,42 @@ export const SearchResultsPage = (props: Props): ReactElement<Props> => {
     >
       {isTabletAndUp && (
         <div className="container results-count-container">
-          <div className="row ptd fixed-wrapper">
+          <nav className="usa-breadcrumb " aria-label="Breadcrumbs">
+            <ol className="usa-breadcrumb__list">
+              <li className="usa-breadcrumb__list-item">
+                <a className="usa-breadcrumb__link" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="usa-breadcrumb__list-item">
+                <a className="usa-breadcrumb__link" href="/training-explorer">
+                  Training Explorer
+                </a>
+              </li>
+              <li className="usa-breadcrumb__list-item use-current" aria-current="page">
+                <span data-testid="title">Search</span>
+              </li>
+            </ol>
+          </nav>
+
+          <div className="row fixed-wrapper">
             <div className="col-md-12 fdr fac">
               <div className="result-count-text">{!isLoading && getResultCount()}</div>
               {shouldShowTrainings && <div className="mla">{getSortDropdown()}</div>}
             </div>
           </div>
         </div>
+      )}
+
+      {isTabletAndBelow && (
+        <>
+          <div className="container results-count-container">
+            <a className="back-link" href="/training">
+              <ArrowLeft size={24} />
+              Back
+            </a>
+          </div>
+        </>
       )}
 
       {shouldShowTrainings && (
