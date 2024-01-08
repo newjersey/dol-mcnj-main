57d340f12be307069b3dfd0fac4fa750a32788ec
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-01-08 08:26:15 -0500

    add correct migrations

---
+++ backend/migrations/sqls/20240108132330-add-indemand-heating-ac-fridge-bergen-up.sql
@@ -0,0 +1,4 @@
+DELETE FROM indemandcips;
+DELETE from indemandsocs;
+INSERT into indemandcips (cip, ciptitle, cipcode) VALUES ('47.0201', 'Air Conditioning, Heating, and Refrigeration Technology/Technician', '47.0201');
+INSERT into indemandcips (cip, ciptitle, cipcode) VALUES ('15.0501', 'Air Conditioning, Heating, and Refrigeration Technology/Technician', '15.0501');
