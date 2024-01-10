5d834fccf5709522d5f8a90582e5c44c45acc8a8
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-01-10 09:55:47 -0500

    update sql

--- backend/migrations/sqls/20240108132330-add-indemand-heating-ac-fridge-bergen-down.sql
+++ backend/migrations/sqls/20240108132330-add-indemand-heating-ac-fridge-bergen-down.sql
@@ -1,2 +1,1 @@
-DELETE FROM indemandcips;
 DELETE from indemandsocs;
--- backend/migrations/sqls/20240108132330-add-indemand-heating-ac-fridge-bergen-up.sql
+++ backend/migrations/sqls/20240108132330-add-indemand-heating-ac-fridge-bergen-up.sql
@@ -1,3 +1,1 @@
-INSERT into indemandcips (cip, ciptitle, cipcode) VALUES ('47.0201', 'Air Conditioning, Heating, and Refrigeration Technology/Technician', '47.0201');
-INSERT into indemandcips (cip, ciptitle, cipcode) VALUES ('15.0501', 'Air Conditioning, Heating, and Refrigeration Technology/Technician', '15.0501');
 INSERT into indemandsocs (soc, occupationtitle) VALUES ('49-9021', 'Heating, Air Conditioning, and Refrigeration Mechanics and Installers');
