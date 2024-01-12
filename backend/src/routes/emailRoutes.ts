6153153472328ca3256a95aa634a33fb66f49fe3
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-01-11 18:54:52 -0800

    Add Pinpoint endpoint management

--- backend/src/routes/emailRoutes.ts
+++ backend/src/routes/emailRoutes.ts
@@ -1,10 +1,19 @@
-// emailRoutes.ts
 import express from "express";
 import AWS from "aws-sdk";
 
 const router = express.Router();
 
-const pinpoint = new AWS.Pinpoint();
+// Configure AWS Pinpoint client
+const pinpointConfig: AWS.Pinpoint.ClientConfiguration = {
+  region: process.env.AWS_REGION as string // Ensure AWS_REGION is set
+};
+
+// Conditionally set the endpoint
+if (process.env.AWS_PINPOINT_ENDPOINT) {
+  pinpointConfig.endpoint = process.env.AWS_PINPOINT_ENDPOINT;
+}
+
+const pinpoint = new AWS.Pinpoint(pinpointConfig);
 
 router.post("/submit-email", async (req, res) => {
   const { email } = req.body;
@@ -27,8 +36,8 @@ router.post("/submit-email", async (req, res) => {
   } catch (error) {
     const errorMessage = (error as Error).message || "An unknown error occurred";
     res
-      .status(500)
-      .json({ success: false, message: "Failed to submit email", error: errorMessage });
+        .status(500)
+        .json({ success: false, message: "Failed to submit email", error: errorMessage });
   }
 });
 
