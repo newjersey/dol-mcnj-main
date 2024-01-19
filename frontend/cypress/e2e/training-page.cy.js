74ab393a199310671197cf9f43fc7ccc7f9284a4
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-01-18 16:57:08 -0800

    replaced "displays training details" with latest

--- frontend/cypress/e2e/training-page.cy.js
+++ frontend/cypress/e2e/training-page.cy.js
@@ -1,54 +1,58 @@
 describe("Training Page", () => {
   it("displays training details", () => {
-    cy.visit("/training/45957");
+    cy.visit("/training/33127");
     cy.injectAxe();
 
     // titles
-    cy.contains(".NET Developer").should("exist");
-    cy.contains("h3", "IT America, Inc.").should("exist");
+    cy.contains("Building Maintenance").should("exist");
+    cy.contains("h3", "Orleans Technical College").should("exist");
 
     // stat boxes
-    cy.contains("In-Demand in all of New Jersey.").should("exist");
-    cy.contains("42.8%").should("exist");
+    cy.contains("In-Demand").should("exist");
+    cy.contains("35.2%").should("exist");
+    cy.contains("$27,556").should("exist");
 
     // description
     cy.contains(
-      ".Net Developer program focuses on providing development skills needed to successfully develop Web/Windows applications using both Visual Basic and C# languages. Taught by experienced/senior program developers, the program takes step-by-step from programming basics to application development; analyze and validate business requirements from development prospective; client/server communication and related performance standards. Upon completion of the program, student will gain knowledge on .NET development and have a substantial portfolio demonstrating all aspects of work performing a project",
+        "This program teaches diversified skills needed to maintain and renovate commercial and residential properties. Students are taught basic skills in electricity, carpentry, plumbing, heating and air conditioning maintenance. Training also includes painting, papering, and tiling. Students acquire skills in appliance repair.",
     ).should("exist");
 
     // quick stats
     cy.contains(
-      "Prerequisites: High school diploma; Working Knowledge of any Operating System and Internet Browser",
+        "Prerequisites: High school Diploma or GED, valid driver's license, pass math/reading entrance test",
     ).should("exist");
-    cy.contains("Completion time: 3-5 months").should("exist");
+    cy.contains("Completion time: 6-12 months").should("exist");
 
     // associated occupations
-    cy.contains("Computer Network Support Specialists").should("exist");
-    cy.contains("Computer Programmers").should("exist");
+    cy.contains("Facilities Managers").should("exist");
+    cy.contains("Maintenance and Repair Workers, General").should("exist");
 
     // share trainings
     cy.contains(
-      "This training leads to an occupation that is in-demand, which may qualify for funding. Contact your NJ County One-Stop Career, who will help determine funding eligibility, and share this training page with them.",
+        "This training leads to an occupation that is in-demand, which may qualify for " +
+        "funding. Contact your NJ County One-Stop Career, who will help determine funding " +
+        "eligibility, and share this training page with them.",
     ).should("exist");
     cy.contains("Copy a link to this training opportunity").should("exist");
     cy.contains("Save and print this training opportunity").should("exist");
     cy.contains("Learn more about funding options and One-Stop Centers").should("exist");
 
     // cost
-    cy.contains("$4,000.00").should("exist");
-    cy.contains("$3,800.00").should("exist");
-    cy.contains("$100.00").should("exist");
+    cy.contains("$17,855.00").should("exist");
+    cy.contains("$13,500.00").should("exist");
     cy.contains("$0.00").should("exist");
+    cy.contains("$1,810.00").should("exist");
+    cy.contains("$2,320.00").should("exist");
+    cy.contains("$225.00").should("exist");
 
     // provider details
-    cy.contains("span", "IT America, Inc.").should("exist");
-    cy.contains("100 Metroplex Drive").should("exist");
-    cy.contains("Suite 207").should("exist");
-    cy.contains("Edison, NJ 08817").should("exist");
-    cy.contains("Praveen Kumar Thadakamalla").should("exist");
-    cy.contains("Director").should("exist");
-    cy.contains("(732) 497-9989").should("exist");
-    cy.contains("www.itamericaschool.com").should("exist");
+    cy.contains("span", "Orleans Technical College").should("exist");
+    cy.contains("2770 Red Lion Road").should("exist");
+    cy.contains("Philadelphia, PA 19114").should("exist");
+    cy.contains("Debbie Bello").should("exist");
+    cy.contains("Director of Admissions").should("exist");
+    cy.contains("(215) 728-4733").should("exist");
+    cy.contains("www.orleanstech.edu").should("exist");
 
     cy.checkA11y();
   });
