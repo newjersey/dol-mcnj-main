4b29144aba324785f1b66f8b6cc422be06416ba0
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-01-24 10:50:35 -0500

    jest test progress

--- frontend/src/components/BetaBanner.test.tsx
+++ frontend/src/components/BetaBanner.test.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { BetaBanner } from "./BetaBanner";
 
 describe("<BetaBanner />", () => {
   it("accepts a prop for removing the header margin", () => {
-    const subject = render(<BetaBanner noHeader={true} />);
-    const banner = subject.getByText("Share your feedback!", { exact: false }).parentElement;
+    act(() => {
+      render(<BetaBanner noHeader={true} />);
+    })
+    const banner = screen.getByText("Share your feedback!", { exact: false }).parentElement;
     expect(banner).toHaveClass("no-header-margin");
   });
 
   it("uses default margin if no header prop passed", () => {
-    const subject = render(<BetaBanner />);
-    const banner = subject.getByText("Share your feedback!", { exact: false }).parentElement;
+    act(() => {
+      render(<BetaBanner />);
+    })
+    const banner = screen.getByText("Share your feedback!", { exact: false }).parentElement;
     expect(banner).not.toHaveClass("no-header-margin");
   });
 });
