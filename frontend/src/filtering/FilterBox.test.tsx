b697a5d82641bb9e3e04af6a32a32ddde9ccd607
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-01-24 09:55:01 -0500

    fix filter tests

--- frontend/src/filtering/FilterBox.test.tsx
+++ frontend/src/filtering/FilterBox.test.tsx
@@ -1,11 +1,12 @@
 import { TrainingResult } from "../domain/Training";
 import { Filter, FilterableElement } from "../domain/Filter";
-import { fireEvent, render, RenderResult } from "@testing-library/react";
+import { fireEvent, render, RenderResult, screen } from "@testing-library/react";
 import { FilterContext } from "./FilterContext";
 import React from "react";
 import { FilterBox } from "./FilterBox";
 import { useMediaQuery } from "@material-ui/core";
 import { en as Content } from "../locales/en";
+import { act } from 'react-dom/test-utils';
 
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 function mockFunctions() {
@@ -21,13 +22,13 @@ jest.mock("@material-ui/core", () => mockFunctions());
 const { mobileFilterText, maxCostLabel, searchButtonDefaultText, searchButtonUpdateResultsText } =
   Content.SearchAndFilter;
 
-describe("<FilterBox />", () => {
-  const renderWithFilters = (filters: Filter[]): RenderResult => {
-    const state = {
-      filters: filters,
-    };
+const setupWithFilters = (filters: Filter[]): void => {
+  const state = {
+    filters: filters,
+  };
 
-    return render(
+  return act(() => {
+    render(
       <FilterContext.Provider value={{ state: state, dispatch: jest.fn() }}>
         <FilterBox
           searchQuery={"some-query"}
@@ -38,7 +39,30 @@ describe("<FilterBox />", () => {
           <div />
         </FilterBox>
       </FilterContext.Provider>,
-    );
+    )
+  });
+}
+
+describe("<FilterBox />", () => {
+  const renderWithFilters = (filters: Filter[]): RenderResult | void => {
+    const state = {
+      filters: filters,
+    };
+  
+    return act(() => {
+      render(
+        <FilterContext.Provider value={{ state: state, dispatch: jest.fn() }}>
+          <FilterBox
+            searchQuery={"some-query"}
+            resultCount={1}
+            setShowTrainings={jest.fn()}
+            resetStateForReload={jest.fn()}
+          >
+            <div />
+          </FilterBox>
+        </FilterContext.Provider>,
+      )
+    });
   };
 
   const renderFilterBox = ({ resultCount = 1, setShowTrainings = jest.fn() }): RenderResult => {
@@ -71,7 +95,7 @@ describe("<FilterBox />", () => {
   };
 
   it("sets initial filter value from context if present", () => {
-    const subject = renderWithFilters([
+    renderWithFilters([
       {
         func: (results: TrainingResult[]): TrainingResult[] => results,
         element: FilterableElement.MAX_COST,
@@ -79,82 +103,82 @@ describe("<FilterBox />", () => {
       },
     ]);
 
-    expect(subject.getByLabelText(maxCostLabel, { exact: false })).toHaveValue(5000);
+    expect(screen.getByLabelText(maxCostLabel, { exact: false })).toHaveValue(5000);
   });
 
   it("[MOBILE] has default-color text when no filter applied", () => {
     useMobileSize();
-    const subject = renderWithFilters([]);
+    renderWithFilters([]);
 
-    expect(subject.getByText(mobileFilterText, { exact: false })).not.toHaveClass("blue");
+    expect(screen.getByText(mobileFilterText, { exact: false })).not.toHaveClass("blue");
   });
 
   it("[MOBILE] has blue text when a filter is applied", () => {
     useMobileSize();
-    const subject = renderWithFilters([
+    renderWithFilters([
       {
         func: (results: TrainingResult[]): TrainingResult[] => results,
         element: FilterableElement.MAX_COST,
         value: "5000",
       },
     ]);
-    expect(subject.getByText(mobileFilterText, { exact: false })).toHaveClass("blue");
+    expect(screen.getByText(mobileFilterText, { exact: false })).toHaveClass("blue");
   });
 
   it("[MOBILE] opens and closes filter panel on button push", () => {
     useMobileSize();
-    const subject = renderFilterBox({});
-    expect(subject.getByLabelText(maxCostLabel, { exact: false })).not.toBeVisible();
-    fireEvent.click(subject.getByText(mobileFilterText));
-    expect(subject.getByLabelText(maxCostLabel, { exact: false })).toBeVisible();
+    renderFilterBox({});
+    expect(screen.getByLabelText(maxCostLabel, { exact: false })).not.toBeVisible();
+    fireEvent.click(screen.getByText(mobileFilterText));
+    expect(screen.getByLabelText(maxCostLabel, { exact: false })).toBeVisible();
   });
 
   it("[MOBILE] changes arrow to indicate open/close state", () => {
     useMobileSize();
-    const subject = renderFilterBox({});
+    renderFilterBox({});
 
-    expect(subject.queryByText("keyboard_arrow_down")).toBeInTheDocument();
-    expect(subject.queryByText("keyboard_arrow_up")).not.toBeInTheDocument();
+    expect(screen.queryByText("keyboard_arrow_down")).toBeInTheDocument();
+    expect(screen.queryByText("keyboard_arrow_up")).not.toBeInTheDocument();
 
-    fireEvent.click(subject.getByText(mobileFilterText));
+    fireEvent.click(screen.getByText(mobileFilterText));
 
-    expect(subject.queryByText("keyboard_arrow_down")).not.toBeInTheDocument();
-    expect(subject.queryByText("keyboard_arrow_up")).toBeInTheDocument();
+    expect(screen.queryByText("keyboard_arrow_down")).not.toBeInTheDocument();
+    expect(screen.queryByText("keyboard_arrow_up")).toBeInTheDocument();
   });
 
   it("[MOBILE] closes the filter panel when search is executed", () => {
     useMobileSize();
-    const subject = renderFilterBox({});
-    fireEvent.click(subject.getByText(mobileFilterText));
-    expect(subject.getByLabelText(maxCostLabel, { exact: false })).toBeVisible();
+    renderFilterBox({});
+    fireEvent.click(screen.getByText(mobileFilterText));
+    expect(screen.getByLabelText(maxCostLabel, { exact: false })).toBeVisible();
 
-    fireEvent.click(subject.getByText(searchButtonUpdateResultsText));
+    fireEvent.click(screen.getByText(searchButtonUpdateResultsText));
 
-    expect(subject.getByLabelText(maxCostLabel, { exact: false })).not.toBeVisible();
+    expect(screen.getByLabelText(maxCostLabel, { exact: false })).not.toBeVisible();
   });
 
   it("[MOBILE] displays the number of search results", () => {
     useMobileSize();
-    const subject = renderFilterBox({ resultCount: 50 });
-    fireEvent.click(subject.getByText(mobileFilterText));
+    renderFilterBox({ resultCount: 50 });
+    fireEvent.click(screen.getByText(mobileFilterText));
 
-    expect(subject.getByText("50 results")).toBeInTheDocument();
+    expect(screen.getByText("50 results")).toBeInTheDocument();
   });
 
   it("[MOBILE] uses correct grammar on result count", () => {
     useMobileSize();
-    const subject = renderFilterBox({ resultCount: 1 });
-    fireEvent.click(subject.getByText(mobileFilterText));
+    renderFilterBox({ resultCount: 1 });
+    fireEvent.click(screen.getByText(mobileFilterText));
 
-    expect(subject.getByText("1 result")).toBeInTheDocument();
+    expect(screen.getByText("1 result")).toBeInTheDocument();
   });
 
   it("[MOBILE] displays search bar if no search query is given", () => {
     useMobileSize();
-    const subject = renderEmptyFilterBox({});
+    renderEmptyFilterBox({});
 
-    expect(subject.getAllByText(searchButtonDefaultText)[0]).toBeInTheDocument();
-    expect(subject.queryByText(searchButtonUpdateResultsText)).not.toBeInTheDocument();
+    expect(screen.getAllByText(searchButtonDefaultText)[0]).toBeInTheDocument();
+    expect(screen.queryByText(searchButtonUpdateResultsText)).not.toBeInTheDocument();
   });
 
   it("triggers setShowTrainings on screen size change", () => {
@@ -166,9 +190,9 @@ describe("<FilterBox />", () => {
 
   it("[DESKTOP] shows filter panel by default with no toggle button", () => {
     useDesktopSize();
-    const subject = renderFilterBox({});
-    expect(subject.getByLabelText(maxCostLabel, { exact: false })).toBeVisible();
-    expect(subject.queryByText("Filters")).not.toBeInTheDocument();
+    renderFilterBox({});
+    expect(screen.getByLabelText(maxCostLabel, { exact: false })).toBeVisible();
+    expect(screen.queryByText("Filters")).not.toBeInTheDocument();
   });
 
   const useDesktopSize = (): void => {
