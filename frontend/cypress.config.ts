5706e074d4178c01844715301c388216b5a839dc
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-01-31 17:54:30 -0800

    Added save videos on failed/retried test

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress";
+import * as fs from "fs";
 
 export default defineConfig({
   e2e: {
@@ -12,8 +13,24 @@ export default defineConfig({
           console.table(message);
           return null;
         },
-      });
+      }),
+      on(
+          'after:spec',
+          (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+            if (results && results.video) {
+              // Do we have failures for any retry attempts?
+              const failures = results.tests.some((test) =>
+                  test.attempts.some((attempt) => attempt.state === 'failed')
+              )
+              if (!failures) {
+                // delete the video if the spec passed and no tests retried
+                fs.unlinkSync(results.video)
+              }
+            }
+          }
+      )
     },
     baseUrl: "http://localhost:3000",
+    video: true
   },
 });
