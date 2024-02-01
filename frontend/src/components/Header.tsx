789357c7335321c6897b14d691982b0eccb9069b
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-01-31 17:09:16 -0800

    fix string interpolation typo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -29,7 +29,7 @@ export const Header = (data: { mainNav?: NavMenuData; globalNav?: NavMenuData })
             aria-label={process.env.REACT_APP_SITE_NAME}
           >
             <img className="mrd" src={njLogo} alt={t("IconAlt.njLogo")} />
-            <h1 className="text-m">${process.env.REACT_APP_SITE_NAME}</h1>
+            <h1 className="text-m">{process.env.REACT_APP_SITE_NAME}</h1>
           </a>
           <UnstyledButton onClick={toggleIsOpen} className="link-format-black">
             <div className={`toggle ${isOpen ? "open" : "closed"}`}>
@@ -67,7 +67,7 @@ export const Header = (data: { mainNav?: NavMenuData; globalNav?: NavMenuData })
           <div className="basic-logo" id="basic-logo">
             <a href="/" aria-label={process.env.REACT_APP_SITE_NAME}>
               <img className="mrd" src={njLogo} alt={t("IconAlt.njLogo")} />
-              <h1 className="bold">${process.env.REACT_APP_SITE_NAME}</h1>
+              <h1 className="bold">{process.env.REACT_APP_SITE_NAME}</h1>
             </a>
           </div>
         </nav>
