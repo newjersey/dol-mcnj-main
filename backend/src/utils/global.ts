29be9e8ce16de2f7ade8cd7c5e2991607285d02b
denish-fearless <dsubedi@fearless.tech>
2024-02-02 10:48:08 -0500

    configuration user-research brach GCP

--- backend/src/utils/global.ts
+++ backend/src/utils/global.ts
@@ -1,58 +1,15 @@
-import cloudwatchLogger from './cloudwatchLogger';
-import sentryLogger from './sentryLogger';
+import logger from "./logger";
 
-(function overrideConsoleMethods() {
-  const isAwsEnvironment = process.env.NODE_ENV === 'awsprod' ||
-      process.env.NODE_ENV === 'awstest' ||
-      process.env.NODE_ENV === 'awsdev';
+// change global console.log to winston logger type
 
-  const statusDict: Array<string> = ["error", "warn", "info", "http", "verbose", "debug"];
-  const originalConsoleLog = console.log;
-
-  // Define a function to determine which logger to use based on NODE_ENV
-  const logMessage = (message: string, status?: string) => {
-    if (!status || statusDict.indexOf(status) === -1 || !isAwsEnvironment) {
-      originalConsoleLog(message);
-    } else {
-      // Use cloudwatchLogger and sentryLogger for specified statuses
-      switch (status) {
-        case "error":
-          cloudwatchLogger.error(message);
-          sentryLogger.error(message);
-          break;
-        case "warn":
-          cloudwatchLogger.warn(message);
-          break;
-        case "info":
-        case "http":
-        case "debug":
-        case "verbose":
-          cloudwatchLogger.info(message);
-          break;
-      }
+(() => {
+  console.log = function (message, status = "info") {
+    // the available status
+    const status_dict = ["error", "warn", "info", "http", "verbose", "debug"];
+    // if status is not part of the availability, change it to info
+    if (!status_dict.includes(status)) {
+      status = "info";
     }
-  };
-
-  // Override the console methods
-  console.log = logMessage;
-
-  console.info = function (...args) {
-    const message = args.join(' ');
-    logMessage(message, 'info');
-  };
-
-  console.warn = function (...args) {
-    const message = args.join(' ');
-    logMessage(message, 'warn');
-  };
-
-  console.error = function (...args) {
-    const message = args.join(' ');
-    logMessage(message, 'error');
-  };
-
-  console.debug = function (...args) {
-    const message = args.join(' ');
-    logMessage(message, 'debug');
+    logger.log(status, message);
   };
 })();
