da5583dce069e4edb6e83fafa84be0708c593c90
Scott Wambach <scott@scottwamba.ch>
2024-02-14 14:21:41 -0600

    remove rule

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -10,7 +10,7 @@ import { Occupation, OccupationDetail } from "../domain/occupations/Occupation";
 import { Training } from "../domain/training/Training";
 import { TrainingResult } from "../domain/training/TrainingResult";
 import { Selector } from "../domain/training/Selector";
-import { CareerOneStopClient } from "../../src/careeronestop/CareerOneStopClient";
+import { CareerOneStopClient } from "../careeronestop/CareerOneStopClient";
 
 interface RouterActions {
   searchTrainings: SearchTrainings;
