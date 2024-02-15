106a797b7006402521a5c6dfd89f9cb2cff5a7e6
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-02-15 08:16:56 -0500

    fix url change faq bug

--- frontend/cypress/e2e/faq-page.cy.js
+++ frontend/cypress/e2e/faq-page.cy.js
@@ -6,4 +6,11 @@ describe("FAQ Page", () => {
     cy.contains("Get answers to all of your NJ Career Central questions").should("exist");
     cy.checkA11y();
   });
+
+  it("opens the Training Explorer section on the FAQ page", () => {
+    cy.visit("/faq");
+    cy.url().should("include", "#training");
+    cy.contains("Tuition Assistance for Training").should("exist");
+    cy.contains("Tuition Assistance for Training").should("exist");
+  });
 });
