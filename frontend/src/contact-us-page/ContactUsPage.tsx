810526d103deca323e12ae1b9a0e803b98cc6392
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-04 09:58:25 -0500

    container styles

--- frontend/src/contact-us-page/ContactUsPage.tsx
+++ frontend/src/contact-us-page/ContactUsPage.tsx
@@ -34,6 +34,30 @@ export const ContactUsPage = (props: Props): ReactElement<Props> => {
           }
         }
       />
+      <section className="contact-us-content">
+        <div className="contact-us-container">
+          <div className="info-block">
+            <h2>Contact Information</h2>
+            <p>
+              <strong>
+                NJ Department of Labor and Workforce Development
+              </strong>
+            </p>
+            <p>
+              Center for Occupational Employment Information (COEI)
+            </p>
+            <p>
+              PO Box 057, 5th Floor, Trenton, New Jersey 08625-0057
+            </p>
+          </div>
+          <div className="contact-form-container">
+            <h2>Contact Form</h2>
+            <p>
+              Please reach out to us with your questions or comments. Our staff at the Department of Labor and Workforce office will get back with you in 3-5 business days.
+            </p>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 };
