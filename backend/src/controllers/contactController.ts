dad5e3a44bd3f40ad4c5ca487382caf03a0c1afe
denish-fearless <dsubedi@fearless.tech>
2024-03-04 18:44:28 -0500

    cleaned up the types and added tests

--- backend/src/controllers/contactController.ts
+++ backend/src/controllers/contactController.ts
@@ -1,30 +1,30 @@
 import { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
-// import { sendEmail } from '../utils/emailService';
+import { sendEmail } from '../utils/emailService';
 
-export const submitContactForm = async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+interface ContactFormRequest extends Request {
+  body: {
+    email: string;
+    message: string;
+    url?: string;
+  };
+}
 
-  const { name, category, email, message, url } = req.body;
+export const submitContactForm = async (req: ContactFormRequest, res: Response): Promise<void> => {
+  const { email, message, url } = req.body;
   const emailBody = `
-    Name: ${name}\n
-    Category: ${category}\n
     Email: ${email}\n
     Message: ${message}\n
     URL: ${url || 'N/A'}
   `;
-
+ 
   try {
-    // await sendEmail({
-    //   recipient: process.env.CONTACT_RECEIVER_EMAIL || '',
-    //   subject: `[My Career NJ] New Contact Request from ${name}`,
-    //   body: emailBody,
-    // });
+    await sendEmail({
+      subject: `[My Career NJ] New Contact Request from ${email}`,
+      body: emailBody,
+    });
     res.status(200).json({ message: 'Your message has been sent successfully.' });
-  } catch (error) {
-    res.status(500).json({ message: 'There was an error sending your message.' });
+  } catch (error: unknown) {
+    console.error(error);
+    res.status(500).json({ message: 'There was an error sending your message.', error: error});
   }
 };
