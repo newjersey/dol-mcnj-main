dc9402f3c0f1fbd2de13798b0a9fe517839d9567
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-03-05 15:31:07 -0800

    retry

--- backend/src/globalSetup.js
+++ backend/src/globalSetup.js
@@ -1,11 +1,6 @@
 const util = require("util");
-const { exec: execCallback } = require("child_process");
-
-// Enhance the cmd function to accept options, including a custom maxBuffer size
-const cmd = (command, options = {}) => {
-  const execPromisified = util.promisify(execCallback);
-  return execPromisified(command, { maxBuffer: 1024 * 5000, ...options }); // Default to 5 MB buffer, override with options if provided
-};
+const { exec } = require("child_process");
+const cmd = util.promisify(exec);
 
 module.exports = async () => {
   try {
@@ -13,12 +8,16 @@ module.exports = async () => {
     await cmd("psql -c 'create database d4adtest;' -U postgres -h localhost -p 5432");
     console.log("Database created successfully.");
 
-    console.log("Running database migrations...");
-    // For commands that typically produce a lot of output, you can increase the maxBuffer size as needed
-    await cmd("npm run db-migrate up -- -e test", { maxBuffer: 1024 * 10000 }); // Example: Increasing buffer to 10 MB for db-migrate
-    console.log("Database migrations applied successfully.");
+    // Check if running in CI environment and skip migration if so
+    if (!process.env.IS_CI) {
+      console.log("Running database migrations...");
+      await cmd("npm run db-migrate up -- -e test");
+      console.log("Database migrations applied successfully.");
+    } else {
+      console.log("Skipping database migrations in CI environment.");
+    }
   } catch (error) {
     console.error("Error during global setup:", error);
-    throw error; // Rethrow the error to ensure Jest is aware that the global setup failed
+    throw error;
   }
 };
