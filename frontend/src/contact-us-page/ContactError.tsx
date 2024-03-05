2e4ca3f7f91138918e0eea15122ee43f6c272ec9
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-05 14:42:58 -0500

    reset

--- frontend/src/contact-us-page/ContactError.tsx
+++ frontend/src/contact-us-page/ContactError.tsx
@@ -1,13 +1,20 @@
 import ResetFormBtn from "./ResetFormBtn"
 
-const ContactError = () => {
+const ContactError = ({
+  resetForm
+}: {
+  resetForm: () => void
+}) => {
+
+  const handleReset = () => { resetForm() };
+
   return (
     <section>
       <h2>Submission Error</h2>
       <p>
         There was an error with your submission and our team has been alerted. You can reset the form and try your message again.
       </p>
-      <ResetFormBtn />
+      <ResetFormBtn handleReset={handleReset} />
     </section>
   )
 }
--- frontend/src/contact-us-page/ContactSuccess.tsx
+++ frontend/src/contact-us-page/ContactSuccess.tsx
@@ -1,13 +1,20 @@
 import ResetFormBtn from "./ResetFormBtn"
 
-const ContactSuccess = () => {
+const ContactSuccess = ({
+  resetForm
+}: {
+  resetForm: () => void
+}) => {
+
+  const handleReset = () => { resetForm() };
+
   return (
     <section>
       <h2>Success!</h2>
       <p>
         Your message has been sent and we will do our best to respond within 3-5 business days. Be sure to check your spam folder for any communications.
       </p>
-      <ResetFormBtn />
+      <ResetFormBtn handleReset={handleReset} />
     </section>
   )
 }
