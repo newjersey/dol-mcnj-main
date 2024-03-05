437a1df39a044fb270e2b2562ecb4c0b046c7a5d
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-03-05 15:08:22 -0800

    retry

--- backend/src/globalSetup.js
+++ backend/src/globalSetup.js
@@ -1,15 +1,15 @@
 const util = require("util");
-const { exec: execCallback } = require("child_process");
-const exec = util.promisify((command) => execCallback(command, { maxBuffer: 1024 * 5000 })); // Increase maxBuffer to 5MB
+const { exec } = require("child_process");
+const cmd = util.promisify(exec);
 
 module.exports = async () => {
   try {
     console.log("Creating database 'd4adtest'...");
-    const createDbResult = await exec("psql -c 'create database d4adtest;' -U postgres -h localhost -p 5432");
+    const createDbResult = await cmd("psql -c 'create database d4adtest;' -U postgres -h localhost -p 5432", { maxBuffer: 1024 * 5000 }); // Increase maxBuffer to 5MB
     console.log("Database created successfully. Output:", createDbResult.stdout);
 
     console.log("Running database migrations...");
-    const migrateResult = await exec("npm run db-migrate up -- -e test");
+    const migrateResult = await cmd("npm run db-migrate up -- -e test", { maxBuffer: 1024 * 5000 }); // Same increase for migrations
     console.log("Database migrations applied successfully. Output:", migrateResult.stdout);
   } catch (error) {
     console.error("Error during global setup:", error);
