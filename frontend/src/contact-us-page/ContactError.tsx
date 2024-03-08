c5f812eb5e3aead74750b9de485217432f1e0103
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-08 08:22:24 -0500

    fixing error

--- frontend/src/contact-us-page/ContactError.tsx
+++ frontend/src/contact-us-page/ContactError.tsx
@@ -9,13 +9,13 @@ const ContactError = ({
   const handleReset = () => { resetForm() };
 
   return (
-    <section>
+    <>
       <h2>Submission Error</h2>
       <p>
         There was an error with your submission and our team has been alerted. You can reset the form and try your message again.
       </p>
       <ResetFormBtn handleReset={handleReset} />
-    </section>
+    </>
   )
 }
 
