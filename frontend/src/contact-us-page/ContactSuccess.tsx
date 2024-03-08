db8d957007c6e169447ad6ee50342667c6184194
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-08 08:22:24 -0500

    fixing error

--- frontend/src/contact-us-page/ContactSuccess.tsx
+++ frontend/src/contact-us-page/ContactSuccess.tsx
@@ -9,13 +9,13 @@ const ContactSuccess = ({
   const handleReset = () => { resetForm() };
 
   return (
-    <section>
+    <>
       <h2>Success!</h2>
       <p>
         Your message has been sent and we will do our best to respond within 3-5 business days. Be sure to check your spam folder for any communications.
       </p>
       <ResetFormBtn handleReset={handleReset} />
-    </section>
+    </>
   )
 }
 
