4d85d5903fd0eba303365bc19cb7635c8ea4dada
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-03-11 13:23:37 -0700

    Implemented env var REACT_APP_FEATURE_BETA

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -55,11 +55,15 @@ export const Layout = (props: LayoutProps) => {
           dismissible
         />
       )}
-      <AlertBar
-        copy="My Career NJ is in beta. You can send feedback and comments to us through our [contact form](/contact)."
-        type="info"
-        className="beta-alert"
-      />
+      {process.env.REACT_APP_FEATURE_BETA === "true" && (
+          <AlertBar
+              copy="My Career NJ is in beta. You can send feedback and comments to us through our [contact form](/contact)."
+              type="info"
+              className="beta-alert"
+          />
+      )}
+
+
       {props.seo && <Seo {...props.seo} />}
       <Header {...headerProps} />
       <main
