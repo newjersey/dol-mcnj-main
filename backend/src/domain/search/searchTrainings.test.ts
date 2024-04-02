b69ba2f07ac86d596ac7cba5c2fea2ee398e7348
denish-fearless <dsubedi@fearless.tech>
2024-04-02 18:43:28 -0400

    alphabetical sorting with asc and desc

--- backend/src/domain/search/searchTrainings.test.ts
+++ backend/src/domain/search/searchTrainings.test.ts
@@ -7,6 +7,8 @@ jest.mock("@sentry/node");
 jest.mock("../../credentialengine/CredentialEngineAPI");
 
 describe('searchTrainingsFactory', () => {
+  const searchTrainings = searchTrainingsFactory();
+  const getResultsSpy = jest.spyOn(credentialEngineAPI, 'getResults');
   const ceData: AxiosResponse = {
     data: { data: [], extra: {TotalResults: 0} },
     status: 200,
@@ -28,15 +30,13 @@ describe('searchTrainingsFactory', () => {
       previousPage: null
     }
   }
+  
   afterEach(() => {
-    mockAxios.reset();
+    jest.clearAllMocks()
   });
 
   it('should return cached results when available', async () => {
-    const searchTrainings = searchTrainingsFactory();
-    const getResultsSpy = jest.spyOn(credentialEngineAPI, 'getResults');
     getResultsSpy.mockResolvedValueOnce(ceData);
-  
     await searchTrainings({ searchQuery: 'test', page: 1, limit: 10 });
     // Second call to the function to retrieve data from the cache
     const result2 = await searchTrainings({ searchQuery: 'test', page: 1, limit: 10 });
@@ -44,25 +44,17 @@ describe('searchTrainingsFactory', () => {
     expect(getResultsSpy).toHaveBeenCalledTimes(1); // API should only be called once
   });
   
-
-  
   it('should return results from the API when cache is not available', async () => {
-    const searchTrainings = searchTrainingsFactory();
-    const getResultsSpy = jest.spyOn(credentialEngineAPI, 'getResults');
     getResultsSpy.mockResolvedValueOnce(ceData);
-
-    const result = await searchTrainings({ searchQuery: 'test', page: 1, limit: 10 });
+    const result = await searchTrainings({ searchQuery: 'test_no_cache', page: 1, limit: 10 });
     expect(result).toEqual(expectedData);
     expect(getResultsSpy).toHaveBeenCalled();
   });
 
   it('should throw an error when API request fails', async () => {
-    const searchTrainings = searchTrainingsFactory();
-    const getResultsSpy = jest.spyOn(credentialEngineAPI, 'getResults');
     getResultsSpy.mockRejectedValueOnce(new Error('API error'));
-
     try {
-      await searchTrainings({ searchQuery: 'test', page: 1, limit: 10 });
+      await searchTrainings({ searchQuery: 'test_error', page: 1, limit: 10 });
     } catch (error:unknown) {
       if (error instanceof Error) {
         expect(error.message).toEqual("Failed to fetch results from Credential Engine API");
@@ -70,4 +62,44 @@ describe('searchTrainingsFactory', () => {
     }
     expect(credentialEngineAPI.getResults).toHaveBeenCalled();
   });
+
+  it('should handle asc sorting correctly', async () => {
+    // Test ascending sort
+    getResultsSpy.mockResolvedValueOnce(ceData);
+    await searchTrainings({ searchQuery: 'test', page: 1, limit: 10, sort: 'asc' });
+    let asc = false;
+    getResultsSpy.mock.calls.forEach(call => {
+      if (call.includes("ceterms:name")) {
+        asc = true;
+      }
+    });
+    expect(asc).toBe(true);
+  });
+
+  it('should handle desc sorting correctly', async () => {
+    // Test descending sort
+    getResultsSpy.mockResolvedValueOnce(ceData);
+    await searchTrainings({ searchQuery: 'test', page: 1, limit: 10, sort: 'desc' });
+    let desc = false;
+    getResultsSpy.mock.calls.forEach(call => {
+      if (call.includes("^ceterms:name")) {
+        desc = true;
+      }
+    });
+    expect(desc).toBe(true);
+  });
+
+  it('should handle default sorting correctly', async () => {
+    // Test default sort
+    getResultsSpy.mockResolvedValueOnce(ceData);
+    await searchTrainings({ searchQuery: 'test_default_sorting', page: 1, limit: 10 });
+    let deafultSort = false;
+    getResultsSpy.mock.calls.forEach(call => {
+      if (call.includes("^search:relevance")) {
+        deafultSort = true;
+      }
+    });
+    expect(deafultSort).toBe(true);
+  });
+
 });
