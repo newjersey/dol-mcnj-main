8f111e3d24f0e722208b6401fd3f7e8d288b77a1
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-04-05 18:03:58 -0700

    refactor calculateTotalClockHoursFromEstimatedDuration

--- backend/src/credentialengine/CredentialEngineUtils.ts
+++ backend/src/credentialengine/CredentialEngineUtils.ts
@@ -45,5 +45,32 @@ export const credentialEngineUtils = {
       return price ? Number(price) : null; // Convert price to number, return null if conversion fails or price is undefined
     }
     return null; // Return null if no estimatedCostObject is found
+  },
+
+  calculateTotalClockHoursFromEstimatedDuration: async function (certificate: CTDLResource) {
+    const estimatedDuration = certificate["ceterms:estimatedDuration"];
+    if (!estimatedDuration || estimatedDuration.length === 0) return 0;
+    const exactDuration = estimatedDuration[0]["ceterms:exactDuration"];
+    return exactDuration ? this.convertIso8601ToTotalHours(exactDuration) : 0;
+  },
+
+// Function to convert ISO 8601 duration to total hours
+  convertIso8601ToTotalHours: async function (isoString: string) {
+    const match = isoString.match(
+      /P(?:([0-9]+)Y)?(?:([0-9]+)M)?(?:([0-9]+)W)?(?:([0-9]+)D)?T?(?:([0-9]+)H)?(?:([0-9]+)M)?(?:([0-9]+)S)?/,
+    );
+    if (!match) {
+      return 0; // Return 0 if the string does not match the pattern
+    }
+
+    const years = parseInt(match[1] || "0", 10) * 365 * 24;
+    const months = parseInt(match[2] || "0", 10) * 30 * 24;
+    const weeks = parseInt(match[3] || "0", 10) * 7 * 24;
+    const days = parseInt(match[4] || "0", 10) * 24;
+    const hours = parseInt(match[5] || "0", 10);
+    const minutes = parseInt(match[6] || "0", 10) / 60;
+    const seconds = parseInt(match[7] || "0", 10) / 3600;
+
+    return years + months + weeks + days + hours + minutes + seconds; // Sum up all components
   }
 }
