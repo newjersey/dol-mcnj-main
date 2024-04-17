0d12509775e0b5099b83ea74c33ba69f20cba440
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-04-16 17:29:30 -0700

    rv

--- scripts/feature-tests.sh
+++ scripts/feature-tests.sh
@@ -1,38 +1,25 @@
 #!/usr/bin/env bash
 
-echo "Debug: Moving to the git repository's root directory..."
-cd $(git rev-parse --show-toplevel) || { echo "Failed to change directory to the git repository's root. Exiting..."; exit 1; }
+cd $(git rev-parse --show-toplevel)
 
 APP_PORT=8080
-echo "Debug: Using application port ${APP_PORT}."
 
-echo "Debug: Killing any process currently using port ${APP_PORT}..."
-kill $(lsof -i:${APP_PORT} -t) || echo "No process to kill on port ${APP_PORT}."
+kill $(lsof -i:${APP_PORT} -t)
 
 set -e
 
-echo "Debug: Starting the application..."
+echo "starting app"
 ./scripts/build.sh
-if [ $? -ne 0 ]; then
-    echo "Build failed, exiting..."
-    exit 1
-fi
 ./scripts/prod-start-local.sh > /dev/null &
-echo "Debug: Waiting for the application to start listening on port ${APP_PORT}..."
-while ! echo exit | nc localhost ${APP_PORT}; do
-    echo "Debug: Application not yet ready, sleeping for 1 second..."
-    sleep 1
-done
+while ! echo exit | nc localhost ${APP_PORT}; do sleep 1; done
 
-echo "Debug: Application has started."
+echo "app started"
 
-echo "Debug: Running Cypress tests..."
 npm --prefix=frontend run cypress:run -- --config baseUrl=http://localhost:${APP_PORT}
 
 set +e
 
-echo "Debug: Killing any process currently using port ${APP_PORT}..."
-kill $(lsof -i:${APP_PORT} -t) || echo "No process to kill on port ${APP_PORT}."
+kill $(lsof -i:${APP_PORT} -t)
 
 echo "   __            _                                             _"
 echo "  / _| ___  __ _| |_ _   _ _ __ ___  ___   _ __   __ _ ___ ___| |"
