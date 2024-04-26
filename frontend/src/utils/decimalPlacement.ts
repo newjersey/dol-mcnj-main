dca3ff015f297186bf3c0d27b0daf603ec62a044
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2024-04-26 15:56:50 -0700

    change Training data object to use full cipDefinition for cip title lookup - created a basic working example on TrainingPage.tsx

--- frontend/src/utils/decimalPlacement.ts
+++
@@ -1,9 +0,0 @@
-export const decimalPlacement = (value: number | string): number => {
-  if (typeof value === "string") {
-    value = parseInt(value);
-  }
-
-  value = value / 10000;
-
-  return parseFloat(value.toFixed(4));
-};
