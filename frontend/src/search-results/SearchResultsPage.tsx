9a191c48c3f34d4197e5ce01529c0572a0fefd47
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-05-16 13:22:09 -0400

    search results page styling

--- frontend/src/search-results/SearchResultsPage.tsx
+++ frontend/src/search-results/SearchResultsPage.tsx
@@ -5,7 +5,7 @@ import {
   useEffect,
   useState
 } from "react";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 import { WindowLocation } from "@reach/router";
 import { RouteComponentProps } from "@reach/router";
 
@@ -22,6 +22,7 @@ import { usePageTitle } from "../utils/usePageTitle";
 import { Breadcrumbs } from "./Breadcrumbs";
 import { TrainingComparison } from "./TrainingComparison";
 import { ResultsCount } from "./ResultsCount";
+import { SortByAndLimitSelects } from "./SortByAndLimitSelects";
 import { TrainingResultCard } from "./TrainingResultCard";
 
 interface Props extends RouteComponentProps {
@@ -30,12 +31,11 @@ interface Props extends RouteComponentProps {
 }
 
 export const SearchResultsPage = ({ client, location }: Props) : ReactElement<Props> => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   const [isError, setIsError] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [itemsPerPage, setItemsPerPage] = useState<number>(10);
-  const [metaData, setMetaData] = useState<TrainingData["meta"]>();
   const [metaCount, setMetaCount] = useState<number>(0);
   const [pageNumber, setPageNumber] = useState<number>(1);
   const [pageTitle, setPageTitle] = useState<string>(
@@ -66,7 +66,7 @@ export const SearchResultsPage = ({ client, location }: Props) : ReactElement<Pr
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location?.search);
-    const limit = urlParams.get("limit") || "10";
+    const limit = urlParams.get("limit");
     const miles = urlParams.get("miles") || undefined;
     const page = urlParams.get("p");
     const zip = urlParams.get("zip") || undefined;
@@ -74,11 +74,11 @@ export const SearchResultsPage = ({ client, location }: Props) : ReactElement<Pr
     setIsLoading(true);
 
     if (limit) {
-      setItemsPerPage(parseInt(limit));
+      setItemsPerPage(Number(limit));
     }
 
     if (page) {
-      setPageNumber(parseInt(page));
+      setPageNumber(Number(page));
     } else {
       setPageNumber(1);
     }
@@ -92,7 +92,6 @@ export const SearchResultsPage = ({ client, location }: Props) : ReactElement<Pr
           {
             onSuccess: ({ data, meta }: TrainingData) => {
               setTrainings(data);
-              setMetaData(meta);
               setMetaCount(meta.totalItems);
               getPageTitle();
               setIsLoading(false);
@@ -109,8 +108,6 @@ export const SearchResultsPage = ({ client, location }: Props) : ReactElement<Pr
         );
       }
     }
-
-    console.log(trainings, metaData)
   }, [location?.pathname, pageNumber, itemsPerPage, sortBy]);
 
   if (isError) {
@@ -121,6 +118,17 @@ export const SearchResultsPage = ({ client, location }: Props) : ReactElement<Pr
     const newSortOrder = event.target.value as "asc" | "desc" | "price_asc"  | "price_desc" | "EMPLOYMENT_RATE" | "best_match";
     setSortBy(newSortOrder);
     logEvent("Search", "Updated sort", newSortOrder);
+  }
+
+  const handleLimitChange = (event: ChangeEvent<{ value: string }>): void => {
+    setIsLoading(true);
+    setItemsPerPage(
+      event.target.value as unknown as number,
+    );
+    const newUrl = new URL(window.location.href);
+    newUrl.searchParams.set("p", "1");
+    newUrl.searchParams.set("limit", event.target.value);
+    window.history.pushState({}, "", newUrl.toString());
   }
 
   return (
@@ -136,74 +144,19 @@ export const SearchResultsPage = ({ client, location }: Props) : ReactElement<Pr
         <div className="container">
           <div className="results-count-container">
             <Breadcrumbs />
-
-            <div className="results-container">
-              <div className="row fixed-wrapper">
-                <div className="col-md-12 fdr fac">
-                  <div className="result-count-text">
-                    {!isLoading && (
-                      <ResultsCount
-                        searchQuery={searchQuery}
-                        metaCount={metaCount}
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div>
-                {trainings.length > 0 && (
-                  <div className="input-wrapper sorting-wrapper">
-                    <label className="usa-label" htmlFor="per-page">
-                      {t("SearchAndFilter.sortByLabel")}
-                    </label>
-
-                    <select
-                      className="usa-select"
-                      name="per-page"
-                      id="per-page"
-                      onChange={handleSortChange}
-                    >
-                      <option value="best_match">{t("SearchAndFilter.sortByBestMatch")}</option>
-                      <option value="asc">A to Z</option>
-                      <option value="desc">Z to A</option>
-                      <option value="price_asc">{t("SearchAndFilter.sortByCostLowToHigh")}</option>
-                      <option value="price_desc">{t("SearchAndFilter.sortByCostHighToLow")}</option>
-                      <option value="EMPLOYMENT_RATE">{t("SearchAndFilter.sortByEmploymentRate")}</option>
-                    </select>
-                  </div>
-                )}
-              </div>
-              <div>
-              {trainings.length > 0 && (
-                <div className="input-wrapper per-page-wrapper">
-                  <label className="usa-label" htmlFor="per-page">
-                    Results per page
-                  </label>
-
-                  <select
-                    className="usa-select"
-                    name="per-page"
-                    defaultValue={itemsPerPage}
-                    id="per-page"
-                    onChange={(e) => {
-                      setIsLoading(true);
-                      setItemsPerPage(
-                        e.target.options[e.target.selectedIndex].value as unknown as number,
-                      );
-                      const newUrl = new URL(window.location.href);
-                      newUrl.searchParams.set("p", "1");
-                      newUrl.searchParams.set("limit", e.target.value);
-                      window.history.pushState({}, "", newUrl.toString());
-                    }}
-                  >
-                    <option value="10">10</option>
-                    <option value="25">25</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                  </select>
-                </div>
+            <div className="results-count-selects-container">
+              {!isLoading && (
+                <ResultsCount
+                  searchQuery={searchQuery}
+                  metaCount={metaCount}
+                />
               )}
-              </div>
+              <SortByAndLimitSelects
+                handleSortChange={handleSortChange}
+                handleLimitChange={handleLimitChange}
+                itemsPerPage={itemsPerPage}
+                sortBy={sortBy}
+              />
             </div>
             <div>
               {isLoading ? (
@@ -212,7 +165,6 @@ export const SearchResultsPage = ({ client, location }: Props) : ReactElement<Pr
                 </div>
               ) : (
                 <div>
-                  <h1>Search Results</h1>
                   {trainings.map((training) => (
                     <TrainingResultCard
                       key={training.id}
