c8a84cc41bdf7b16d990167d8008530f2b64a115
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-05-31 11:23:46 -0400

    close on click off

--- frontend/src/filtering/FilterDrawer.tsx
+++ frontend/src/filtering/FilterDrawer.tsx
@@ -1,10 +1,12 @@
 import { ReactElement, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { FunnelSimple } from "@phosphor-icons/react";
+import { FunnelSimple, X } from "@phosphor-icons/react";
+import { Drawer, useMediaQuery } from "@material-ui/core";
 
 
 export const FilterDrawer = (): ReactElement => {
   const { t } = useTranslation();
+  const mobile = useMediaQuery("(max-width:767px)");
   const [open, setOpen] = useState<boolean>(false);
 
   const toggleDrawer = () => {
@@ -20,6 +22,23 @@ export const FilterDrawer = (): ReactElement => {
           {t("SearchResultsPage.filtersButton")} <FunnelSimple />
         </button>
       </div>
+      <Drawer
+        anchor={mobile ? "bottom" : "left"}
+        open={open}
+        onClose={toggleDrawer}
+      >
+        <div id="filter-drawer-container">
+          <div className="drawer-header">
+            <h2>Add Filters</h2>
+            <button
+              className="close-button"
+              onClick={toggleDrawer}
+            >
+              <X />
+            </button>
+          </div>
+        </div>
+      </Drawer>
     </>
   )
 }
