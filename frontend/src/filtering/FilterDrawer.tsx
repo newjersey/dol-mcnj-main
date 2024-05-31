93f9051ced791a380c75355ceb222257d97b4362
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-05-31 12:09:19 -0400

    readd pagination

--- frontend/src/filtering/FilterDrawer.tsx
+++ frontend/src/filtering/FilterDrawer.tsx
@@ -1,16 +1,36 @@
 import { ReactElement, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { FunnelSimple, X } from "@phosphor-icons/react";
+import { FormProvider, useForm } from "react-hook-form";
+import { FunnelSimple, MagnifyingGlass, X } from "@phosphor-icons/react";
 import { Drawer, useMediaQuery } from "@material-ui/core";
 
+import { FilterFormInput } from "./FilterFormInput";
 
-export const FilterDrawer = (): ReactElement => {
+interface Props {
+  searchQuery: string;
+}
+
+export const FilterDrawer = ({
+  searchQuery
+}: Props): ReactElement => {
   const { t } = useTranslation();
   const mobile = useMediaQuery("(max-width:767px)");
   const [open, setOpen] = useState<boolean>(false);
 
   const toggleDrawer = () => {
     setOpen(!open);
+  };
+
+  const methods = useForm<Props>({
+    defaultValues: {
+      searchQuery
+    }
+  });
+
+  const { handleSubmit } = methods;
+
+  const onSubmit = (data: Props) => {
+    console.log(data);
   };
 
   return (
@@ -25,7 +45,7 @@ export const FilterDrawer = (): ReactElement => {
       <Drawer
         anchor={mobile ? "bottom" : "left"}
         open={open}
-        onClose={toggleDrawer}
+        // onClose={toggleDrawer}
       >
         <div id="filter-drawer-container">
           <div className="drawer-header">
@@ -37,6 +57,24 @@ export const FilterDrawer = (): ReactElement => {
               <X />
             </button>
           </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div id="form-container">
+              <FormProvider {...methods}>
+                <div id="form-fields">
+                  <FilterFormInput
+                    inputLabel="Search by training, provider, certifcation, SOC code, or keyword"
+                    inputName="searchQuery"
+                    hasIcon={true}
+                    icon={<MagnifyingGlass />}
+                  />
+                </div>
+                <div id="drawer-btn-container" className="row">
+                  <button type="submit" id="submit-button">Apply Filters</button>
+                  <button type="reset" id="reset-button">Clear Filters</button>
+                </div>
+              </FormProvider>
+            </div>
+          </form>
         </div>
       </Drawer>
     </>
--- frontend/src/search-results/SearchResultsPage.tsx
+++ frontend/src/search-results/SearchResultsPage.tsx
@@ -1,7 +1,13 @@
-import { ReactElement, useEffect, useState } from "react";
+import {
+  ReactElement,
+  useContext,
+  useEffect,
+  useState
+} from "react";
 import { RouteComponentProps, WindowLocation } from "@reach/router";
 import { CircularProgress } from "@material-ui/core";
 
+import { ComparisonContext } from "../comparison/ComparisonContext";
 import { Layout } from "../components/Layout";
 import { Client } from "../domain/Client";
 import { TrainingResult, TrainingData } from "../domain/Training";
@@ -11,9 +17,12 @@ import { usePageTitle } from "../utils/usePageTitle";
 import { FilterDrawer } from "../filtering/FilterDrawer";
 
 import { Breadcrumbs } from "./Breadcrumbs";
+import { Pagination } from "./Pagination";
 import { ResultsHeader } from "./ResultsHeader";
 import { SearchTips } from "./SearchTips";
 import { SearchSelects } from "./SearchSelects";
+import { TrainingComparison } from "./TrainingComparison";
+import { TrainingResultCard } from "./TrainingResultCard";
 
 import { getPageTitle, getSearchQuery } from "./searchResultFunctions";
 
@@ -26,37 +35,45 @@ export const SearchResultsPage = ({ client, location }: Props): ReactElement<Pro
   const [isError, setIsError] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [metaData, setMetaData] = useState<TrainingData["meta"]>();
+  const [pageNumber, setPageNumber] = useState<number>();
   const [pageTitle, setPageTitle] = useState<string>(
     `Advanced Search | Training Explorer | ${process.env.REACT_APP_SITE_NAME}`,
   );
   const [trainings, setTrainings] = useState<TrainingResult[]>([]);
 
+  const comparisonState = useContext(ComparisonContext).state;
   const searchQuery = getSearchQuery(location?.search);
 
   usePageTitle(pageTitle);
 
   useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get("p");
+    
     setIsLoading(true)
-    const queryToSearch = searchQuery ? searchQuery : "";
+    setPageNumber(page ? parseInt(page) : 1);
 
-    if (queryToSearch && queryToSearch !== "null") {
-      client.getTrainingsByQuery(
-        queryToSearch,
-        {
-          onSuccess: ({ data, meta }: TrainingData) => {
-            setTrainings(data);
-            setMetaData(meta);
-            getPageTitle(setPageTitle, searchQuery);
-            setIsLoading(false);
-          },
-          onError: () => {
-            setIsError(true);
-          },
-        }
-      );
+    if (pageNumber) {
+      const queryToSearch = searchQuery ? searchQuery : "";
+  
+      if (queryToSearch && queryToSearch !== "null") {
+        client.getTrainingsByQuery(
+          queryToSearch,
+          {
+            onSuccess: ({ data, meta }: TrainingData) => {
+              setTrainings(data);
+              setMetaData(meta);
+              getPageTitle(setPageTitle, searchQuery);
+              setIsLoading(false);
+            },
+            onError: () => {
+              setIsError(true);
+            },
+          }
+        );
+      }
     }
-    // setIsLoading(false);
-  }, [searchQuery, client]);
+  }, [searchQuery, client, pageNumber]);
 
   return (
     <Layout
@@ -97,7 +114,9 @@ export const SearchResultsPage = ({ client, location }: Props): ReactElement<Pro
           {!isLoading
             && trainings.length > 0
             && (
-            <FilterDrawer />
+            <FilterDrawer
+              searchQuery={searchQuery || ""}
+            />
           )}
         </div>
         <div id="results-container">
@@ -112,7 +131,38 @@ export const SearchResultsPage = ({ client, location }: Props): ReactElement<Pro
             && (
               <SearchTips />
           )}
+          {!isLoading
+            && !isError
+            && trainings.length > 0
+            && (
+            <div id="results-list">
+              {trainings.map((training) => (
+                <TrainingResultCard
+                  key={training.id}
+                  trainingResult={training}
+                  comparisonItems={comparisonState.comparison}
+                />
+              ))}
+              {!isLoading
+                && trainings.length > 0
+                && pageNumber
+                && (
+                <Pagination
+                  isLoading={isLoading}
+                  setIsLoading={setIsLoading}
+                  currentPage={pageNumber || 1}
+                  totalPages={metaData?.totalPages || 0}
+                  hasPreviousPage={metaData?.hasPreviousPage || false}
+                  hasNextPage={metaData?.hasNextPage || false}
+                  setPageNumber={setPageNumber}
+                />
+              )}
+            </div>
+          )}
         </div>
+        {!isLoading && (
+          <TrainingComparison comparisonItems={comparisonState.comparison} />
+        )}
       </div>
   </Layout>
   );
