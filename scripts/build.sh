0747f69e69a20f33670a92feb5fa4f25f361c707
chelseakr <chelsea.kelly-reif@coforma.io>
2024-06-03 09:56:19 -0700

    source node_options for build proc

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# Source the .env file
+source ./backend/.env
+
+# Export NODE_OPTIONS from the sourced environment variables
+export NODE_OPTIONS
+
 cd $(git rev-parse --show-toplevel)
 npm --prefix=frontend run build
 npm --prefix=backend run build
