7fe500bc1b5d71cff2fa3cff4b10100eccd71c32
denish-fearless <dsubedi@fearless.tech>
2024-06-03 22:03:50 -0400

    configuring SMTP using Nodemailer instead of SES API

--- backend/src/utils/emailService.ts
+++ backend/src/utils/emailService.ts
@@ -1,37 +1,35 @@
-import AWS from 'aws-sdk';
-
-AWS.config.update({ region: 'us-east-1' });
-const ses = new AWS.SES();
+import Nodemailer from "nodemailer"
 
 interface EmailParams {
-  subject: string;
-  body: string;
+  subject: string,
+  body: string
 }
 
-export async function sendEmail(emailParams: EmailParams): Promise<void>{
-  if(!process.env.EMAIL_SOURCE) {
-     throw 'Email Source not defined'
-  }
-  if(!process.env.CONTACT_RECEIVER_EMAIL) {
-    throw 'Email Receiver not defined'
- }
-  const params: AWS.SES.SendEmailRequest = {
-    Source: process.env.EMAIL_SOURCE, 
-    Destination: {
-      ToAddresses: [process.env.CONTACT_RECEIVER_EMAIL],
-    },
-    Message: {
-      Subject: {
-        Data: emailParams.subject,
-      },
-      Body: {
-        Text: {
-          Data: emailParams.body,
-        },
-      },
-    },
-  };
+const transporter = Nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT as string, 10) || 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
-  await ses.sendEmail(params).promise();
-  console.log('Email sent successfully');
+export async function sendEmail(emailParams: EmailParams): Promise<void> {
+  if (!process.env.EMAIL_SOURCE) {
+    throw 'Email Source not defined'
+  }
+  if (!process.env.CONTACT_RECEIVER_EMAIL) {
+    throw 'Email Receiver not defined'
+  }
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_SOURCE,
+      to: process.env.CONTACT_RECEIVER_EMAIL,
+      subject: emailParams.subject,
+      text: emailParams.body,
+    })
+  } catch (error) {
+    console.log("Error Sending Email Via SES", error)
+  }
 }
