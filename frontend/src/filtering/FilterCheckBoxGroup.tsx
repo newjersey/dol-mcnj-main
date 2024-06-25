0a5e8de1c666b3c7059d1ef4436cb3b37cb59bc7
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-06-25 09:55:07 -0400

    add county dropdown

---
+++ frontend/src/filtering/FilterCheckBoxGroup.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Checkbox } from "@material-ui/core";
+import { useFormContext } from 'react-hook-form';
+
+interface Props {
+  inputName: string;
+  defaultValues?: string[];
+  inputLabel?: string;
+  options: {id: string, label: string}[];
+}
+
+export const FilterFormCheckGroup = ({
+  inputName,
+  defaultValues = [],
+  inputLabel,
+  options
+}: Props) => {
+  const { setValue } = useFormContext();
+  const lowerCaseValues = defaultValues.map(value => value.toLowerCase());
+  const [selected, setSelected] = useState(lowerCaseValues);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.id;
+    const index = selected.indexOf(value);
+    const newSelected = [...selected];
+
+    if (index === -1) {
+      newSelected.push(value);
+    } else {
+      newSelected.splice(index, 1);
+    }
+
+    setSelected(newSelected);
+    setValue(inputName, newSelected);
+  }
+
+
+  return (
+    <div className="field-group">
+      {inputLabel && (
+        <div className="label-container">
+          <label htmlFor={inputName}>
+            {inputLabel}
+          </label>
+        </div>
+      )}
+      <div className="input-wrapper">
+        {options.map((option) => (
+          <div key={option.id} className="checkbox-container">
+            <Checkbox
+              id={option.id}
+              name={inputName}
+              value={option.id}
+              checked={selected.includes(option.id)}
+              onChange={handleCheckboxChange}
+            />
+            <label htmlFor={option.id}>{option.label}</label>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
