541f3cca55341bd069f2b8e6d38f65f73c8af91b
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-07-18 11:34:38 -0400

    remove filter float button

--- frontend/src/filtering/FilterButton.tsx
+++ frontend/src/filtering/FilterButton.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement} from "react";
 import { FunnelSimple } from "@phosphor-icons/react";
 import { useTranslation } from "react-i18next";
 
@@ -10,30 +10,10 @@ export const FilterButton = ({
   toggleDrawer
 }: Props): ReactElement<Props> => {
   const { t } = useTranslation();
-
-  const [isFloating, setIsFloating] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 200) {
-        setIsFloating(true);
-      } else {
-        setIsFloating(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div id="filter-button-container">
       <button
         onClick={toggleDrawer}
-        className={isFloating ? "floating" : ""}
       >
         {t("SearchResultsPage.filtersButton")} <FunnelSimple />
       </button>
