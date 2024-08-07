e27ff15a2f16c5b12d4b95031a38eb1a6fa40648
Scott Wambach <scott@scottwamba.ch>
2024-08-07 12:55:51 -0500

    Update SearchBlock.test.tsx

--- frontend/src/components/SearchBlock.test.tsx
+++ frontend/src/components/SearchBlock.test.tsx
@@ -1,72 +1,89 @@
 // Import necessary libraries and types
-import React from 'react';
-import {fireEvent, render, screen} from '@testing-library/react';
-import {act} from 'react-dom/test-utils';
-import {SearchBlock} from './SearchBlock';
-import type {ContentfulRichText} from '../types/contentful';
-import {BLOCKS} from "@contentful/rich-text-types";
-
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { SearchBlock } from "./SearchBlock";
+import type { ContentfulRichText } from "../types/contentful";
+import { BLOCKS } from "@contentful/rich-text-types";
 
 // Define the test data for the drawer content
 const testDrawerContent: ContentfulRichText = {
   json: {
     data: {},
-    content: [{
-      data: {},
-      content: [{
+    content: [
+      {
         data: {},
-        marks: [],
-        value: "SOC code",
-        nodeType: "text"
-      }],
-      nodeType: BLOCKS.HEADING_3
-    }, {
-      data: {},
-      content: [{
+        content: [
+          {
+            data: {},
+            marks: [],
+            value: "SOC code",
+            nodeType: "text",
+          },
+        ],
+        nodeType: BLOCKS.HEADING_3,
+      },
+      {
         data: {},
-        marks: [],
-        value: "The \"Standard Occupational Classification\" system is used to categorize occupations.",
-        nodeType: "text"
-      }],
-      nodeType: BLOCKS.PARAGRAPH
-    }, {
-      data: {},
-      content: [{
+        content: [
+          {
+            data: {},
+            marks: [],
+            value:
+              'The "Standard Occupational Classification" system is used to categorize occupations.',
+            nodeType: "text",
+          },
+        ],
+        nodeType: BLOCKS.PARAGRAPH,
+      },
+      {
         data: {},
-        marks: [],
-        value: "You can find a list of SOC codes ",
-        nodeType: "text"
-      }, {
-        data: {
-          uri: "https://www.bls.gov/oes/current/oes_stru.htm"
-        },
-        content: [{
-          data: {},
-          marks: [],
-          value: "here",
-          nodeType: "text"
-        }],
-        nodeType: BLOCKS.PARAGRAPH
-      }, {
+        content: [
+          {
+            data: {},
+            marks: [],
+            value: "You can find a list of SOC codes ",
+            nodeType: "text",
+          },
+          {
+            data: {
+              uri: "https://www.bls.gov/oes/current/oes_stru.htm",
+            },
+            content: [
+              {
+                data: {},
+                marks: [],
+                value: "here",
+                nodeType: "text",
+              },
+            ],
+            nodeType: BLOCKS.PARAGRAPH,
+          },
+          {
+            data: {},
+            marks: [],
+            value: ".",
+            nodeType: "text",
+          },
+        ],
+        nodeType: BLOCKS.PARAGRAPH,
+      },
+      {
         data: {},
-        marks: [],
-        value: ".",
-        nodeType: "text"
-      }],
-      nodeType: BLOCKS.PARAGRAPH
-    }, {
-      data: {},
-      content: [{
-        data: {},
-        marks: [],
-        value: "",
-        nodeType: "text"
-      }],
-      nodeType: BLOCKS.PARAGRAPH
-    }],
-    nodeType: BLOCKS.DOCUMENT
+        content: [
+          {
+            data: {},
+            marks: [],
+            value: "",
+            nodeType: "text",
+          },
+        ],
+        nodeType: BLOCKS.PARAGRAPH,
+      },
+    ],
+    nodeType: BLOCKS.DOCUMENT,
   },
-  links: { assets: { block: [] } }
+  links: { assets: { block: [] } },
 };
 
 describe("SearchBlock", () => {
@@ -75,14 +92,13 @@ describe("SearchBlock", () => {
   // Mock assign function
   let assignMock: jest.Mock;
 
-
   beforeAll(() => {
     // Save the original location
     originalLocation = window.location;
 
     // Mock the window.location with a jest.fn() for each function you need, for example, assign
     // Use Object.defineProperty to bypass the type error without using 'as any'
-    Object.defineProperty(window, 'location', {
+    Object.defineProperty(window, "location", {
       value: {
         ...originalLocation,
         assign: jest.fn(),
@@ -124,7 +140,7 @@ describe("SearchBlock", () => {
     const searchInput = screen.getByTestId("search-input") as HTMLInputElement;
     fireEvent.change(searchInput, { target: { value: "test" } });
 
-    const clearAllButton = screen.getByText("Clear All");
+    const clearAllButton = screen.getByText("Clear all");
     fireEvent.click(clearAllButton);
 
     expect(searchInput.value).toBe("");
