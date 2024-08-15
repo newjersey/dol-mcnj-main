6d9e55a9fd6cbff83c76d29b32692ae3f489fee5
Scott Wambach <scott@scottwamba.ch>
2024-08-15 13:36:43 -0500

    skeleton

--- frontend/src/search-results/ResultsHeader.tsx
+++ frontend/src/search-results/ResultsHeader.tsx
@@ -3,11 +3,11 @@ import { useTranslation } from "react-i18next";
 export const ResultsHeader = ({
   loading,
   searchQuery,
-  metaCount
-}:{
-  loading: boolean,
-  searchQuery: string,
-  metaCount: number
+  metaCount,
+}: {
+  loading: boolean;
+  searchQuery: string;
+  metaCount: number;
 }) => {
   const { t } = useTranslation();
   const query = decodeURIComponent(searchQuery.replace(/\+/g, " "));
@@ -19,12 +19,16 @@ export const ResultsHeader = ({
   return (
     <div id="search-header-container">
       <h2 className="text-xl weight-500 pts cutoff-text">
-        {!searchQuery || searchQuery === "null" || loading ? (
-          t("SearchResultsPage.noSearchTermHeader")
+        {loading ? (
+          "Searching training data for your results..."
         ) : (
-          message
+          <>
+            {!searchQuery || searchQuery === "null"
+              ? t("SearchResultsPage.noSearchTermHeader")
+              : message}
+          </>
         )}
       </h2>
     </div>
-  )
+  );
 };
