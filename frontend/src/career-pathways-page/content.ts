2245619acf9d65f20f76417640b77fde739eec80
Scott Wambach <scott@scottwamba.ch>
2024-09-20 16:02:45 -0500

    landing page

--- frontend/src/career-pathways-page/content.ts
+++ frontend/src/career-pathways-page/content.ts
@@ -1,6 +1,8 @@
 import manufacturingImage from "../images/welder.jpg";
 import healthcareImage from "../images/medical.jpg";
 import tdlImage from "../images/mechanic.jpg";
+import { SelectorProps } from "../svg/Selector";
+
 export const content = {
   banner: {
     title: "Welcome to New Jersey Career Pathways!",
@@ -19,6 +21,17 @@ export const content = {
         active: true,
         description:
           "Explore career pathways in the field of Manufacturing in the state of New Jersey.",
+        drawerCards: [
+          {
+            icon: "manufacturing" as SelectorProps["name"],
+            title: "Why to consider a Manufacturing Career",
+            copy: "### Growth potential\n\nA lot of manufacturing companies offer on-site job training, so there is always room to grow and advance your career.\n\n### Cutting edge\n\nInnovative new technologies like robotics, wearables, 3D printing, and drones get developed and used in manufacturing.\n\n### Gratifying\n\nYou can actually see the outcome of your work in a tangible object. People who work in the field can say, “I made that.”",
+          },
+          {
+            title: "Top Manufacturing Sectors",
+            copy: "### Food\n\nFood manufacturers pick New Jersey because the state is at the center of one of the nation’s largest metropolitan areas. And so, food produced in New Jersey easily reaches millions of people. Our state is home to some of the world’s leading food brands like Campbells Soup, Mars Bars, M&Ms, and Nabisco – all made right here in the state. New and upcoming brands like Oatly and Bai started here too\n\n### Pharmaceuticals\n\nNew Jersey is known as “The Medicine Chest of the World” because of its importance to the global pharmaceutical industry. Novartis, Merck & Co., Johnson & Johnson, Bristol-Myers Squibb, Novo Nordisk, and Bayer Healthcare are among the state’s leading firms in the industry. Pharmaceutical companies are in New Jersey because the state has a highly educated workforce, including scientists.### Fabricated Metals\n\nMetal manufacturers transform metals into parts that are used in airplanes, bridges, buildings, computers, machinery, ships, power generation, and more.  Companies in this industry forge, stamp, bend, weld, shape, and machine metals. Nowadays, metal manufacturers achieve a level of precision that was undreamed of just 20 years ago to do things like lathing, 3-D printing, plasma cutting, and milling using computer numerical control (CNC) devices.",
+          },
+        ],
       },
       {
         image: healthcareImage,
@@ -27,6 +40,7 @@ export const content = {
         active: false,
         description:
           "Pathways for this sector are in development but you can still explore the most in-demand healthcare occupations.",
+        drawerCards: [],
       },
       {
         image: tdlImage,
@@ -36,22 +50,11 @@ export const content = {
         active: false,
         description:
           "Pathways for this sector are in development but you can still explore the most in-demand TDL occupations.",
+        drawerCards: [],
       },
     ],
   },
-  markdownSection: `
-  ## What are Career Pathways?
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
-
-  ## Methodology
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
-
-  ## Data
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
-  `,
+  markdownSection: `## Career Pathways and Industry Sectors—what’s the difference, exactly?\n\n### Think of Career Pathways as a GPS.\n\nCareer Pathways offers the best route to reach your career destination, outlining the steps you need to take to get where you want to go, including education, training, and upskilling.\n\n### Think of Industry Sector Information as a guidebook.\n\nIndustry Sector Information provides an overview of various industries, including a snapshot of the job market. Here’s some of the helpful information you’ll find:\n\n- Types of jobs\n- Salary expectations\n- Specific skills in high demand\n- Companies that are the major players\n- Potential for Industry growth\n\n## It’s important to make informed choices. See how Career Pathways and Industry Sector Information can help.\n\nThrough Career Pathways and Industry Sector Information, you’ll be able to make impactful, data-driven decisions. For instance, if you choose to explore the Manufacturing Career Pathway, you’ll discover:\n- Information about the most in-demand manufacturing jobs in New Jersey\n- Insights from over 40 manufacturing companies throughout New Jersey\n- Valuable data about available jobs\n- The skills, education, and experience you need to get started.\n\n## Our legwork can help you get a leg up in your career.\n\nOur data comes from trusted sources with the most up-to-date information:\n\n- Experienced New Jersey labor market analysts who study job trends\n- Actual New Jersey employers\n- Recent employment and wage surveys\n\nEveryone’s career path is different. With Career Pathways and Industry Sector Information, you’ll have what you need to help you move forward successfully.`,
   cta: {
     heading: "Still have a question about My Career NJ?",
     button: {
--- frontend/src/components/IndustrySelector.tsx
+++ frontend/src/components/IndustrySelector.tsx
@@ -1,8 +1,10 @@
-import { ArrowRight, DotsThreeVertical, X } from "@phosphor-icons/react";
+import { ArrowRight, DotsThreeVertical, Fire, X } from "@phosphor-icons/react";
 import { content } from "../career-pathways-page/content";
 import { Heading } from "./modules/Heading";
 import { LinkObject } from "./modules/LinkObject";
 import { useEffect, useState } from "react";
+import { Selector, SelectorProps } from "../svg/Selector";
+import { parseMarkdownToHTML } from "../utils/parseMarkdownToHTML";
 // import { Accordion } from "./Accordion";
 // import { Selector } from "../svg/Selector";
 
@@ -13,6 +15,7 @@ const Panel = ({
   description,
   title,
   shorthandTitle,
+  drawerCards,
 }: {
   open: boolean;
   photo: string;
@@ -20,6 +23,11 @@ const Panel = ({
   title: string;
   setOpen: (index: number | null) => void;
   shorthandTitle?: string;
+  drawerCards: {
+    icon?: SelectorProps["name"];
+    title: string;
+    copy: string;
+  }[];
 }) => {
   return (
     <>
@@ -46,7 +54,20 @@ const Panel = ({
             <p>{description}</p>
             <img src={photo} alt={title} className="photo" />
           </div>
-          <div className="accordion-wrapper">test</div>
+          <div className="accordion-wrapper">
+            {drawerCards.map((card) => (
+              <div className="box">
+                <div className="heading-bar">
+                  {card.icon ? <Selector name={card.icon} /> : <Fire size={32} />}
+                  {card.title}
+                </div>
+                <div
+                  className="content"
+                  dangerouslySetInnerHTML={{ __html: parseMarkdownToHTML(card.copy) }}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
@@ -78,53 +99,53 @@ export const IndustrySelector = () => {
       <div className="container">
         <div className="inner">
           <Heading level={2}>{content.industrySelector.heading}</Heading>
-          <nav aria-label="Industry Navigation" id="industry-nav">
-            <ul className="unstyled">
-              {content.industrySelector.items.map((props, index) => {
-                const { image, title, slug, description, active } = props;
-                return (
-                  <li key={image}>
-                    <div>
-                      <div className="heading-section">
-                        <p className="title">{title}</p>
-                        <span className={`tag-item color-${active ? "purple" : "navy"}`}>
-                          {active ? "Pathway included" : "Pathway coming soon"}
-                        </span>
-                        <div className="image">
-                          <img src={image} alt={title} />
-                        </div>
+
+          <ul className="unstyled">
+            {content.industrySelector.items.map((props, index) => {
+              const { image, title, slug, description, active, drawerCards } = props;
+              return (
+                <li key={image}>
+                  <div>
+                    <div className="heading-section">
+                      <p className="title">{title}</p>
+                      <span className={`tag-item color-${active ? "purple" : "navy"}`}>
+                        {active ? "Pathway included" : "Pathway coming soon"}
+                      </span>
+                      <div className="image">
+                        <img src={image} alt={title} />
                       </div>
-                      <p>{description}</p>
                     </div>
-                    <div className="buttons">
-                      <LinkObject
-                        url={`/career-pathways/${slug}`}
-                        className="usa-button margin-right-0 primary"
-                      >
-                        Explore <ArrowRight size={22} />
-                      </LinkObject>
-                      <button
-                        className="usa-button usa-button--outline bg-white margin-right-0 primary"
-                        onClick={() => {
-                          setActivePanel(index);
-                        }}
-                      >
-                        Learn more <DotsThreeVertical size={22} weight="bold" />
-                      </button>
-                    </div>
-                    <Panel
-                      open={activePanel === index}
-                      setOpen={setActivePanel}
-                      photo={image}
-                      description={description}
-                      title={title}
-                      shorthandTitle={slug}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
+                    <p>{description}</p>
+                  </div>
+                  <div className="buttons">
+                    <LinkObject
+                      url={`/career-pathways/${slug}`}
+                      className="usa-button margin-right-0 primary"
+                    >
+                      Explore <ArrowRight size={22} />
+                    </LinkObject>
+                    <button
+                      className="usa-button usa-button--outline bg-white margin-right-0 primary"
+                      onClick={() => {
+                        setActivePanel(index);
+                      }}
+                    >
+                      Learn more <DotsThreeVertical size={22} weight="bold" />
+                    </button>
+                  </div>
+                  <Panel
+                    open={activePanel === index}
+                    drawerCards={drawerCards}
+                    setOpen={setActivePanel}
+                    photo={image}
+                    description={description}
+                    title={title}
+                    shorthandTitle={slug}
+                  />
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </section>
