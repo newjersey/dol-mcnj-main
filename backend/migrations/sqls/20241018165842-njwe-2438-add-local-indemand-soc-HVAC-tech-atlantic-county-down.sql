65febc335bdd346abfad79176c07bfc001edbfd3
ChelseaKR <chelsea.kelly-reif@coforma.io>
2024-10-18 10:05:25 -0700

    njwe-2438 initial commit

---
+++ backend/migrations/sqls/20241018165842-njwe-2438-add-local-indemand-soc-HVAC-tech-atlantic-county-down.sql
@@ -0,0 +1,15 @@
+DELETE FROM localexceptioncips 
+WHERE soc = '49-9021' 
+  AND occupation = 'Heating, Air Conditioning, and Refrigeration Mechanics and Installers'
+  AND county = 'ATLANTIC'
+  AND cip = '15.0501' 
+  AND cipdescription = 'Heating, Ventilation, Air Conditioning and Refrigeration Engineering Technology/Technician.'
+  AND cipcode = '150501';
+
+DELETE FROM localexceptioncips 
+WHERE soc = '49-9021' 
+  AND occupation = 'Heating, Air Conditioning, and Refrigeration Mechanics and Installers'
+  AND county = 'ATLANTIC'
+  AND cip = '47.0201' 
+  AND cipdescription = 'Heating, Air Conditioning, Ventilation and Refrigeration Maintenance Technology/Technician.'
+  AND cipcode = '470201';
