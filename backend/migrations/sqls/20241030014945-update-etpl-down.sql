b86d0875f9e9a697d6f8463618592d30e20b2709
ChelseaKR <chelsea.kelly-reif@coforma.io>
2024-10-29 18:56:06 -0700

    etpl data 2024-10-29

---
+++ backend/migrations/sqls/20241030014945-update-etpl-down.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:efad4bb298b89327779f25db0f209cfbfdc7c73169c40ce9dee226607a00b522
+size 77753177
---
+++ backend/migrations/sqls/20241030014945-update-etpl-up.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd25c5bc8e2d9afc852578488b24e50f9a60c143d97dcb4f2770dc4ad010a867
+size 77754140
