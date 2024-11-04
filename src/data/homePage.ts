2b290083962d3dec960e48ba5d933ef668b89a48
Scott Wambach <scott@scottwamba.ch>
2024-11-04 12:09:33 -0600

    data

---
+++ src/data/homePage.ts
@@ -0,0 +1,548 @@
+export const homePage = {
+  title: "My Career NJ",
+  pageDescription:
+    "Explore My Career NJ to find job training, career resources, and employment opportunities with the New Jersey Department of Labor.",
+  keywords: [
+    "Career",
+    "Job Training",
+    "Resources",
+    "Tuition Assistance",
+    "Career Pathways",
+    "Training Explorer",
+    "New Jersey Training Explorer",
+    "Career Navigator",
+    "My Career NJ",
+    "jobs",
+    "nj jobs",
+    "Career Resources NJ",
+    "Job Training NJ",
+    "New Jersey Career Pathways",
+    "NJ Training Explorer",
+    "Career Navigator NJ",
+    "Tuition Assistance NJ",
+    "Apprenticeship Programs NJ",
+    "New Jersey Department of Labor",
+    "NJ Job Search Tools",
+    "In-Demand Occupations NJ",
+    "Professional Development NJ",
+    "Career Support NJ",
+    "NJ Career Guidance",
+    "Job Search Help NJ",
+    "Career Services NJ",
+    "NJ Workforce Development",
+    "Free Online Training NJ",
+    "NJ Labor Market Information",
+    "Community College Assistance NJ",
+    "Training Provider Resources NJ",
+    "Career resources in Bergen County",
+    "Job training in Passaic County",
+    "Workforce development in Hudson County",
+    "Employment opportunities in Essex County",
+    "Career Navigator in Morris County",
+    "Job assistance in Middlesex County",
+    "Training programs in Somerset County",
+    "Career services in Mercer County",
+    "Employment support in Monmouth County",
+    "Workforce development in Union County",
+    "Career resources in Camden County",
+    "Job training in Burlington County",
+    "Workforce development in Atlantic County",
+    "Employment opportunities in Gloucester County",
+    "Career Navigator in Ocean County",
+    "Employment services in Newark, NJ",
+    "Job support in Jersey City, NJ",
+    "Career training in Trenton, NJ",
+    "Job assistance in Atlantic City, NJ",
+    "Workforce programs in Paterson, NJ",
+  ],
+  bannerButtonCopy: "Explore My Career NJ",
+  introBlocks: {
+    heading:
+      "No matter where you are on your career journey, My Career NJ is here to help you.",
+    message:
+      "Explore a connected suite of career, job, and training tools to help level up your career.",
+    sections: [
+      {
+        link: {
+          url: "/training",
+          copy: "Visit the NJ Training Explorer",
+        },
+        title: "NJ Training Explorer",
+        heading:
+          "Certifications, Professional Development, Apprenticeships & More!",
+        message:
+          "Search by job, training program, and more to find a training that works for you. In-demand trainings may even be eligible for funding assistance. ",
+      },
+      {
+        link: {
+          url: "/navigator",
+          copy: "Visit the NJ Career Navigator",
+        },
+        title: "NJ Career Navigator",
+        heading: "Personalized Career Recommendations",
+        message:
+          "Get personalized, data-driven career recommendations based on your skills and experience.",
+      },
+      {
+        link: {
+          url: "/career-pathways",
+          copy: "Visit the NJ Career Pathways",
+        },
+        title: "NJ Career Pathways",
+        heading: "Explore Pathways to In-Demand Careers",
+        message:
+          "Uncover career opportunities and growth potential in key industries across New Jersey. From healthcare to manufacturing, Career Pathways provides the roadmap to help you navigate your journey to success.",
+      },
+    ],
+    sectionsHeading: "How My Career NJ can help you:",
+  },
+  ogImage: {
+    sys: {
+      id: "buiOl5KtlbWd2n75TzD61",
+    },
+    url: "https://images.ctfassets.net/jbdk7q9c827d/buiOl5KtlbWd2n75TzD61/8614058229c32e1d4cfe2bb035b7746c/0d4a1adf-de41-46a6-b45a-75015bf737b3.png",
+    title: "My Career NJ Title Card",
+    width: 2400,
+    height: 1260,
+    fileName: "0d4a1adf-de41-46a6-b45a-75015bf737b3.png",
+    contentType: "image/png",
+  },
+  bannerImage: {
+    url: "https://images.ctfassets.net/jbdk7q9c827d/JAzMHNRnrmPy5KfO7l3PZ/c39660e2d16c0b5bf98fe3c05ab38dc3/NJCC_Hero_Image.png",
+    description: "Four images of people working in various industries",
+    height: 521,
+    width: 750,
+  },
+  toolsCollection: {
+    items: [
+      {
+        sys: {
+          id: "26zkyxkXyFqjo9OnzOc7Bw",
+        },
+        icon: null,
+        sectionIcon: "jobs",
+        copy: "Find a Job",
+        url: "#jobs",
+        description: null,
+      },
+      {
+        sys: {
+          id: "5wJtrE4cjzjKbSEae06Nhd",
+        },
+        icon: null,
+        sectionIcon: "training",
+        copy: "Get Training",
+        url: "#training",
+        description: null,
+      },
+      {
+        sys: {
+          id: "6kMVEjI0gxExoVUbybTy4M",
+        },
+        icon: null,
+        sectionIcon: "explore",
+        copy: "Explore Careers",
+        url: "#explore",
+        description: null,
+      },
+      {
+        sys: {
+          id: "4VOdOmvmcnBx65REzMUS5e",
+        },
+        icon: null,
+        sectionIcon: "support",
+        copy: "Find Support and Assistance",
+        url: "#support",
+        description: null,
+      },
+    ],
+  },
+  tools: [
+    {
+      heading: "All Job Search Tools",
+      theme: "blue",
+      sectionId: "jobs",
+      cards: [
+        {
+          sys: {
+            id: "3iK2gvVLLjtCnJtFRZ5S1h",
+          },
+          sectionIcon: "jobs",
+          icon: null,
+          copy: "NJ Career Navigator",
+          url: "/navigator",
+          description: "Get personalized career change recommendations",
+        },
+        {
+          sys: {
+            id: "4HeyegJGi8NXaDvUCtKKUa",
+          },
+          sectionIcon: "jobs",
+          icon: null,
+          copy: " In-Demand Occupations List",
+          url: "/in-demand-occupations",
+          description: "See careers with openings now and in the future in NJ",
+        },
+        {
+          sys: {
+            id: "3AmMT5psdguST2Rrsok5R5",
+          },
+          sectionIcon: "jobs",
+          icon: null,
+          copy: "CareerOneStop Job Board",
+          url: "https://www.careeronestop.org/Toolkit/Jobs/find-jobs-results.aspx?keyword=&location=New%20Jersey&radius=25&source=NLX&curPage=1&referer=%2FToolkit%2FJobs%2Ffind-jobs.aspx",
+          description: "Search for current job openings in New Jersey",
+        },
+        {
+          sys: {
+            id: "4FzcfTXyculFqFlnWgcdo9",
+          },
+          sectionIcon: "jobs",
+          icon: null,
+          copy: "Labor Market Information",
+          url: "https://www.nj.gov/labor/labormarketinformation/",
+          description: "Learn about the labor market in NJ",
+        },
+        {
+          sys: {
+            id: "7eLHEa4Hr7d1Fv09qp7c5Y",
+          },
+          sectionIcon: "jobs",
+          icon: null,
+          copy: "Apprenticeship Programs",
+          url: "https://www.nj.gov/labor/career-services/apprenticeship/",
+          description: "Learn about apprenticeship opportunities",
+        },
+      ],
+    },
+    {
+      heading: "All Training Tools",
+      theme: "green",
+      sectionId: "training",
+      cards: [
+        {
+          sys: {
+            id: "5QTcssKSYsIg8np1ggO2UF",
+          },
+          sectionIcon: "training",
+          icon: null,
+          copy: "NJ Training Explorer",
+          url: "/training",
+          description: "Search for training programs in NJ",
+        },
+        {
+          sys: {
+            id: "3vq84kWYDEB52xrvLCFfDU",
+          },
+          sectionIcon: "training",
+          icon: null,
+          copy: "NJ Career Navigator",
+          url: "/navigator",
+          description: "Get personalized career change recommendations",
+        },
+        {
+          sys: {
+            id: "1pZCOFlrVUNAKFNKJjxIXI",
+          },
+          sectionIcon: "training",
+          icon: null,
+          copy: "Tuition Assistance Resources",
+          url: "/support-resources/tuition-assistance",
+          description: "Learn about ways to get help paying for education",
+        },
+        {
+          sys: {
+            id: "3ynJm1nUCMfmzVVznbAmNI",
+          },
+          sectionIcon: "training",
+          icon: null,
+          copy: "Training Provider Resources",
+          url: "/training-provider-resources",
+          description: "Important information for training providers",
+        },
+        {
+          sys: {
+            id: "35fYExqOs6GFx6fvsOvtpT",
+          },
+          sectionIcon: "training",
+          icon: null,
+          copy: "SkillUp",
+          url: "https://nj.metrixlearning.com/landing.cfm",
+          description: "Free on-line training videos",
+        },
+      ],
+    },
+    {
+      heading: "All Career Exploration Resources",
+      theme: "purple",
+      sectionId: "explore",
+      cards: [
+        {
+          sys: {
+            id: "3iK2gvVLLjtCnJtFRZ5S1h",
+          },
+          icon: null,
+          sectionIcon: "jobs",
+          copy: "NJ Career Navigator",
+          url: "/navigator",
+          description: "Get personalized career change recommendations",
+        },
+        {
+          sys: {
+            id: "1FwclNJoyvGOX2NY5qOkBt",
+          },
+          icon: "MapTrifold",
+          sectionIcon: "explore",
+          copy: "NJ Career Pathways",
+          url: "/career-pathways",
+          description: "Explore in-demand jobs and training opportunities.",
+        },
+      ],
+    },
+    {
+      heading: "All Support and Assistance Resources",
+      theme: "navy",
+      sectionId: "support",
+      cards: [
+        {
+          sys: {
+            id: "65ombyNutpQ2adGMwCzJw5",
+          },
+          icon: null,
+          sectionIcon: "support",
+          copy: "Browse Support by Category",
+          url: "/support-resources",
+          description: null,
+        },
+        {
+          sys: {
+            id: "6tH2KP1p9PfAUZyI2AbHUi",
+          },
+          icon: null,
+          sectionIcon: "support",
+          copy: "Career Support",
+          url: "/support-resources/career-support",
+          description:
+            "Career Guidance, Job Search Help, and Re-entering the Workforce",
+        },
+        {
+          sys: {
+            id: "1yqrHQv5pPbq6BaNmXhR5Y",
+          },
+          icon: null,
+          sectionIcon: "support",
+          copy: "Tuition Assistance",
+          url: "/support-resources/tuition-assistance",
+          description: "Job Training, Community College, and More",
+        },
+        {
+          sys: {
+            id: "2KQhAwOxDFbPUiuQoI0lNF",
+          },
+          icon: null,
+          sectionIcon: "support",
+          copy: "Other Assistance",
+          url: "/support-resources/other",
+          description: "Housing, Transportation, Healthcare, and More",
+        },
+        {
+          sys: {
+            id: "57KxzbJWCglLxqVad6TbiT",
+          },
+          icon: null,
+          sectionIcon: "support",
+          copy: "Frequently Asked Questions",
+          url: "/faq",
+          description: null,
+        },
+      ],
+    },
+  ],
+  jobSearchToolLinksCollection: {
+    items: [
+      {
+        sys: {
+          id: "3iK2gvVLLjtCnJtFRZ5S1h",
+        },
+        sectionIcon: "jobs",
+        icon: null,
+        copy: "NJ Career Navigator",
+        url: "/navigator",
+        description: "Get personalized career change recommendations",
+      },
+      {
+        sys: {
+          id: "4HeyegJGi8NXaDvUCtKKUa",
+        },
+        sectionIcon: "jobs",
+        icon: null,
+        copy: " In-Demand Occupations List",
+        url: "/in-demand-occupations",
+        description: "See careers with openings now and in the future in NJ",
+      },
+      {
+        sys: {
+          id: "3AmMT5psdguST2Rrsok5R5",
+        },
+        sectionIcon: "jobs",
+        icon: null,
+        copy: "CareerOneStop Job Board",
+        url: "https://www.careeronestop.org/Toolkit/Jobs/find-jobs-results.aspx?keyword=&location=New%20Jersey&radius=25&source=NLX&curPage=1&referer=%2FToolkit%2FJobs%2Ffind-jobs.aspx",
+        description: "Search for current job openings in New Jersey",
+      },
+      {
+        sys: {
+          id: "4FzcfTXyculFqFlnWgcdo9",
+        },
+        sectionIcon: "jobs",
+        icon: null,
+        copy: "Labor Market Information",
+        url: "https://www.nj.gov/labor/labormarketinformation/",
+        description: "Learn about the labor market in NJ",
+      },
+      {
+        sys: {
+          id: "7eLHEa4Hr7d1Fv09qp7c5Y",
+        },
+        sectionIcon: "jobs",
+        icon: null,
+        copy: "Apprenticeship Programs",
+        url: "https://www.nj.gov/labor/career-services/apprenticeship/",
+        description: "Learn about apprenticeship opportunities",
+      },
+    ],
+  },
+  trainingToolLinksCollection: {
+    items: [
+      {
+        sys: {
+          id: "5QTcssKSYsIg8np1ggO2UF",
+        },
+        sectionIcon: "training",
+        icon: null,
+        copy: "NJ Training Explorer",
+        url: "/training",
+        description: "Search for training programs in NJ",
+      },
+      {
+        sys: {
+          id: "3vq84kWYDEB52xrvLCFfDU",
+        },
+        sectionIcon: "training",
+        icon: null,
+        copy: "NJ Career Navigator",
+        url: "/navigator",
+        description: "Get personalized career change recommendations",
+      },
+      {
+        sys: {
+          id: "1pZCOFlrVUNAKFNKJjxIXI",
+        },
+        sectionIcon: "training",
+        icon: null,
+        copy: "Tuition Assistance Resources",
+        url: "/support-resources/tuition-assistance",
+        description: "Learn about ways to get help paying for education",
+      },
+      {
+        sys: {
+          id: "3ynJm1nUCMfmzVVznbAmNI",
+        },
+        sectionIcon: "training",
+        icon: null,
+        copy: "Training Provider Resources",
+        url: "/training-provider-resources",
+        description: "Important information for training providers",
+      },
+      {
+        sys: {
+          id: "35fYExqOs6GFx6fvsOvtpT",
+        },
+        sectionIcon: "training",
+        icon: null,
+        copy: "SkillUp",
+        url: "https://nj.metrixlearning.com/landing.cfm",
+        description: "Free on-line training videos",
+      },
+    ],
+  },
+  careerExplorationToolLinksCollection: {
+    items: [
+      {
+        sys: {
+          id: "3iK2gvVLLjtCnJtFRZ5S1h",
+        },
+        icon: null,
+        sectionIcon: "jobs",
+        copy: "NJ Career Navigator",
+        url: "/navigator",
+        description: "Get personalized career change recommendations",
+      },
+      {
+        sys: {
+          id: "1FwclNJoyvGOX2NY5qOkBt",
+        },
+        icon: "MapTrifold",
+        sectionIcon: "explore",
+        copy: "NJ Career Pathways",
+        url: "/career-pathways",
+        description: "Explore in-demand jobs and training opportunities.",
+      },
+    ],
+  },
+  supportAndAssistanceLinksCollection: {
+    items: [
+      {
+        sys: {
+          id: "65ombyNutpQ2adGMwCzJw5",
+        },
+        icon: null,
+        sectionIcon: "support",
+        copy: "Browse Support by Category",
+        url: "/support-resources",
+        description: null,
+      },
+      {
+        sys: {
+          id: "6tH2KP1p9PfAUZyI2AbHUi",
+        },
+        icon: null,
+        sectionIcon: "support",
+        copy: "Career Support",
+        url: "/support-resources/career-support",
+        description:
+          "Career Guidance, Job Search Help, and Re-entering the Workforce",
+      },
+      {
+        sys: {
+          id: "1yqrHQv5pPbq6BaNmXhR5Y",
+        },
+        icon: null,
+        sectionIcon: "support",
+        copy: "Tuition Assistance",
+        url: "/support-resources/tuition-assistance",
+        description: "Job Training, Community College, and More",
+      },
+      {
+        sys: {
+          id: "2KQhAwOxDFbPUiuQoI0lNF",
+        },
+        icon: null,
+        sectionIcon: "support",
+        copy: "Other Assistance",
+        url: "/support-resources/other",
+        description: "Housing, Transportation, Healthcare, and More",
+      },
+      {
+        sys: {
+          id: "57KxzbJWCglLxqVad6TbiT",
+        },
+        icon: null,
+        sectionIcon: "support",
+        copy: "Frequently Asked Questions",
+        url: "/faq",
+        description: null,
+      },
+    ],
+  },
+};
