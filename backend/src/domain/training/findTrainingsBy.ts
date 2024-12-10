c42290fa0be8dcceb6c7a23da83b934ffd4bbf14
ChelseaKR <chelsea.kelly-reif@coforma.io>
2024-12-09 23:06:43 -0800

    refactored certificate -> record

--- backend/src/domain/training/findTrainingsBy.ts
+++ backend/src/domain/training/findTrainingsBy.ts
@@ -23,48 +23,48 @@ export const findTrainingsByFactory = (dataClient: DataClient): FindTrainingsBy 
     }
 
     return await Promise.all(
-      ceRecords.map(async (certificate: CTDLResource) => {
-        const provider = await credentialEngineUtils.getProviderData(certificate);
+      ceRecords.map(async (record: CTDLResource) => {
+        const provider = await credentialEngineUtils.getProviderData(record);
 
         //const provider = {"@id":"https://sandbox.credentialengineregistry.org/resources/ce-b4a6bf5d-103b-4548-877c-649f6f79fb01","@type":"ceterms:CredentialOrganization","@context":"https://credreg.net/ctdl/schema/context/json","ceterms:ctid":"ce-b4a6bf5d-103b-4548-877c-649f6f79fb01","ceterms:name":{"en-US":"BH Test Organization"},"ceterms:email":["bhauss@agatesoftware.com"],"ceterms:agentType":[{"@type":"ceterms:CredentialAlignmentObject","ceterms:framework":"https://credreg.net/ctdl/terms/OrganizationType","ceterms:targetNode":"orgType:NonTraditional","ceterms:frameworkName":{"en-US":"Organization Type"},"ceterms:targetNodeName":{"en-US":"Alternative/Non-Traditional School"},"ceterms:targetNodeDescription":{"en-US":"Secondary (i.e., high school) that: 1) addresses needs of students which cannot typically be met in a regular school; 2) provides nontraditional education; 3) falls outside of the categories of regular, magnet/special program emphasis, or career and technical education."}}],"ceterms:description":{"en-US":"Class A CDL Training Program provides students with the skills to get a Class A CDL and become eligible for entry-level commercial driver positions."},"ceterms:subjectWebpage":"https://agatesoftware.com/","ceterms:agentSectorType":[{"@type":"ceterms:CredentialAlignmentObject","ceterms:framework":"https://credreg.net/ctdl/terms/AgentSector","ceterms:targetNode":"agentSector:PrivateForProfit","ceterms:frameworkName":{"en-US":"Agent Sector"},"ceterms:targetNodeName":{"en-US":"Private For-Profit"},"ceterms:targetNodeDescription":{"en-US":"Sector that contains privately-owned organizations that operate for profit."}}],"ceterms:lifeCycleStatusType":{"@type":"ceterms:CredentialAlignmentObject","ceterms:framework":"https://credreg.net/ctdl/terms/LifeCycleStatus","ceterms:targetNode":"lifeCycle:Active","ceterms:frameworkName":{"en-US":"Life Cycle Status"},"ceterms:targetNodeName":{"en-US":"Active"},"ceterms:targetNodeDescription":{"en-US":"Resource is active, current, ongoing, offered, operational, or available."}}};
 
-        const cipCode = await credentialEngineUtils.extractCipCode(certificate);
+        const cipCode = await credentialEngineUtils.extractCipCode(record);
         console.log(provider.providerId);
         const cipDefinition = await dataClient.findCipDefinitionByCip(cipCode);
 
         const outcomesDefinition = await dataClient.findOutcomeDefinition(provider.providerId, cipCode);
-        const credentials = await credentialEngineUtils.constructCredentialsString(certificate["ceterms:isPreparationFor"] as CetermsConditionProfile[]);
+        const credentials = await credentialEngineUtils.constructCredentialsString(record["ceterms:isPreparationFor"] as CetermsConditionProfile[]);
 
         const training = {
-          ctid: certificate["ceterms:ctid"],
-          name: certificate["ceterms:name"] ? certificate["ceterms:name"]["en-US"] : "",
+          ctid: record["ceterms:ctid"],
+          name: record["ceterms:name"] ? record["ceterms:name"]["en-US"] : "",
           cipDefinition: cipDefinition ? cipDefinition[0] : null,
           provider: provider,
-          availableAt: await credentialEngineUtils.getAvailableAtAddresses(certificate),
-          description: certificate["ceterms:description"] ? certificate["ceterms:description"]["en-US"] : "",
+          availableAt: await credentialEngineUtils.getAvailableAtAddresses(record),
+          description: record["ceterms:description"] ? record["ceterms:description"]["en-US"] : "",
           credentials,
-          prerequisites: await credentialEngineUtils.extractPrerequisites(certificate),
-          totalClockHours: await credentialEngineUtils.getTimeRequired(certificate),
-          calendarLength: await credentialEngineUtils.getCalendarLengthId(certificate),
-          occupations: await credentialEngineUtils.extractOccupations(certificate),
+          prerequisites: await credentialEngineUtils.extractPrerequisites(record),
+          totalClockHours: await credentialEngineUtils.getTimeRequired(record),
+          calendarLength: await credentialEngineUtils.getCalendarLengthId(record),
+          occupations: await credentialEngineUtils.extractOccupations(record),
           inDemand: inDemandCIPCodes.includes(cipCode ?? ""),
           localExceptionCounty: await getLocalExceptionCounties(dataClient, cipCode),
-          tuitionCost: await credentialEngineUtils.extractCost(certificate, "costType:Tuition"),
-          feesCost: await credentialEngineUtils.extractCost(certificate, "costType:MixedFees"),
-          booksMaterialsCost: await credentialEngineUtils.extractCost(certificate, "costType:LearningResource"),
-          suppliesToolsCost: await credentialEngineUtils.extractCost(certificate, "costType:TechnologyFee"),
-          otherCost: await credentialEngineUtils.extractCost(certificate, "costType:ProgramSpecificFee"),
-          totalCost: await credentialEngineUtils.extractCost(certificate, "costType:AggregateCost"),
-          deliveryTypes: await credentialEngineUtils.hasLearningDeliveryTypes(certificate),
-          // percentEmployed: await credentialEngineUtils.extractEmploymentData(certificate),
+          tuitionCost: await credentialEngineUtils.extractCost(record, "costType:Tuition"),
+          feesCost: await credentialEngineUtils.extractCost(record, "costType:MixedFees"),
+          booksMaterialsCost: await credentialEngineUtils.extractCost(record, "costType:LearningResource"),
+          suppliesToolsCost: await credentialEngineUtils.extractCost(record, "costType:TechnologyFee"),
+          otherCost: await credentialEngineUtils.extractCost(record, "costType:ProgramSpecificFee"),
+          totalCost: await credentialEngineUtils.extractCost(record, "costType:AggregateCost"),
+          deliveryTypes: await credentialEngineUtils.hasLearningDeliveryTypes(record),
+          // percentEmployed: await credentialEngineUtils.extractEmploymentData(record),
           percentEmployed: outcomesDefinition ? formatPercentEmployed(outcomesDefinition.peremployed2) : null,
-          // averageSalary: await credentialEngineUtils.extractAverageSalary(certificate),
+          // averageSalary: await credentialEngineUtils.extractAverageSalary(record),
           averageSalary: outcomesDefinition ? formatAverageSalary(outcomesDefinition.avgquarterlywage2) : null,
-          hasEveningCourses: await credentialEngineUtils.hasEveningSchedule(certificate),
-          languages: await credentialEngineUtils.getLanguages(certificate),
-          isWheelchairAccessible: await credentialEngineUtils.checkAccommodation(certificate, "accommodation:PhysicalAccessibility"),
-          hasJobPlacementAssistance: await credentialEngineUtils.checkSupportService(certificate, "support:JobPlacement"),
-          hasChildcareAssistance: await credentialEngineUtils.checkSupportService(certificate, "support:Childcare")
+          hasEveningCourses: await credentialEngineUtils.hasEveningSchedule(record),
+          languages: await credentialEngineUtils.getLanguages(record),
+          isWheelchairAccessible: await credentialEngineUtils.checkAccommodation(record, "accommodation:PhysicalAccessibility"),
+          hasJobPlacementAssistance: await credentialEngineUtils.checkSupportService(record, "support:JobPlacement"),
+          hasChildcareAssistance: await credentialEngineUtils.checkSupportService(record, "support:Childcare")
         };
 
         return training;
