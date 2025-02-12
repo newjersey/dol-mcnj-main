79bb2702fdc9868b748377055692a289d01d5942
Scott Wambach <scott.wambach@coforma.io>
2025-02-12 13:27:19 -0600

    minor style updates (#3236)

--- frontend/src/components/SignUpFormModal.tsx
+++ frontend/src/components/SignUpFormModal.tsx
@@ -25,10 +25,10 @@ export const SignUpFormModal = () => {
   const handleSubmission = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (resetForm) return;
-  
+
     setSubmitting(true);
     setHasErrors("");
-  
+
     const allErrorCheck = () => {
       if (
         (firstName.length !== 0 && firstName.length < 2) ||
@@ -41,40 +41,40 @@ export const SignUpFormModal = () => {
       }
       return false;
     };
-  
+
     // Perform validations
     if (firstName.length !== 0 && firstName.length < 2) {
       setFirstNameError(t("SignUpFormModal.firstNameError"));
     } else {
       setFirstNameError("");
     }
-  
+
     if (lastName.length !== 0 && lastName.length < 2) {
       setLastNameError(t("SignUpFormModal.lastNameError"));
     } else {
       setLastNameError("");
     }
-  
+
     if (!email) {
       setEmailError(t("SignUpFormModal.emailRequired"));
-    } else if (!emailRegex.test(email)) {  
+    } else if (!emailRegex.test(email)) {
       setEmailError(t("SignUpFormModal.emailError"));
     } else {
       setEmailError("");
     }
-  
+
     if (phone && phone.length < 12) {
       setPhoneError(t("SignUpFormModal.phoneError"));
     } else {
       setPhoneError("");
     }
-  
+
     if (allErrorCheck()) {
       setSubmitting(false);
       setHasErrors(t("SignUpFormModal.attentionRequired"));
       return;
     }
-  
+
     // Construct payload
     const formData = {
       firstName,
@@ -82,7 +82,7 @@ export const SignUpFormModal = () => {
       email,
       phone,
     };
-  
+
     try {
       const response = await fetch("/api/signup", {
         method: "POST",
@@ -91,9 +91,9 @@ export const SignUpFormModal = () => {
         },
         body: JSON.stringify(formData),
       });
-  
+
       const result = await response.json();
-  
+
       if (response.ok) {
         setSuccess(true);
         setHasErrors("");
@@ -106,10 +106,9 @@ export const SignUpFormModal = () => {
       setSuccess(false);
       setHasErrors(t("SignUpFormModal.serverErrorMessage"));
     }
-  
+
     setSubmitting(false);
   };
-  
 
   function formatPhoneNumber(input: string): string {
     const cleaned = input.replace(/\D/g, "");
@@ -180,9 +179,7 @@ export const SignUpFormModal = () => {
           {!success && (
             <>
               <p className="heading">{t("SignUpFormModal.formTitle")}</p>
-              <p>
-                {t("SignUpFormModal.formDescription")}
-              </p>
+              <p>{t("SignUpFormModal.formDescription")}</p>
             </>
           )}
           {success ? (
@@ -205,138 +202,142 @@ export const SignUpFormModal = () => {
               </div>
             </>
           ) : (
-              <>
+            <>
               <span className="instruction">
                 <p>
-                  {t("SignUpFormModal.requiredFieldIndicator.part1")}
-                  (<span className="red">*</span>)
-                  {t("SignUpFormModal.requiredFieldIndicator.part2")}
+                  {t("SignUpFormModal.requiredFieldIndicator.part1")}(<span className="red">*</span>
+                  ){t("SignUpFormModal.requiredFieldIndicator.part2")}
                 </p>
               </span>
-                <form onSubmit={handleSubmission} onChange={() => setResetForm(false)}>
-                  <div className="row">
-                    <label htmlFor="firstName" className={firstNameError ? "error" : ""}>
-                      <span>{t("SignUpFormModal.firstNameLabel")}</span>
-                      <input
-                          type="text"
-                          id="firstName"
-                          placeholder="Jane"
-                          value={firstName}
-                          onChange={(e) => {
-                            if (firstName.length > 2) {
-                              setFirstNameError("");
-                            }
-                            setFirstName(e.target.value);
-                          }}
-                      />
-                      {firstNameError && <div className="errorMessage">{firstNameError}</div>}
-                    </label>
-                    <label htmlFor="lastName" className={lastNameError ? "error" : ""}>
-                      <span>{t("SignUpFormModal.lastNameLabel")}</span>
-                      <input
-                          type="text"
-                          id="lastName"
-                          value={lastName}
-                          placeholder="Smith"
-                          onChange={(e) => {
-                            if (lastName.length > 2) {
-                              setLastNameError("");
-                            }
-                            setLastName(e.target.value);
-                          }}
-                      />
-                      {lastNameError && <div className="errorMessage">{lastNameError}</div>}
-                    </label>
-                  </div>
-                  <label htmlFor="email" className={`email${emailError ? " error" : ""}`}>
+              <form onSubmit={handleSubmission} onChange={() => setResetForm(false)}>
+                <div className="row">
+                  <label htmlFor="firstName" className={firstNameError ? "error" : ""}>
+                    <span>{t("SignUpFormModal.firstNameLabel")}</span>
+                    <input
+                      type="text"
+                      id="firstName"
+                      placeholder="Jane"
+                      value={firstName}
+                      onChange={(e) => {
+                        if (firstName.length > 2) {
+                          setFirstNameError("");
+                        }
+                        setFirstName(e.target.value);
+                      }}
+                    />
+                    {firstNameError && <div className="errorMessage">{firstNameError}</div>}
+                  </label>
+                  <label htmlFor="lastName" className={lastNameError ? "error" : ""}>
+                    <span>{t("SignUpFormModal.lastNameLabel")}</span>
+                    <input
+                      type="text"
+                      id="lastName"
+                      value={lastName}
+                      placeholder="Smith"
+                      onChange={(e) => {
+                        if (lastName.length > 2) {
+                          setLastNameError("");
+                        }
+                        setLastName(e.target.value);
+                      }}
+                    />
+                    {lastNameError && <div className="errorMessage">{lastNameError}</div>}
+                  </label>
+                </div>
+                <label htmlFor="email" className={`email${emailError ? " error" : ""}`}>
                   <span>
                     {t("SignUpFormModal.emailLabel")} <span className="red">*</span>
                   </span>
-                    <div>
-                      <EnvelopeSimple size={20} weight="bold"/>
-                      <input
-                          type="text"
-                          value={email}
-                          id="email"
-                          placeholder="example@email.com"
-                          onChange={(e) => {
-                            if (email && email.includes("@")) {
-                              setEmailError("");
-                            }
-
-                            setEmail(e.target.value);
-                          }}
-                      />
-                    </div>
-                    {emailError && <div className="errorMessage">{emailError}</div>}
-                  </label>
-                  <label htmlFor="phone" className={phoneError ? "error" : ""}>
-                    <span>{t("SignUpFormModal.phoneLabel")}</span>
-                    {t("SignUpFormModal.usPhoneOnlyLabel")}
+                  <div>
+                    <EnvelopeSimple size={20} weight="bold" />
                     <input
-                        type="text"
-                        value={formatPhoneNumber(phone)}
-                        id="phone"
-                        placeholder="___-___-____"
-                        onChange={(e) => {
-                          if (e.target.value.length > 11) {
-                            setPhoneError("");
-                          }
-                          setPhone(formatPhoneNumber(e.target.value));
-                        }}
-                    />
-                    {phoneError && <div className="errorMessage">{phoneError}</div>}
-                  </label>
-                  {hasErrors && (
-                      <div className="usa-alert usa-alert--error" role="alert">
-                        <div className="usa-alert__body">
-                          <p className="usa-alert__text">{hasErrors}</p>
-                        </div>
-                      </div>
-                  )}
-                  <div className="buttons">
-                    <Button
-                        variant="primary"
-                        onClick={() => {
-                          setResetForm(false);
-                        }}
-                    >
-                      {submitting && (
-                          <div className="spinner">
-                            <CircleNotch size={20} weight="bold"/>
-                          </div>
-                      )}
-                      {submitting ? t("SignUpFormModal.loadingMessage") : t("SignUpFormModal.submitButton")}
-                    </Button>
-                    <button
-                        className="usa-button usa-button--unstyled"
-                        onClick={() => {
-                          setFirstName("");
-                          setLastName("");
-                          setEmail("");
-                          setPhone("");
-                          setFirstNameError("");
-                          setLastNameError("");
-                          setPhoneError("");
+                      type="text"
+                      value={email}
+                      id="email"
+                      placeholder="example@email.com"
+                      onChange={(e) => {
+                        if (email && email.includes("@")) {
                           setEmailError("");
-                          setHasErrors("");
-                          setResetForm(true);
-                          setSubmitting(false);
-                        }}
-                    >
-                      {t("SignUpFormModal.resetForm")}
-                    </button>
+                        }
+
+                        setEmail(e.target.value);
+                      }}
+                    />
                   </div>
-                </form>
-                <p>
-                  {t("SignUpFormModal.readAboutOur")}{" "}
-                  <a href="/privacy-policy" target="_blank"
-                     rel="noopener noreferrer">{t("SignUpFormModal.privacyPolicy")}</a>{" "}
-                  {t("SignUpFormModal.andOur")}{" "}
-                  <a href="/sms-use-policy" target="_blank"
-                     rel="noopener noreferrer">{t("SignUpFormModal.smsUsePolicy")}</a>.
-                </p>
-              </>
+                  {emailError && <div className="errorMessage">{emailError}</div>}
+                </label>
+                <label htmlFor="phone" className={phoneError ? "error" : ""}>
+                  <span>{t("SignUpFormModal.phoneLabel")}</span>
+                  {t("SignUpFormModal.usPhoneOnlyLabel")}
+                  <input
+                    type="text"
+                    value={formatPhoneNumber(phone)}
+                    id="phone"
+                    placeholder="___-___-____"
+                    onChange={(e) => {
+                      if (e.target.value.length > 11) {
+                        setPhoneError("");
+                      }
+                      setPhone(formatPhoneNumber(e.target.value));
+                    }}
+                  />
+                  {phoneError && <div className="errorMessage">{phoneError}</div>}
+                </label>
+                {hasErrors && (
+                  <div className="usa-alert usa-alert--error" role="alert">
+                    <div className="usa-alert__body">
+                      <p className="usa-alert__text">{hasErrors}</p>
+                    </div>
+                  </div>
+                )}
+                <div className="buttons">
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      setResetForm(false);
+                    }}
+                  >
+                    {submitting && (
+                      <div className="spinner">
+                        <CircleNotch size={20} weight="bold" />
+                      </div>
+                    )}
+                    {submitting
+                      ? t("SignUpFormModal.loadingMessage")
+                      : t("SignUpFormModal.submitButton")}
+                  </Button>
+                  <button
+                    className="usa-button usa-button--unstyled"
+                    onClick={() => {
+                      setFirstName("");
+                      setLastName("");
+                      setEmail("");
+                      setPhone("");
+                      setFirstNameError("");
+                      setLastNameError("");
+                      setPhoneError("");
+                      setEmailError("");
+                      setHasErrors("");
+                      setResetForm(true);
+                      setSubmitting(false);
+                    }}
+                  >
+                    {t("SignUpFormModal.resetForm")}
+                  </button>
+                </div>
+              </form>
+              <p>
+                {t("SignUpFormModal.readAboutOur")}{" "}
+                <a href="/privacy-policy" target="_blank" rel="noopener noreferrer">
+                  {t("SignUpFormModal.privacyPolicy")}
+                </a>{" "}
+                {t("SignUpFormModal.andOur")}{" "}
+                <a href="/sms-use-policy" target="_blank" rel="noopener noreferrer">
+                  {t("SignUpFormModal.smsUsePolicy")}
+                </a>
+                .
+              </p>
+            </>
           )}
         </div>
       </div>
