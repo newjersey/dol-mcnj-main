19b8ed5beeeb00cb4061f54f44be7aa38fe839cf
Scott Wambach <scott@scottwamba.ch>
2025-03-26 12:55:20 -0500

    job detail page

---
+++ src/app/occupation/[code]/Content.tsx
@@ -0,0 +1,218 @@
+"use client";
+/* eslint-disable @next/next/no-img-element */
+import { LabelBox } from "@components/modules/LabelBox";
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
+import { LinkObject } from "@components/modules/LinkObject";
+import { RelatedHeading } from "@components/modules/RelatedHeading";
+import { SeeMoreList } from "@components/modules/SeeMoreList";
+import { ResultCard } from "@components/modules/ResultCard";
+import { OccupationPageProps } from "@utils/types";
+import careeronestop from "@images/careeronestop.png";
+import Script from "next/script";
+import { OccupationBanner } from "@components/blocks/OccupationBanner";
+
+export const revalidate = 1800;
+
+export const Content = ({
+  occupation,
+}: {
+  occupation: OccupationPageProps;
+}) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const reactToPrintFn = useReactToPrint({ contentRef });
+
+  const tasks = occupation.tasks?.map((task: {}) => {
+    return {
+      copy: task,
+    };
+  }) as {
+    copy: string;
+  }[];
+
+  const generateJsonLd = {
+    "@context": "https://schema.org/",
+    "@type": "Occupation",
+    name: occupation.title,
+    description: occupation.description,
+    // "qualifications": "Qualifications information",
+    // "skills": ["Skills information"],
+    responsibilities: occupation.tasks,
+    educationRequirements: occupation.education,
+    // "experienceRequirements": "Experience requirements information",
+    occupationalCategory: occupation.soc,
+    estimatedSalary: {
+      "@type": "MonetaryAmount",
+      currency: "USD",
+      value: {
+        "@type": "QuantitativeValue",
+        value: occupation.medianSalary,
+        unitText: "YEAR",
+      },
+    },
+  };
+
+  return (
+    <div ref={contentRef}>
+      <Script type="application/ld+json" id="occupation">
+        {JSON.stringify(generateJsonLd)}
+      </Script>
+      <OccupationBanner
+        name={occupation.title}
+        id={occupation.soc}
+        provider={occupation.soc}
+        printHandler={reactToPrintFn}
+        breadcrumbsCollection={{
+          items: [
+            {
+              url: "/",
+              copy: "Home",
+            },
+            {
+              url: "/in-demand-occupations",
+              copy: "In-Demand Occupation List",
+            },
+          ],
+        }}
+        inDemand={occupation.inDemand}
+        employmentRate={occupation.medianSalary || 0}
+        salary={
+          occupation.openJobsCount && occupation.openJobsCount > 0
+            ? occupation.openJobsCount || 0
+            : undefined
+        }
+      />
+
+      <section className="body-copy">
+        <div className="container">
+          <LinkObject
+            className="openingsLink"
+            url={`https://www.careeronestop.org/Toolkit/Jobs/find-jobs-results.aspx?keyword=${occupation.soc}&location=New%20Jersey&radius=0&source=NLX&currentpage=1`}
+          >
+            <span>Search job openings in New Jersey</span>
+          </LinkObject>
+          <div className="inner">
+            <div>
+              <LabelBox
+                large
+                subheading="About this Occupation"
+                title="Description"
+                color="purple"
+                className="description"
+                headingLevel={2}
+              >
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: occupation.description,
+                  }}
+                />
+              </LabelBox>
+              <LabelBox
+                large
+                title="A Day in the Life"
+                subheading="Details about this occupation"
+                color="purple"
+                className="tasks"
+                headingLevel={2}
+              >
+                <SeeMoreList items={tasks} />
+              </LabelBox>
+            </div>
+            <div>
+              {occupation.education && (
+                <LabelBox
+                  large
+                  title="Education"
+                  subheading="Types of education and training needed for this occupation"
+                  color="purple"
+                  className="education"
+                  headingLevel={2}
+                >
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: occupation.education,
+                    }}
+                  />
+                </LabelBox>
+              )}
+              {occupation.relatedOccupations.length > 0 && (
+                <LabelBox
+                  large
+                  title="Related Occupations"
+                  subheading="Explore these related occupations"
+                  color="purple"
+                  className="relatedBox"
+                  headingLevel={2}
+                >
+                  {occupation.relatedOccupations.map((occupation) => (
+                    <LinkObject
+                      key={occupation.soc}
+                      url={`/occupation/${occupation.soc}`}
+                    >
+                      {occupation.title}
+                    </LinkObject>
+                  ))}
+                </LabelBox>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="related">
+        <div className="container">
+          <div className="inner">
+            <RelatedHeading
+              title={occupation.title}
+              hasTraining={occupation.relatedTrainings.length > 0}
+              headingLevel={3}
+              className="text-xl"
+            />
+            {occupation.relatedTrainings.length > 0 ? (
+              <>
+                {occupation.relatedTrainings?.splice(0, 3).map((training) => (
+                  <ResultCard
+                    key={training.id}
+                    title={training.name}
+                    trainingId={training.id}
+                    url={`/training/${training.id}`}
+                    education={training.providerName}
+                    percentEmployed={training.percentEmployed || undefined}
+                    cipDefinition={training.cipDefinition}
+                    cost={training.totalCost}
+                    timeToComplete={training.calendarLength}
+                    inDemandLabel={training.inDemand ? "In Demand" : undefined}
+                    location={`${training.city}, ${training.county}`}
+                  />
+                ))}
+              </>
+            ) : (
+              "This data is not yet available for this occupation."
+            )}
+          </div>
+          <div className="container-full">
+            <p className="accessible-gray">
+              <strong>Source:</strong>
+              &nbsp;O*NET OnLine by the U.S. Department of Labor, Employment and
+              Training Administration (USDOL/ETA). Used under the CC BY 4.0
+              license. O*NET® is a trademark of USDOL/ETA.
+            </p>
+            <p className="accessible-gray">
+              <strong>Source:</strong>
+              &nbsp;Bureau of Labor Statistics, U.S. Department of Labor,
+              Occupational Outlook Handbook
+            </p>
+            <p>
+              <img
+                src={careeronestop.src}
+                width={careeronestop.width}
+                height={careeronestop.height}
+                alt="Career One Stop Logo"
+              />
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
