6982f180bc7982ec2dc83c13ce616963f5d3634a
Scott Wambach <scott@scottwamba.ch>
2025-04-04 09:14:43 -0500

    Update Header.tsx

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -56,7 +56,7 @@ const Header = ({ mainNav, globalNav, lang = "en" }: HeaderProps) => {
         lang={lang}
         label="Primary navigation"
         className={`main-nav${isOpen ? " open" : ""}`}
-        innerClassName="usa-nav-container tabletLg:flex"
+        innerClassName="usa-nav-container main tabletLg:flex justify-between w-full"
         icons
         extraItems={
           <div className="contact-links">
