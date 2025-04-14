f330b41ffd1b3ecef9291789b9b57e5e22e53935
Scott Wambach <scott@scottwamba.ch>
2025-04-14 10:38:11 -0500

    fixes

--- frontend/src/career-pathways-page/content.ts
+++ frontend/src/career-pathways-page/content.ts
@@ -2,7 +2,7 @@ import manufacturingImage from "../images/welder.jpg";
 import healthcareImage from "../images/medical.jpg";
 import tdlImage from "../images/mechanic.jpg";
 import { SelectorProps } from "../svg/Selector";
-import sectionImage from "../images/1207302676.jpg";
+import sectionImage from "../images/1207302676_cropped.png";
 import sectionImage2 from "../images/1404069302.jpg";
 
 export const content = {
