203cc6e79137a46ccb23e5f94a4826af1ab56be8
denish-fearless <dsubedi@fearless.com>
2025-05-16 09:44:52 -0400

    zero downtime buld deployment

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,9 @@ cd $(git rev-parse --show-toplevel)
 npm --prefix=frontend run build
 npm --prefix=backend run build
 
+rm -rf backend/dist_old
+mv backend/dist backend/dist_old 2>/dev/null
+mv backend/dist_temp backend/dist
 mv frontend/build backend/dist
+
+echo "✅ Build successfully completed."
