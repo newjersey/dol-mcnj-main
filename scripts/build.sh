14e3dc21d1616fed550f78d4ee44c6c19e391b8a
denish-fearless <dsubedi@fearless.com>
2025-05-27 12:38:03 -0400

    was failing becasue of the set -e, exists when no folder is found: fixed it

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,9 @@ cd $(git rev-parse --show-toplevel)
 npm --prefix=frontend run build
 npm --prefix=backend run build
 
-rm -rf backend/dist_old
-mv backend/dist backend/dist_old 2>/dev/null
+rm -rf backend/dist_old || true
+[ -d backend/dist ] && mv backend/dist backend/dist_old
+
 mv backend/dist_temp backend/dist
 mv frontend/build backend/dist
 
