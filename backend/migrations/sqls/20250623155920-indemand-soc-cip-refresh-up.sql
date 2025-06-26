d58da719d2e296ceb122d334b7093b4c1f530dea
ChelseaKR <chelsea.kelly-reif@coforma.io>
2025-06-26 10:21:33 -0700

    DELETE public.localexceptioncips; in up file

--- backend/migrations/sqls/20250623155920-indemand-soc-cip-refresh-up.sql
+++ backend/migrations/sqls/20250623155920-indemand-soc-cip-refresh-up.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e87513495145ae997c495a8575272eaa743c5859e7d2a23dee84f13edbdbb64
-size 156061
+oid sha256:023590679e1e0b33ec133a55179aef60fae7ee185ca5595c3f0cfc53ec0a78b6
+size 156066
