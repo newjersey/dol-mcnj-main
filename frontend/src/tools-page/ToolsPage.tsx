e800d9185c6856ccfb14a48142932010bc952465
Scott Wambach <scott@scottwamba.ch>
2025-07-30 09:59:53 -0500

    Update ToolsPage.tsx

--- frontend/src/tools-page/ToolsPage.tsx
+++ frontend/src/tools-page/ToolsPage.tsx
@@ -158,6 +158,14 @@ export const ToolsPage = (props: Props): ReactElement => {
                 href: "/support-resources",
               },
             },
+            // {
+            //   heading: t("ToolsPage.supportLink2Heading"),
+            //   description: t("ToolsPage.supportLink2Description"),
+
+            //   link: {
+            //     href: "/faq",
+            //   },
+            // },
           ]}
         />
       </div>
