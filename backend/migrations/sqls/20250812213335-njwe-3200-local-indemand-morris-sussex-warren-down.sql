cdf78f335b4770f8f8dd2f3ff8433cf638b798d8
ChelseaKR <chelsea.kelly-reif@coforma.io>
2025-08-12 15:53:25 -0700

    - Add migration to insert localexceptioncips records for MORRIS, SUSSEX, and WARREN counties - Add down migration to remove specific localexceptioncips records for MORRIS, SUSSEX, and WARREN counties

---
+++ backend/migrations/sqls/20250812213335-njwe-3200-local-indemand-morris-sussex-warren-down.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7a91caf7f2c605e61ac80f295980a50fc9d1ca64acc7ba0b16bb44c10d14a14d
+size 4347
---
+++ backend/migrations/sqls/20250812213335-njwe-3200-local-indemand-morris-sussex-warren-up.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa9c158a1fe149555a00154977e06bb8e0082ba5fb955cd282ff4a1eb872c82b
+size 4579
