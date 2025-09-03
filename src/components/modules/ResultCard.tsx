a135fa4602634b7cf6deba8964cb0c6926213bdf
ChelseaKR <chelsea.kelly-reif@coforma.io>
2025-09-03 15:46:38 -0700

    refine resultscard

--- src/components/modules/ResultCard.tsx
+++ src/components/modules/ResultCard.tsx
@@ -7,6 +7,7 @@ import {
   ClockIcon,
   GraduationCapIcon,
   MapPin,
+  CurrencyDollarIcon,
 } from "@phosphor-icons/react";
 import { Tag } from "./Tag";
 import { FormInput } from "./FormInput";
@@ -17,7 +18,13 @@ import { CipDefinition } from "@utils/types";
 import { ProgramOutcome } from "@utils/types/components";
 import { formatCip } from "@utils/formatCip";
 import { Flex } from "@components/utility/Flex";
-import { getPercentEmployed, getCompletionRate, formatPercentage } from "@utils/outcomeHelpers";
+import {
+  getPercentEmployed,
+  getCompletionRate,
+  formatPercentage,
+  getMedianSalary,
+  formatSalary,
+} from "@utils/outcomeHelpers";
 
 interface ResultCardProps {
   cipDefinition?: CipDefinition;
@@ -57,6 +64,11 @@ const ResultCard = (props: ResultCardProps) => {
     trainingId,
     url,
   } = props;
+
+  const medianSalary = outcomes
+    ? getMedianSalary(outcomes, 4) ?? getMedianSalary(outcomes, 2)
+    : undefined;
+
   return (
     <div
       className={`resultCard${className ? ` ${className}` : ""}`}
@@ -71,78 +83,83 @@ const ResultCard = (props: ResultCardProps) => {
         <span>{(cost && toUsCurrency(cost)) || "$0"}</span>
       </div>
       <div className="information">
-        <div className="education-row">
+        <div className="main-info">
           {education && (
             <p className="education">
               <GraduationCapIcon size={16} />
               <span>{education}</span>
             </p>
           )}
+          {location && (
+            <p className="location">
+              <MapPin weight="fill" size={16} />
+              <span>{location}</span>
+            </p>
+          )}
+          {timeToComplete && (
+            <p className="timeToComplete">
+              <ClockIcon size={16} />
+              <span>{calendarLength(timeToComplete)} to complete</span>
+            </p>
+          )}
+          {cipDefinition ? (
+            <>
+              <p className="cipCode">
+                <Flex
+                  alignItems="center"
+                  gap="xxs"
+                  elementTag="span"
+                  columnBreak="none"
+                >
+                  <BookBookmarkIcon size={16} weight="fill" />
+                  <span>CIP: {formatCip(cipDefinition.cipcode)}</span>
+                </Flex>
+                <b>{cipDefinition.ciptitle}</b>
+              </p>
+            </>
+          ) : (
+            <span>No Data Available</span>
+          )}
+          {description && (
+            <p className="description">
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: `"...${replaceDoubleBracketsWithStrong(
+                    description
+                  )}..."`,
+                }}
+              />
+            </p>
+          )}
+        </div>
+        <div className="metrics-section">
+          <p className="percentEmployed">
+            <BriefcaseIcon size={16} />
+            <span>
+              {outcomes
+                ? `${formatPercentage(getPercentEmployed(outcomes))} employed`
+                : percentEmployed
+                ? `${Math.floor(percentEmployed * 100 * 10) / 10}% employed`
+                : "-- employed"}
+            </span>
+          </p>
 
-          <div className="metrics-section">
-            <p className="percentEmployed">
-              <BriefcaseIcon size={16} />
+          {outcomes && getCompletionRate(outcomes) !== undefined && (
+            <p className="completion-rate">
+              <GraduationCapIcon size={16} />
               <span>
-                {outcomes 
-                  ? formatPercentage(getPercentEmployed(outcomes))
-                  : percentEmployed
-                  ? `${Math.floor(percentEmployed * 100 * 10) / 10}% employed`
-                  : "--"
-                } employed
+                {`${formatPercentage(getCompletionRate(outcomes))} completion`}
               </span>
             </p>
-            
-            {outcomes && getCompletionRate(outcomes) !== undefined && (
-              <p className="completion-rate">
-                <GraduationCapIcon size={16} />
-                <span>
-                  {formatPercentage(getCompletionRate(outcomes))} completion
-                </span>
-              </p>
-            )}
-          </div>
-        </div>
-        {location && (
-          <p className="location">
-            <MapPin weight="fill" size={16} />
-            <span>{location}</span>
-          </p>
-        )}
-        {timeToComplete && (
-          <p className="timeToComplete">
-            <ClockIcon size={16} />
-            <span>{calendarLength(timeToComplete)} to complete</span>
-          </p>
-        )}
-        {cipDefinition ? (
-          <>
-            <p className="cipCode">
-              <Flex
-                alignItems="center"
-                gap="xxs"
-                elementTag="span"
-                columnBreak="none"
-              >
-                <BookBookmarkIcon size={16} weight="fill" />
-                <span>CIP: {formatCip(cipDefinition.cipcode)}</span>
-              </Flex>
-              <b>{cipDefinition.ciptitle}</b>
+          )}
+
+          {medianSalary !== undefined && (
+            <p className="median-wage">
+              <CurrencyDollarIcon size={16} />
+              <span>{`${formatSalary(medianSalary)} median wage`}</span>
             </p>
-          </>
-        ) : (
-          <span>No Data Available</span>
-        )}
-        {description && (
-          <p className="description">
-            <span
-              dangerouslySetInnerHTML={{
-                __html: `"...${replaceDoubleBracketsWithStrong(
-                  description
-                )}..."`,
-              }}
-            />
-          </p>
-        )}
+          )}
+        </div>
       </div>
       <div className={`footing${!inDemandLabel ? " noLabel" : ""}`}>
         {inDemandLabel && (
