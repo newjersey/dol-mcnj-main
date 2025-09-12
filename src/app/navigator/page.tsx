88088018ecd68965c7a37cc80d1bdce3d8dccb1f
Scott Wambach <scott@scottwamba.ch>
2025-09-12 11:08:42 -0400

    Update page.tsx

--- src/app/navigator/page.tsx
+++ src/app/navigator/page.tsx
@@ -1,64 +1,70 @@
-import { CtaBanner } from "@components/blocks/CtaBanner";
-import { PageHero } from "@components/blocks/PageHero";
-import { River } from "@components/blocks/River";
-import { Stepper } from "@components/blocks/Stepper";
-import { IconCard } from "@components/modules/IconCard";
-import { SectionHeading } from "@components/modules/SectionHeading";
-import { NAVIGATOR_PAGE_DATA as pageData } from "@data/pages/navigator";
-import globalOgImage from "@images/globalOgImage.jpeg";
-import { SupportedLanguages } from "@utils/types/types";
-import { Steps } from "app/training/Steps";
-import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
 
-export const revalidate = 86400;
-
-export async function generateMetadata({}) {
-  return {
-    title: `${pageData.seo.title} | ${process.env.REACT_APP_SITE_NAME}`,
-    description: pageData.seo.pageDescription,
-    keywords: pageData.seo.keywords,
-    icons: {
-      icon: "/favicon.ico",
-    },
-    openGraph: {
-      images: [globalOgImage.src],
-    },
-  };
+export default async function NavigatorPage() {
+  notFound();
 }
 
-export default async function CareerNavigatorPage() {
-  const cookieStore = await cookies();
-  const lang = (cookieStore.get("lang")?.value as SupportedLanguages) || "en";
+// import { CtaBanner } from "@components/blocks/CtaBanner";
+// import { PageHero } from "@components/blocks/PageHero";
+// import { River } from "@components/blocks/River";
+// import { Stepper } from "@components/blocks/Stepper";
+// import { IconCard } from "@components/modules/IconCard";
+// import { SectionHeading } from "@components/modules/SectionHeading";
+// import { NAVIGATOR_PAGE_DATA as pageData } from "@data/pages/navigator";
+// import globalOgImage from "@images/globalOgImage.jpeg";
+// import { SupportedLanguages } from "@utils/types/types";
+// import { Steps } from "app/training/Steps";
+// import { cookies } from "next/headers";
 
-  return (
-    <div className="page careerNavigator">
-      <PageHero {...pageData[lang].pageHero} />
-      <Steps theme="blue" items={pageData[lang].steps} />
+// export const revalidate = 86400;
 
-      <section className="howTo">
-        <div className="container">
-          <SectionHeading {...pageData[lang].howTo.sectionHeading} />
-        </div>
-        <div className="container ">
-          <Stepper theme="purple" steps={pageData[lang].howTo.cards} />
-        </div>
-      </section>
-      <CtaBanner {...pageData[lang].midPageCta} />
-      <CtaBanner {...pageData[lang].ctaBanner} />
-      <section className="info">
-        <div className="container">
-          <SectionHeading {...pageData[lang].info.sectionHeading} />
-        </div>
-        <div className="container narrow">
-          <div className="inner">
-            {pageData[lang].info.cards.map((card) => {
-              return <IconCard key={card.sys?.id} {...card} />;
-            })}
-          </div>
-        </div>
-      </section>
-      <River {...pageData[lang].river} />
-      <CtaBanner {...pageData[lang].footerCta} />
-    </div>
-  );
-}
+// export async function generateMetadata({}) {
+//   return {
+//     title: `${pageData.seo.title} | ${process.env.REACT_APP_SITE_NAME}`,
+//     description: pageData.seo.pageDescription,
+//     keywords: pageData.seo.keywords,
+//     icons: {
+//       icon: "/favicon.ico",
+//     },
+//     openGraph: {
+//       images: [globalOgImage.src],
+//     },
+//   };
+// }
+
+// export default async function CareerNavigatorPage() {
+//   const cookieStore = await cookies();
+//   const lang = (cookieStore.get("lang")?.value as SupportedLanguages) || "en";
+
+//   return (
+//     <div className="page careerNavigator">
+//       <PageHero {...pageData[lang].pageHero} />
+//       <Steps theme="blue" items={pageData[lang].steps} />
+
+//       <section className="howTo">
+//         <div className="container">
+//           <SectionHeading {...pageData[lang].howTo.sectionHeading} />
+//         </div>
+//         <div className="container ">
+//           <Stepper theme="purple" steps={pageData[lang].howTo.cards} />
+//         </div>
+//       </section>
+//       <CtaBanner {...pageData[lang].midPageCta} />
+//       <CtaBanner {...pageData[lang].ctaBanner} />
+//       <section className="info">
+//         <div className="container">
+//           <SectionHeading {...pageData[lang].info.sectionHeading} />
+//         </div>
+//         <div className="container narrow">
+//           <div className="inner">
+//             {pageData[lang].info.cards.map((card) => {
+//               return <IconCard key={card.sys?.id} {...card} />;
+//             })}
+//           </div>
+//         </div>
+//       </section>
+//       <River {...pageData[lang].river} />
+//       <CtaBanner {...pageData[lang].footerCta} />
+//     </div>
+//   );
+// }
