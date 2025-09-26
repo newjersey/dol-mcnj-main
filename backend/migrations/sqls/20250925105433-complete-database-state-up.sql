ac7dc7d303a17d0750a2bbceaa4955ee4019e5a8
ChelseaKR <chelsea.kelly-reif@coforma.io>
2025-09-25 19:09:55 -0700

    CRITICAL FIX: Remove search_path reset that broke db-migrate
    - Remove 'SELECT pg_catalog.set_config('search_path', '', false);' from migration
    - This line cleared the search path, preventing db-migrate from finding migrations table
    - Migration SQL ran successfully but db-migrate couldn't record completion
    - Verbose logs showed INSERT to migrations table failed due to cleared search path

--- backend/migrations/sqls/20250925105433-complete-database-state-up.sql
+++ backend/migrations/sqls/20250925105433-complete-database-state-up.sql
@@ -11,7 +11,8 @@ SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
+-- Note: Removed 'SELECT pg_catalog.set_config('search_path', '', false);' 
+-- This was preventing db-migrate from finding the migrations table after the migration ran
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
