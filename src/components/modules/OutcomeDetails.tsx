38431ffe3472eb4d71e4cbdadd4b49c1cef7ca86
ChelseaKR <chelsea.kelly-reif@coforma.io>
2025-09-29 18:43:22 -0700

    latest

--- src/components/modules/OutcomeDetails.tsx
+++ src/components/modules/OutcomeDetails.tsx
@@ -11,7 +11,7 @@ import {
   hasOutcomeData 
 } from '@utils/outcomeHelpers';
 import { useState } from 'react';
-import { CrcInfoDrawer } from '../../app/training/[code]/CrcInfoDrawer';
+import { CrcInfoDrawer } from '@components/modules/CrcInfoDrawer';
 import { BriefcaseIcon, BuildingOfficeIcon, GraduationCapIcon, MoneyWavyIcon, Warning } from '@phosphor-icons/react';
 
 interface OutcomeDetailsProps {
