feb7bfc177f7b5de2066dd85b3a5e0997f82ca95
ChelseaKR <chelsea.kelly-reif@coforma.io>
2025-10-16 16:33:43 -0700

    chore: remove backend/dist_old and add to gitignore

--- backend/dist_old/helpers/nameValidator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidName = void 0;
-const isValidName = (name) => {
-    const nameRegex = /^[a-zA-Z\s'-]{1,50}$/;
-    return typeof name === "string" && nameRegex.test(name);
-};
-exports.isValidName = isValidName;
